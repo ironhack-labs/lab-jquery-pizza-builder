11dc3f34cd4d396498e1e4a0031012b4eb7f159e
Tomas Ruiz <tomastrg22@gmail.com>
2017-01-24 16:27:12 +0100

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,34 @@
 // Write your Pizza Builder JavaScript in this file.
+
+//ITERATION 1
+$(document).ready(function(){
+$(".btn-pepperonni").on("click",function(){
+  $(".pep").toggle();
+  $(".btn-pepperonni").toggleClass("active");
+});
+
+$(".btn-mushrooms").on("click",function(){
+  $(".mushroom").toggle();
+  $(".btn-mushrooms").toggleClass("active");
+});
+$(".btn-green-peppers").on("click",function(){
+  $(".btn-green-peppers").toggleClass("active");
+});
+
+$(".btn-sauce").on("click",function(){
+  $(".sauce").toggleClass("sauce-white");
+  $(".btn-sauce").toggleClass("active");
+});
+
+$(".btn-crust").on("click",function(){
+  $(".crust").toggleClass( "crust-gluten-free" );
+  $(".btn-crust").toggleClass("active");
+
+});
+
+
+//ITERATION2
+$(".crust").toggleClass("crust-gluten-free");
+$(".sauce").toggleClass("sauce-white");
+
+});
