67bc1c7c9796d7a0120614c0b84a8a47279ee795
Jorge Abad <jorgejoseabad@gmail.com>
2017-01-25 14:23:01 +0100

    primera fase

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,24 @@
-// Write your Pizza Builder JavaScript in this file.
+$(document).on('ready',function(){
+  console.log('linked');
+  $('.btn-pepperonni').on('click', function(){
+    $('.pep').toggle();
+  });
+  $('.btn-mushrooms').on('click', function(){
+    $('.mushroom').toggle();
+  });
+  $('.btn-green-peppers').on('click', function(){
+    $('.green-pepper').toggle();
+  });
+
+  $('.sauce').removeClass('sauce-white');
+
+  $('.btn-sauce').on('click', function(){
+    $('.sauce').addClass('sauce-white');
+  });
+
+  $('.crust').removeClass('crust-gluten-free');
+
+  $('.btn-crust').on('click',function(){
+    $('.crust').addClass('crust-gluten-free');
+  });
+});// Write your Pizza Builder JavaScript in this file.
