be46c0a29ab3b771dc112f2d5c71b62b48035c61
Kevin D. Silva <kevin.dasilva1@gmail.com>
2017-02-08 09:36:55 -0500

    not done-2

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,36 @@
-// Write your Pizza Builder JavaScript in this file.
+// Write your Pizza Builder JavaScript in this file
+$(document).ready(function(){
+
+
+
+var greenPepper = $('.green-pepper');
+var mushroom = $('.mushroom');
+var pepperonni = $('.pep');
+var glutenFreeCrust = $('.crust-gluten-free');
+var whiteSauce = $('.sauce-white');
+greenPepper.hide();
+mushroom.hide();
+pepperonni.hide();
+
+$('.btn-pepperonni').click(function(){
+  pepperonni.toggle();
+});
+$('.btn-mushrooms').click(function(){
+  mushroom.toggle();
+});
+$('.btn-green-peppers').click(function(){
+  greenPepper.toggle();
+});
+
+$('.btn-sauce').click(function(){
+  whiteSauce.toggle();
+});
+
+$('.btn-crust').click(function(){
+  glutenFreeCrust.toggle(); //// NO CRUST FOR THE HIPPIES
+});
+
+
+});
+
+// Unfinished Tuesday 6:30pm-ish. Oleh, Javi, Kev0
