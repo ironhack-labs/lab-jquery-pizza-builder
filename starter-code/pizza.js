8ef459e636cf1f212db7624871fbb97309d407fe
SatishS <satish25@gmail.com>
2017-02-25 16:17:49 -0500

    iteration 4

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -3,6 +3,8 @@ var defaultIngredients = function(){
   $('.sauce').removeClass('sauce-white');
   $('.btn-crust').removeClass('active');
   $('.btn-sauce').removeClass('active');
+  $('.panel.price').find("ul").find("li:contains('$5')").toggle();
+  $('.panel.price').find("ul").find("li:contains('$3')").toggle();
 
 };
 
@@ -13,28 +15,33 @@ $(document).ready(function(){
   $('.btn-pepperonni').click(function(){
     $('.pep ').fadeToggle(100);
     $('.btn-pepperonni').toggleClass('active');
+    $('.panel.price').find("ul").find("li:contains('pepperonni')").toggle();
   });
 
   $('.btn-mushrooms').click(function(){
     $('.mushroom ').fadeToggle(100);
     $('.btn-mushrooms').toggleClass('active');
+    $('.panel.price').find("ul").find("li:contains('mushroom')").toggle();
   });
 
   $('.btn-green-peppers').click(function(){
     $('.green-pepper').fadeToggle(100);
     $('.btn-green-peppers').toggleClass('active');
+    $('.panel.price').find("ul").find("li:contains('green')").toggle();
   });
 
   $('.btn-sauce').click(function(){
     // $('.sauce-white').fadeToggle(500);
     $('.sauce').toggleClass('sauce-white');
     $('.btn-sauce').toggleClass('active');
+    $('.panel.price').find("ul").find("li:contains('$3')").toggle();
   });
 
   $('.btn-crust').click(function(){
     // $('.crust-gluten-free').toggleClass();
     $('.crust').toggleClass('crust-gluten-free');
     $('.btn-crust').toggleClass('active');
+    $('.panel.price').find("ul").find("li:contains('$5')").toggle();
   });
 
 });
