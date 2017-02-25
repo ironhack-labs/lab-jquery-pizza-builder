06afae346c9fcfef9fc45c6a4f18863e9a33a714
SatishS <satish25@gmail.com>
2017-02-25 15:58:55 -0500

    iteration 3

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,41 @@
+var defaultIngredients = function(){
+  $('.crust').removeClass('crust-gluten-free');
+  $('.sauce').removeClass('sauce-white');
+  $('.btn-crust').removeClass('active');
+  $('.btn-sauce').removeClass('active');
+
+};
+
+$(document).ready(function(){
+
+  defaultIngredients();
+
+  $('.btn-pepperonni').click(function(){
+    $('.pep ').fadeToggle(100);
+    $('.btn-pepperonni').toggleClass('active');
+  });
+
+  $('.btn-mushrooms').click(function(){
+    $('.mushroom ').fadeToggle(100);
+    $('.btn-mushrooms').toggleClass('active');
+  });
+
+  $('.btn-green-peppers').click(function(){
+    $('.green-pepper').fadeToggle(100);
+    $('.btn-green-peppers').toggleClass('active');
+  });
+
+  $('.btn-sauce').click(function(){
+    // $('.sauce-white').fadeToggle(500);
+    $('.sauce').toggleClass('sauce-white');
+    $('.btn-sauce').toggleClass('active');
+  });
+
+  $('.btn-crust').click(function(){
+    // $('.crust-gluten-free').toggleClass();
+    $('.crust').toggleClass('crust-gluten-free');
+    $('.btn-crust').toggleClass('active');
+  });
+
+});
 // Write your Pizza Builder JavaScript in this file.
