c8ce5cef0284c0148538773da6764923453dbc86
Gustavo Gutierrez <imguss@gmail.com>
2017-02-25 15:06:19 -0500

    ingredients done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,29 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+
+  // ingredients
+
+  $('.btn-pepperonni').on('click',function(){
+    $('.pep').toggle();
+  });
+
+  $('.btn-mushrooms').on('click',function(){
+    $('.mushroom').toggle();
+  });
+
+  $('.btn-green-peppers').on('click',function(){
+    $('.green-pepper').toggle();
+  });
+
+  $('.btn-sauce').on('click',function(){
+    $('.sauce').toggle();
+  });
+
+  $('.btn-crust').on('click',function(){
+    $('.crust').toggle();
+  });
+
+// end ingredients
+
+
+});
