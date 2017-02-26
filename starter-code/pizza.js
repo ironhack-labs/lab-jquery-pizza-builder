a55049b5c0ef11394802fbfbb5fdc01bd8d05dd8
MiguelM <maldesan@gmail.com>
2017-02-26 17:28:12 -0500

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,65 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(".btn-pepperonni").click(function(e){
+  //  e.preventDefault();
+  //console.log("You clicked the button!");
+  $(this).toggleClass('active');
+  $('.pep').toggle();
+  $("li:contains('pepperonni')").toggle();
+  calcPrice();
+});
+
+$(".btn-mushrooms").click(function(e){
+  $(this).toggleClass('active');
+  $('.mushroom').toggle();
+  $("li:contains('mushrooms')").toggle();
+  calcPrice();
+});
+
+$(".btn-green-peppers").click(function(e){
+  $(this).toggleClass('active');
+  $('.green-pepper').toggle();
+  $("li:contains('green peppers')").toggle();
+  calcPrice();
+});
+
+$(".btn-sauce").click(function(e){
+  $(this).toggleClass('active');
+  $('.sauce').toggle();
+  $("li:contains('white sauce')").toggle();
+  calcPrice();
+});
+
+$(".btn-crust").click(function(e){
+  $(this).toggleClass('active');
+//  $('.crust').toggle();
+  $("li:contains('gluten-free crust')").toggle();
+  calcPrice();
+});
+
+function calcPrice () {
+var totalPrice = 10;
+
+        if  ($('.btn-pepperonni').hasClass('active'))
+        {
+            $('#totalPrice').html(totalPrice += 1);
+        }
+
+        if  ($('.btn-mushrooms').hasClass('active'))
+        {
+            $('#totalPrice').html(totalPrice += 1);
+        }
+
+        if  ($('.btn-green-peppers').hasClass('active'))
+        {
+            $('#totalPrice').html(totalPrice += 1);
+        }
+
+        if ($('.btn-sauce').hasClass('active')) {
+            $('#totalPrice').html(totalPrice += 3);
+        }
+
+        if ($('.btn-crust').hasClass('active')) {
+          $('#totalPrice').html(totalPrice += 5);
+        }
+}
