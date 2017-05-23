0d68177b14ffeaccdecb2b04233ea3087120b27d
Juanma Perez <juanmaperezvar@gmail.com>
2017-05-23 21:34:59 +0200

    Done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,43 @@
-// Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+  var priceBase = 10;
+
+
+  $('.btn-mushrooms').click(function(){
+    $('.mushroom').toggleClass('hidden');
+    $('.pizza-mushroom').toggleClass('hidden');
+
+  });
+
+  $('.btn-green-peppers').click(function(){
+    $('.green-pepper').toggleClass('hidden');
+    $('.pizza-green-pepper').toggleClass('hidden');
+  });
+
+  $('.btn-pepperonni').click(function(){
+    $('.pep').toggleClass('hidden');
+    $('.pizza-pep').toggleClass('hidden');
+
+  });
+
+  $('.btn-sauce').click(function(){
+    $('.pizza-sauce').toggleClass('hidden');
+    $('.sauce').toggleClass('sauce-white');
+  });
+
+  $('.btn-crust').click(function(){
+    $('.pizza-crust').toggleClass('hidden');
+    $('.crust').toggleClass('crust-gluten-free');
+  });
+
+  $('button').click(function(){
+    $(this).toggleClass('active');
+  });
+
+  if($('.pizza-mushroom').length > 0){
+    
+  }
+
+  var total = priceBase + pricePep + priceGreenPepper + priceMushroom + priceSauce + priceCrust;
+
+
+});
