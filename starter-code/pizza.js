e485db7da4673903d13fc50771690191e09d2832
Javier Armero <Javalba90@gmail.com>
2017-05-23 21:36:30 +0200

    First commit Toni - Javier

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,30 @@
-// Write your Pizza Builder JavaScript in this file.
+// $(document).ready(function(){ **********************************ASK WHEN THIS IS NECESSARY
+
+/*ITERATION 1*/
+$(".btn-pepperonni").click(function() {
+  $(".pep").toggle();
+  // $(this).removeClass("active");
+
+});
+
+$(".btn-green-peppers").click(function() {
+  $(".green-pepper").toggle();
+});
+
+
+$(".btn-mushrooms").click(function() {
+  $(".mushroom").toggle();
+});
+
+/*
+ITERATION 2
+ */
+$(".crust").removeClass("crust-gluten-free");
+$(".sauce").removeClass("sauce-white");
+$(".btn-crust").click(function() {
+  $(".crust").toggleClass("crust-gluten-free");
+});
+
+$(".btn-sauce").click(function() {
+  $(".sauce").toggleClass("sauce-white");
+});
