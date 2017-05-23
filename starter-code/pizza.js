5db9b9f9dbaf5dc183c0cee6b5331564a9f93979
Manuel Valle <vallerem17@gmail.com>
2017-05-23 22:16:08 +0200

    In progress

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,39 @@
-// Write your Pizza Builder JavaScript in this file.
+
+
+$(document).ready(function() {
+
+  //Buttons
+
+  $("button").click(function(event){
+    $(this).toggleClass("active");
+  });
+
+  $("button.btn-mushrooms").click(function(event){
+    $("section.mushroom").toggle("display");
+  });
+
+  $("button.btn-green-peppers").click(function(event){
+    $("section.green-pepper").toggle("display");
+  });
+
+  $("button.btn-pepperonni ").click(function(event){
+    $("section.pep").toggle("display");
+  });
+
+  //Sauce and crust Buttons
+
+
+  $("button.btn-sauce").removeClass("active");
+
+  $("section.sauce").removeClass("sauce-white");
+
+  $("button.btn-sauce").click(function(event){
+    $("section.sauce").toggleClass("sauce-white");
+  });
+
+
+
+  $("button.btn-crust").removeClass("active");
+
+
+});
