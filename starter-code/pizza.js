8154671024ed390cabf209086304b39596b1bcb9
Manuel Valle <vallerem17@gmail.com>
2017-05-25 00:24:09 +0200

    F****ng 5th step

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -2,38 +2,108 @@
 
 $(document).ready(function() {
 
+
   //Buttons
+
 
   $("button").click(function(event){
     $(this).toggleClass("active");
   });
 
-  $("button.btn-mushrooms").click(function(event){
-    $("section.mushroom").toggle("display");
+
+
+  $("button.btn-pepperonni").click(function(event){
+    $("section.pep").toggle("display");
+    $("li:contains('pepperonni')").toggle("display");
+
+    if(!$("button.btn-pepperonni").hasClass("active")){
+      $(".price strong").text("$" + (counter++));
+    } else if ($("button.btn-pepperonni").hasClass("active")){
+      $(".price strong").text("$" + (counter--));
+    }
+
   });
+
+
+
 
   $("button.btn-green-peppers").click(function(event){
     $("section.green-pepper").toggle("display");
+    $("li:contains('green peppers')").toggle("display");
+    if(!$("button.btn-green-peppers").hasClass("active")){
+      $(".price strong").text("$" + (counter++));
+    } else if ($("button.btn-green-peppers").hasClass("active")){
+      $(".price strong").text("$" + (counter--));
+    }
   });
 
-  $("button.btn-pepperonni ").click(function(event){
-    $("section.pep").toggle("display");
+  $("button.btn-mushrooms").click(function(event){
+    $("section.mushroom").toggle("display");
+    $("li:contains('mushrooms')").toggle("display");
+    if(!$("button.btn-mushrooms").hasClass("active")){
+      $(".price strong").text("$" + (counter++));
+    } else if ($("button.btn-mushrooms").hasClass("active")){
+      $(".price strong").text("$" + (counter--));
+    }
   });
 
   //Sauce and crust Buttons
 
 
-  $("button.btn-sauce").removeClass("active");
-
+  //Sauce
+  $("button.btn-sauce").toggleClass("active");
   $("section.sauce").removeClass("sauce-white");
 
   $("button.btn-sauce").click(function(event){
     $("section.sauce").toggleClass("sauce-white");
+    $("li:contains('white sauce')").toggle("display");
   });
 
+  if (!$("button.btn-sauce").hasClass("active")){
+    $("li:contains('white sauce')").css({"display":"none"});
+  }
 
 
-  $("button.btn-crust").removeClass("active");
+  // Crust
+  $("button.btn-crust").toggleClass("active");
+  $("section.crust").removeClass("crust-gluten-free");
+
+  $("button.btn-crust").click(function(event){
+    $("section.crust").toggleClass("crust-gluten-free");
+    $("li:contains('gluten-free crust')").toggle("display");
+  });
+
+  if (!$("button.btn-crust").hasClass("active")){
+    $("li:contains('gluten-free crust')").css({"display":"none"});
+  }
+
+
+  //Adding to the final price
+
+  var counter = 16;
+   $(".price strong").text("$" + counter);
+
+
+//console.log(counter);
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
