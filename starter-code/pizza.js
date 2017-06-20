301e2e7dc9dd46c84f6290eaf1565d32f060a3f6
Carlos Dueñas <carlosdss84@gmail.com>
2017-06-20 12:44:55 +0200

    versión iteration 2 en curso

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,33 @@
-// Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+
+  $(".btn-green-peppers").click(function(event) {
+    $(".green-pepper").toggle();
+  })
+
+    $(".btn-mushrooms").click(function(event) {
+      $(".mushroom").toggle();
+    })
+
+    $(".btn-pepperonni").click(function(event) {
+        $(".pep").toggle();
+    })
+
+    $(".btn-sauce").click(function(event) {
+        if ($(".sauce").hasClass("sauce-white")){
+          $(".sauce").removeClass("sauce-white");
+          $(".sauce").removeClass("sauce-white")
+        };
+    })
+
+
+    /*var m = $("#pizza" ".green-pepper").each(function(e){
+      var display = $(this).css("display");
+      if (display === "none"){
+        $(this).css("display", "inherit");
+      }else if (display === "inherit"){
+        $(this).css("display", "none");
+      }
+      }
+    })*/
+
+});
