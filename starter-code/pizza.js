98ad8136649813b07d5ee3ba4914f61834157d0c
Carlos Dueñas <carlosdss84@gmail.com>
2017-06-20 15:43:48 +0200

    versión con iteración 2

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -12,14 +12,44 @@ $(document).ready(function() {
         $(".pep").toggle();
     })
 
+    $(".btn-sauce").text("Regular sauce");
+    $(".btn-sauce").css("background-color", "crimson");
+    $(".sauce-white").css("background-color", "crimson");
+    $(".btn-sauce").css("color", "white");
+
     $(".btn-sauce").click(function(event) {
-        if ($(".sauce").hasClass("sauce-white")){
-          $(".sauce").removeClass("sauce-white");
-          $(".sauce").removeClass("sauce-white")
-        };
-    })
+        if (($(".btn-sauce").text())==="White sauce"){
+          $(".btn-sauce").text("Regular sauce");
+          $(".btn-sauce").css("background-color", "crimson");
+          $(".sauce-white").css("background-color", "crimson");
+          $(".btn-sauce").css("color", "white");
+        } else if(($(".btn-sauce").text())==="Regular sauce"){
+          $(".btn-sauce").text("White sauce");
+          $(".sauce-white").css("background-color", "#fff4aa");
+          $(".btn-sauce").css("background-color", "#fff4aa");
+          $(".btn-sauce").css("color", "#555");
+        }
+      });
 
 
+      $(".btn-crust").text("Crust");
+      $(".btn-crust").css("background-color", "#CD853F");
+      $(".crust-gluten-free").css("background-color", "#CD853F");
+      $(".btn-crust").css("color", "white");
+
+      $(".btn-crust").click(function(event) {
+          if (($(".btn-crust").text())==="Crust"){
+            $(".btn-crust").text("crust-gluten-free");
+            $(".btn-crust").css("background-color", "#FFDAB9");
+            $(".crust-gluten-free").css("background-color", "#FFDAB9");
+            $(".btn-crust").css("color", "white");
+          } else if(($(".btn-crust").text())==="crust-gluten-free"){
+            $(".btn-crust").text("Crust");
+            $(".crust-gluten-free").css("background-color", "#CD853F");
+            $(".btn-crust").css("background-color", "#CD853F");
+            $(".btn-crust").css("color", "white");
+          }
+        });
     /*var m = $("#pizza" ".green-pepper").each(function(e){
       var display = $(this).css("display");
       if (display === "none"){
