05e5ce7084ad34b19e09ba4a3b87438efb0b4061
rosadl <rosadl84@gmail.com>
2017-06-20 12:41:34 +0200

    firt iteration

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,17 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+  $('.btn').click(function(){
+    $(this).toggleClass('active');
+  });
+
+  $(".btn-pepperonni").click(function() {
+    $(".pep").toggle();
+  });
+  $(".btn-green-peppers").click(function() {
+    $(".green-pepper").toggle();
+  });
+  $(".btn-mushrooms").click(function() {
+    $(".mushroom").toggle();
+  });
+  
+});
