d43afc8cd58b863fe1b6bdf5a3949bd9861b6e98
mikelRumayor <mikel.rumayor@gmail.com>
2017-06-20 12:22:18 +0200

    iteration 2

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,45 @@
 // Write your Pizza Builder JavaScript in this file.
+
+  $('.btn-pepperonni').click(function(){
+    if( $('.pep').css('display') === 'none'){
+      $('.pep').css('display', 'block')
+    } else {
+      $('.pep').css('display', 'none')
+    }
+
+  });
+
+  $('.btn-green-peppers').click(function(){
+    if( $('.green-pepper').css('display') === 'none'){
+      $('.green-pepper').css('display', 'block')
+    } else {
+      $('.green-pepper').css('display', 'none')
+    }
+
+  });
+
+  $('.btn-mushrooms').click(function(){
+    if( $('.mushroom').css('display') === 'none'){
+      $('.mushroom').css('display', 'block')
+    } else {
+      $('.mushroom').css('display', 'none')
+    }
+
+  });
+
+
+    $('.btn-crust').click(function(){
+      if( $('.cheese').css('display') === 'none'){
+        $('.cheese').css('display', 'block')
+      } else {
+        $('.cheese').css('display', 'none')
+      }
+    });
+
+    $('.btn-sauce').click(function(){
+      if( $('.sauce').css('display') === 'none'){
+        $('.sauce').css('display', 'block')
+      } else {
+        $('.sauce').css('display', 'none')
+      }
+    });
