3a6d3a4bf29a189f79139ad04d2b87bc160ac49d
Marc Palatsí <mpalatsi@me.com>
2017-06-20 16:57:11 +0200

    casi Done Jorge Manchon y Marc Palatsí

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,48 +1,60 @@
 $(document).ready(function() {
-  // $(".green-pepper").addClass("peppers")
   $(".sauce").toggleClass("sauce-white");
   $(".btn-sauce").toggleClass("active");
   $(".crust").toggleClass("crust-gluten-free");
   $(".btn-crust").toggleClass("active");
+  $(".price li:contains('sauce')").fadeToggle();
+  $(".price li:contains('crust')").fadeToggle();
+
   $(".btn-green-peppers").click(function(event) {
-    // event.preventDefault();
     $(".green-pepper").fadeToggle();
+    $(".price li:contains('peppers')").fadeToggle();
   });
 
   $(".btn-pepperonni").click(function(event) {
     $(".pep").fadeToggle();
+    $(".price li:contains('pepperonni')").fadeToggle();
   });
 
   $(".btn-mushrooms").click(function(event) {
     $(".mushroom").fadeToggle();
+    $(".price li:contains('mushroom')").fadeToggle();
   });
 
   $(".btn-sauce").click(function(event) {
     $(".sauce").toggleClass("sauce-white");
+    $(".price li:contains('sauce')").fadeToggle();
   });
 
   $(".btn-crust").click(function(event) {
     $(".crust").toggleClass("crust-gluten-free");
+    $(".price li:contains('crust')").fadeToggle();
   });
 
   $(".btn-pepperonni").click(function(event) {
-    $(".btn-pepperonni").toggleClass("active");
+    $(this).toggleClass("active");
   });
 
   $(".btn-mushrooms").click(function(event) {
-    $(".btn-mushrooms").toggleClass("active");
+    $(this).toggleClass("active");
   });
 
   $(".btn-green-peppers").click(function(event) {
-    $(".btn-green-peppers").toggleClass("active");
+    $(this).toggleClass("active");
   });
 
   $(".btn-sauce").click(function(event) {
-    $(".btn-sauce").toggleClass("active");
+    $(this).toggleClass("active");
   });
 
   $(".btn-crust").click(function(event) {
-    $(".btn-crust").toggleClass("active");
+    $(this).toggleClass("active");
   });
+
+var totalPrice = 11;
+
+// $('.price li:first').text().split(" ")[0];
+
+$("strong").replaceWith("<strong>$" + totalPrice + "</strong>");
 
 });
