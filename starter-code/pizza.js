086b4d393256bfc253bafd54290fc977dca19a75
Claudia Duarte <Clau@MacBook-Pro-de-Claudia.local>
2017-06-20 12:10:06 +0200

    Primera Interaccion

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,28 @@
 // Write your Pizza Builder JavaScript in this file.
+$("document").ready(function(){
+  $(".btn-pepperonni.active").click(function(){
+    $(".btn-pepperonni").toggleClass("active");
+    if ($(".pep").css("display") == "block"){
+      $(".pep").css("display", "none");
+    }else {
+      $(".pep").css("display","block");
+    }
+  });
+  $(".btn-mushrooms.active").click(function(){
+    $(".btn-mushrooms").toggleClass("active");
+    if ($(".mushroom").css("display") == "block"){
+      $(".mushroom").css("display", "none");
+    }else {
+      $(".mushroom").css("display","block");
+    }
+  });
+  $(".btn-green-peppers.active").click(function(){
+    $(".btn-green-peppers").toggleClass("active");
+    if ($(".green-pepper").css("display") == "block"){
+      $(".green-pepper").css("display", "none");
+    }else {
+      $(".green-pepper").css("display","block");
+    }
+
+  });
+});
