d9d833658eacef60a44ef533c456fc6dfd60b016
sara <sara-cg@github.com>
2017-06-20 12:47:00 +0200

    first try

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,32 @@
-// Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+
+  $(".sauce").removeClass("sauce-white");
+  $(".crust").removeClass("crust-gluten-free");
+
+  $('.btn').on('click', function() {
+    $(this).toggleClass("active");
+  });
+
+
+  $(".btn-pepperonni").click(function() {
+    $(".pep").toggle();
+  });
+
+  $(".btn-mushrooms").click(function() {
+    $(".mushroom").toggle();
+  });
+
+  $(".btn-green-peppers").click(function() {
+    $(".green-pepper").toggle();
+  });
+
+  $(".btn-sauce").click(function() {
+    $(".sauce").toggleClass("sauce-white");
+  });
+
+  $(".btn-crust").click(function() {
+    $(".crust").toggleClass("crust-gluten-free");
+  });
+
+
+});
