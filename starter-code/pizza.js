02d381588dbe902e7e2e84a894f50b1439d0af4f
rosadl <rosadl84@gmail.com>
2017-06-20 16:39:41 +0200

    iter 4

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -6,12 +6,30 @@ $(document).ready(function() {
 
   $(".btn-pepperonni").click(function() {
     $(".pep").toggle();
+    $("li:contains('pepperonni')").toggle();
   });
   $(".btn-green-peppers").click(function() {
     $(".green-pepper").toggle();
+    $("li:contains('green peppers')").toggle();
   });
   $(".btn-mushrooms").click(function() {
     $(".mushroom").toggle();
+    $("li:contains('mushroom')").toggle();
   });
-  
+
+  $("section.crust").removeClass("crust-gluten-free");
+  $(".sauce").removeClass("sauce-white");
+
+
+  $(".btn-crust").click(function(){
+    $("section.crust").toggleClass("crust-gluten-free");
+    $("li:contains('gluten-free crust')").toggle();
+  });
+  $(".btn-sauce").click(function(){
+    $("section.sauce").toggleClass("sauce-white");
+    $("li:contains('white sauce')").toggle();
+  });
+
+  $("li:contains('white sauce')").hide();
+  $("li:contains('gluten-free crust')").hide();
 });
