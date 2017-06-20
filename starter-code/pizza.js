6832ca28bedec074cc87eb47cbf954100f096361
mikelRumayor <mikel.rumayor@gmail.com>
2017-06-20 13:05:50 +0200

    iteration 5

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -4,26 +4,26 @@
     $('.btn-pepperonni').toggleClass('active')
     if( $('.pep').css('display') === 'none'){
       $('.pep').css('display', 'block')
-      $('ul > li:contains(pepperonni)').css('display', 'block')
+      $('ul > li:contains(pepperonni)').css('display', 'list-item')
 
     } else {
       $('.pep').css('display', 'none')
       $('ul > li:contains(pepperonni)').css('display', 'none')
     }
-
+    updatePrice()
   });
 
   $('.btn-green-peppers').click(function(){
     $('.btn-green-peppers').toggleClass('active')
     if( $('.green-pepper').css('display') === 'none'){
       $('.green-pepper').css('display', 'block')
-      $('ul > li:contains(green)').css('display', 'block')
+      $('ul > li:contains(green)').css('display', 'list-item')
 
     } else {
       $('.green-pepper').css('display', 'none')
       $('ul > li:contains(green)').css('display', 'none')
     }
-
+updatePrice()
   });
 
   $('.btn-mushrooms').click(function(){
@@ -31,11 +31,11 @@
 
     if( $('.mushroom').css('display') === 'none'){
       $('.mushroom').css('display', 'block')
-      $('ul > li:contains(mushroom)').css('display', 'block')
+      $('ul > li:contains(mushroom)').css('display', 'list-item')
     } else {
       $('.mushroom').css('display', 'none')
       $('ul > li:contains(mushroom)').css('display', 'none')
-
+updatePrice()
     }
 
   });
@@ -46,11 +46,12 @@
 
       if( $('.cheese').css('display') === 'none'){
         $('.cheese').css('display', 'block')
-        $('ul > li:contains(gluten-free crust)').css('display', 'block')
+        $('ul > li:contains(gluten-free crust)').css('display', 'list-item')
       } else {
         $('.cheese').css('display', 'none')
         $('ul > li:contains(gluten-free crust)').css('display', 'none')
       }
+      updatePrice()
     });
 
     $('.btn-sauce').click(function(){
@@ -58,10 +59,23 @@
 
       if( $('.sauce').css('display') === 'none'){
         $('.sauce').css('display', 'block')
-        $('ul > li:contains(white sauce)').css('display', 'block')
+        $('ul > li:contains(white sauce)').css('display', 'list-item')
       } else {
         $('.sauce').css('display', 'none')
         $('ul > li:contains(white sauce)').css('display', 'none')
-
       }
+      updatePrice()
     });
+
+    function updatePrice () {
+      var total = 10
+      var ul = $($('.panel.price')).children('ul')
+
+      $(ul).children().toArray().map(function (li, index) {
+        if($(li).css('display') === 'list-item'){
+          total += parseInt(($(li).html().match(/\d+/)[0]))
+        }
+      })
+      $($('.panel.price')).children('strong').html('$' + total)
+
+    }
