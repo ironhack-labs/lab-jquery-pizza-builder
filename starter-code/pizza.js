44fe598c70ca28883a647002bbeecdcba3940426
Daniele <daniele.germani.85@gmail.com>
2017-06-20 12:45:02 +0200

    gluten free

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,25 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+
+  $(".btn-pepperonni").click(function() {
+    $(".pep").toggle();
+  });
+  $(".btn-green-peppers").click(function() {
+    $(".green-pepper").toggle();
+  });
+  $(".btn-mushrooms").click(function() {
+    $(".mushroom").toggle();
+  });
+  $(".btn-sauce").click(function() {
+    $(".sauce-white").toggleClass();
+    $(".sauce").toggleClass();
+  });
+  $(".btn-crust").click(function() {
+
+    $(".crust").toggleClass();
+    $(".crust-gluten-free").toggleClass();
+  });
+
+
+
+});
