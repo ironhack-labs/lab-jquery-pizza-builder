61e8aca9d5fb3f4969eef350bd98596edff22e04
Joao Campos <j.campos4g@gmail.com>
2017-08-16 19:02:28 -0400

    in progress

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,64 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+
+
+
+    $('.btn-pepperonni').click(function() {
+        console.log($('.pepPrice').val());
+        $('.pep').toggle();
+        $('.pepPrice').toggle();
+
+    });
+
+});
+
+$(document).ready(function() {
+
+    $('.btn-mushrooms').click(function() {
+
+        $('.mushroom').toggle();
+        $('.mushPrice').toggle();
+    });
+
+});
+
+
+$(document).ready(function() {
+
+    $('.btn-green-peppers').click(function() {
+        $('.green-pepper').toggle();
+        $('.greenPrice').toggle();
+    });
+});
+
+$(document).ready(function() {
+
+  $('.btn-sauce').click(function() {
+      $('.sauce').toggleClass('sauce-white');
+      $('.whitePrice').html('$3 white sauce');
+  });
+
+});
+
+
+$(document).ready(function() {
+
+  $('.btn-crust').click(function() {
+
+      $('.crust').toggleClass('crust-gluten-free');
+
+  });
+
+});
+
+$(document).ready(function() {
+
+  $('.btn').click(function() {
+    $(this).toggleClass('active');
+  });
+
+});
+
+$(document).ready(function() {
+
+});
