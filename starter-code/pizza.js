a52c9fec1510187bb9cc22dd88435e2f2c2e39ee
Francisco Paradela <fparafer@gmail.com>
2017-08-22 16:58:32 +0200

    second iteration

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -13,6 +13,15 @@ $(".btn-green-peppers").on("click", function(){
   $(".green-pepper").toggle();
 })
 
+});
 
+$(".crust-gluten-free").removeClass("crust-gluten-free")
+$(".sauce-white").removeClass("sauce-white");
 
+$(".btn-crust").on("click", function(){
+  $(".crust").addClass("crust-gluten-free");
+});
+
+$(".btn-sauce").on("click", function(){
+  $(".sauce").addClass("sauce-white");
 });
