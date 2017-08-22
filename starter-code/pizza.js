a259d0e014e0d587e13441acb89909a5f07183c3
Carlos Cabezas <carloscabezascontra@gmail.com>
2017-08-22 15:44:53 +0200

    iteration 1 done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,21 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(document).ready(function() {
+  //Iteration 1.
+  $('div.controls button.btn').on('click', function(e) {
+    $x = e.currentTarget 
+    switch ($x.innerHTML) {
+      case 'Pepperonni':
+        $('#pizza section.pep').hide()
+        break;
+      case 'Mushrooms':
+        $('#pizza section.mushroom').hide()
+        break;
+      case 'Green peppers':
+        $('#pizza section.green-pepper').hide()
+        break;
+      default:
+        break;
+    }
+  })
+})
