e845de7098bd925e66d7bb1c9ca55e757034944e
Lucas <lfbodereau@gmail.com>
2017-08-22 17:05:49 +0200

    MAD Lucas y Chris

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,33 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(document).ready(function(){
+  $('.btn-pepperonni').removeClass('active');
+  $('.btn-mushrooms').removeClass('active');
+  $('.btn-green-peppers').removeClass('active');
+
+  $('.btn-pepperonni').click(function(){
+    $('.pep').toggle();
+    $('.btn-pepperonni').toggleClass('active');
+  });
+
+  $('.btn-mushrooms').click(function() {
+    $('.mushroom').toggle();
+    $('.btn-mushrooms').toggleClass('active');
+  });
+
+  $('.btn-green-peppers').click(function(){
+    $('.green-pepper').toggle();
+    $('.btn-green-peppers').toggleClass('active');
+  });
+
+  $('.btn-sauce').click(function(){
+    $('.sauce-white').toggle();
+    $('.btn-sauce').toggleClass('active');
+  });
+
+  $('.btn-crust').click(function(){
+    $('.crust').toggleClass('crust-gluten-free');
+    $('.btn-crust').toggleClass('active');
+  });
+
+});
