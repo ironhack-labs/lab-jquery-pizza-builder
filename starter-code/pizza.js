6d87f676e6f5a2bb199e08f86a956796b223b6b3
ritorrehuelamo <torrehuelamo@gmail.com>
2017-08-22 17:09:25 +0200

    Changes

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,13 +1,30 @@
 // Write your Pizza Builder JavaScript in this file.
 
-$('.btn-pepperonni').on('click', function () {
-	$('#pizza .pep').toggle()
-})
+$(document).ready(function () {
+	
 
-$('.btn-mushrooms').on('click', function () {
-	$('#pizza .mushroom').toggle()
-})
+	
+	// Iteration One - Left Buttons
+	$('.btn-pepperonni').on('click', function () {		
+		$('.pep').toggle()
+	})
+	
+	$('.btn-mushrooms').on('click', function () {
+		$('.mushroom').toggle()
+	})
+	
+	$('.btn-green-peppers').on('click', function () {
+		$('.green-pepper').toggle()
+	})
+	// Iteration Two - Left Buttons
+	$('.btn-sauce').on('click', function () {
+		$('.sauce-white').toggle()
+	})
+	$("section.crust").attr("class","crust")
+	$("section.sauce").attr("class","sauce")
+	
+	// Iteration Three
+	// $(".btn-pepperonni").on(click,)
 
-$('.btn-green-peppers').on('click', function () {
-	$('#pizza .green-pepper').toggle()
+		
 })
