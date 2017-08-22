69063a421be85c25d99ee7f72a0409477e0a6ded
pablo fuentes <pablofuentesg1@yahoo.es>
2017-08-22 17:02:12 +0200

    interaccion 3 a medias

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,28 +1,21 @@
 // Write your Pizza Builder JavaScript in this file.
 
-$(document).ready(function(){
+$(document).ready(function() {
   $(".crust").removeClass("crust-gluten-free");
   $(".sauce").removeClass("sauce-white");
-$(".btn-pepperonni").on("click", function(  ){
-$(".pep").toggle();
 
-}
-);
-$(".btn-mushrooms").on("click", function(  ){
-$(".mushroom").toggle();
+  $(".btn-pepperonni").on("click", function() {
+    $(".pep").toggle();
+    $(".btn-pepperonni").toggleClass("active");
+  });
 
-}
-);
-$(".btn-green-peppers").on("click", function(  ){
-$(".green-pepper").toggle();
+  $(".btn-mushrooms").on("click", function() {
+    $(".mushroom").toggle();
+    $(".btn-mushrooms").toggleClass("active");
+  });
 
-}
-);
-
-
-
-
-
-
-
+  $(".btn-green-peppers").on("click", function() {
+    $(".green-pepper").toggle();
+    $(".btn-green-peppers").toggleClass("active");
+  });
 });
