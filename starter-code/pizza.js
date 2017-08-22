4fb09913f894ed26bcc22de1c1c58bf803aceca1
Carlos Cabezas <carloscabezascontra@gmail.com>
2017-08-22 16:31:20 +0200

    iteration 4 done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,27 +1,39 @@
 // Write your Pizza Builder JavaScript in this file.
 
 $(document).ready(function() {
-  //Iteration 1.
+  $('div.controls button.btn-sauce').toggleClass('active')
+  $('#pizza section.crust section.sauce').hide()
+  $('div.controls button.btn-crust').toggleClass('active')
+  $('#pizza section.crust').toggleClass('crust-gluten-free')
+  $('aside.price li:last-child').hide()
+  $('aside.price li:nth-child(4)').hide()
+  function isActive() {
+
+  }
+
   $('div.controls button.btn').on('click', function(e) {
     $x = e.currentTarget
-    //Iteration 3.
     $($x).toggleClass('active')
     switch ($x.innerHTML) {
       case 'Pepperonni':
         $('#pizza section.pep').toggle()
+        $('aside.price li:first-child').toggle()
         break;
       case 'Mushrooms':
         $('#pizza section.mushroom').toggle()
+        $('aside.price li:first-child + li').toggle()
         break;
       case 'Green peppers':
         $('#pizza section.green-pepper').toggle()
+        $('aside.price li:nth-child(3)').toggle()
         break;
-      //Iteration 2.
       case 'White sauce':
         $('#pizza section.crust section.sauce').toggle()
+        $('aside.price li:nth-child(4)').toggle()
         break;
       case 'Gluten-free crust':
         $('#pizza section.crust').toggleClass('crust-gluten-free')
+        $('aside.price li:last-child').toggle()
       default:
         break;
     }
