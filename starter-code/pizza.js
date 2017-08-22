c2236e59ae69026a7b7eb71cac3b8cb3c4b39a62
Francisco Paradela <fparafer@gmail.com>
2017-08-22 16:19:47 +0200

    first iteration

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,18 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+
+$('.btn-pepperonni').on("click",function(){
+  $(".pep").toggle();
+});
+
+$(".btn-mushrooms").on("click", function(){
+  $(".mushroom").toggle();
+});
+
+$(".btn-green-peppers").on("click", function(){
+  $(".green-pepper").toggle();
+})
+
+
+
+});
