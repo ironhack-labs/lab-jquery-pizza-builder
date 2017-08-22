16cfc337465a0d5f126a6e46ae2404f90fa03664
ritorrehuelamo <torrehuelamo@gmail.com>
2017-08-22 15:24:48 +0200

    Iteration One - Done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,13 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$('.btn-pepperonni').on('click', function () {
+	$('#pizza .pep').toggle()
+})
+
+$('.btn-mushrooms').on('click', function () {
+	$('#pizza .mushroom').toggle()
+})
+
+$('.btn-green-peppers').on('click', function () {
+	$('#pizza .green-pepper').toggle()
+})
