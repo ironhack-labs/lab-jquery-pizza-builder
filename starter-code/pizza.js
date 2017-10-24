0577c9dd8b06e8327e252a76d4a0d49c3fd39b55
gemaderus <hi@gemaderus.com>
2017-10-24 13:33:57 +0200

    First iteration finished.

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,25 @@
 // Write your Pizza Builder JavaScript in this file.
+  $( ".btn-pepperonni" ).on( "click", function() {
+    $(this).toggleClass("active");
+    if($(".pep").css("display") === "block") {
+      $(".pep").css("display", "none");
+    } else {
+      $(".pep").css("display", "block");
+    }
+  });
+  $( ".btn-mushrooms" ).on( "click", function() {
+    $(this).toggleClass("active");
+    if($(".mushroom").css("display") === "block") {
+      $(".mushroom").css("display", "none");
+    } else {
+      $(".mushroom").css("display", "block");
+    }
+  });
+  $( ".btn-green-peppers" ).on( "click", function() {
+    $(this).toggleClass("active");
+    if($(".green-pepper").css("display") === "block") {
+      $(".green-pepper").css("display", "none");
+    } else {
+      $(".green-pepper").css("display", "block");
+    }
+  });
