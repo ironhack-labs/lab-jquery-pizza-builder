708c085a3e7e442909e01728d8598ccd1d1f3df6
Maria Ruiz <mariaruizestevez@gmail.com>
2017-10-24 15:49:13 +0200

    First and second iterations complete

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,8 +1,18 @@
-// Write your Pizza Builder JavaScript in this file.
 $(document).ready(function (){
-    $(".btn-pepperonni").click(function(pepperonni) {
-      $("#pepperonni pep").removeClass("pep");
-  })
 
-
-});
+  $(".btn-pepperonni").click(function(){
+    $(".pep").toggle();
+  });
+  $(".btn-mushrooms").click(function(){
+      $(".mushroom").toggle();
+  });
+  $(".btn-green-peppers").click(function(){
+      $(".green-pepper").toggle();
+  });
+  $(".btn-sauce").click(function(){
+      $(".sauce-white").toggle();
+  });
+  $(".btn-crust").click(function(){
+      $(".crust-gluten-free").toggle();
+  });
+    });
