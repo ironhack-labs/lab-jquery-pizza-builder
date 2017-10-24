6678b1f7c6ea6e9c88c55b628ffc84c769a0c453
Camila Maya <camilamaya21@gmail.com>
2017-10-24 13:16:57 +0200

    Starting the pp  with Virginia Alvarez and Camila Maya

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,28 @@
 // Write your Pizza Builder JavaScript in this file.
+
+ //Iteration 1: Add and Remove Toppings
+
+ $(document).ready(function(){
+   //Ingredients
+   var greenPepper = $('.green-pepper');
+
+
+   //buttons
+   var btnGreenPepers = $('.btn-green-peppers');
+
+
+
+   btnGreenPepers.click(function(){
+     if ( greenPepper ) {
+       greenPepper.appendTo( "body" );
+      greenPepper = null;
+    } else {
+      greenPepper.detach();
+    }
+   });
+
+
+
+
+
+ });
