d1ee1fe0c909b5f847bf15b7a870c938aa70dd4c
Maria Ruiz <mariaruizestevez@gmail.com>
2017-10-24 17:56:19 +0200

    second iteration

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,18 +1,33 @@
-$(document).ready(function (){
+//var peppperonni = "btn-pepperonni";
+//var mush =  "btn-mushrooms";
+//var peppers = "btn-green-peppers";
 
-  $(".btn-pepperonni").click(function(){
-    $(".pep").toggle();
-  });
-  $(".btn-mushrooms").click(function(){
-      $(".mushroom").toggle();
-  });
-  $(".btn-green-peppers").click(function(){
-      $(".green-pepper").toggle();
-  });
-  $(".btn-sauce").click(function(){
-      $(".sauce-white").toggle();
-  });
-  $(".btn-crust").click(function(){
-      $(".crust-gluten-free").toggle();
-  });
+
+
+$(document).ready(function () {
+
+    $(".btn-pepperonni").click(function () {
+        $(".pep").toggle();
+
     });
+    $(".btn-mushrooms").click(function () {
+        $(".mushroom").toggle();
+    });
+    $(".btn-green-peppers").click(function () {
+        $(".green-pepper").toggle();
+    });
+    $(".btn-sauce").click(function () {
+        $(".sauce-white").toggle();
+    });
+    $(".btn-crust").click(function () {
+        $(".crust").toggleClass("crust-gluten-free");
+    })
+
+
+
+
+
+});
+
+
+// $(".active").toggle()
