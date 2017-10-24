65929505fca786707bbd2be2bc2fb880ee3d3451
Asier Cisneros <ascisneros14@gmail.com>
2017-10-24 17:37:57 +0200

    3 and 4 iterations done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -18,6 +18,8 @@ $(document).ready(function() {
   $(".crust").removeClass("crust-gluten-free");
 
   $(".sauce").removeClass("sauce-white");
+  $(".price ul li:nth-child(4)").hide();
+  $(".price ul li:nth-child(5)").hide();
 
   $(".btn-sauce").click(function(){
       $(".sauce").toggleClass("sauce-white");
@@ -29,3 +31,52 @@ $(document).ready(function() {
 });
 
 //iteraction 3
+var pepper = $(".btn-pepperonni");
+pepper.click(function() {
+  $(".btn-pepperonni").toggleClass("active");
+  if ($(".btn-pepperonni").hasClass("active")){
+    $(".price ul li:nth-child(1)").show();
+  }else{
+    $(".price ul li:nth-child(1)").hide();
+  }
+});
+
+var mush = $(".btn-mushrooms");
+mush.click(function() {
+  $(".btn-mushrooms").toggleClass("active");
+  if ($(".btn-mushrooms").hasClass("active")){
+    $(".price ul li:nth-child(2)").show();
+  }else{
+    $(".price ul li:nth-child(2)").hide();
+  }
+});
+
+var gp = $(".btn-green-peppers");
+gp.click(function() {
+  $(".btn-green-peppers").toggleClass("active");
+  if ($(".btn-green-peppers").hasClass("active")){
+    $(".price ul li:nth-child(3)").show();
+  }else{
+    $(".price ul li:nth-child(3)").hide();
+  }
+});
+
+var sauce = $(".btn-sauce");
+sauce.click(function() {
+  $(".btn-sauce").toggleClass("active");
+  if ($(".btn-sauce").hasClass("active")){
+    $(".price ul li:nth-child(4)").hide();
+  }else{
+    $(".price ul li:nth-child(4)").show();
+  }
+});
+
+var gluten = $(".btn-crust");
+gluten.click(function() {
+  $(".btn-crust").toggleClass("active");
+  if ($(".btn-crust").hasClass("active")){
+    $(".price ul li:nth-child(5)").hide();
+  }else{
+    $(".price ul li:nth-child(5)").show();
+  }
+});
