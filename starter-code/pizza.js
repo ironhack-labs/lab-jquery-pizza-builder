e4101cee4679c16f34925d7335339df5050467a3
Juan Vallejo-Nagera <juanvallejo-nagera@Juans-MacBook-Pro.local>
2017-10-24 13:13:58 +0200

    Iteration 1 in process

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,49 @@
-// Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+
+var pimiento = $("section").hasClass('green-pepper');
+
+
+if(pimiento == true){
+      $(".btn-green-peppers").click(function(){
+      $("[class^='green-pepper']").css({"display": "none"});
+      });
+    }
+if(pimiento == false){
+      $(".btn-green-peppers").click(function(){
+      $("[class^='green-pepper']").css({"display": "block"});
+      });
+    }
+
+
+  // $(".btn-green-peppers").click(function(){
+  // $("[class^='green-pepper']").css({"display": "none"});
+  // });
+
+
+
+
+
+
+
+
+
+
+
+
+// $("#menu ul li:first").addClass("selected");
+// $(".container article:nth-child(2)").addClass("middle");
+// $(".list-item:odd").addClass("highlighted");
+// $("#list-container list-item:last").css({"border":"1px solid #eee"});
+//
+ // $("#form-container input").focus(function(){
+ //   $(this).css({"background-color": "green"});
+ // });
+//  $("#form-container input").blur(function(){
+//    $(this).css({"background-color": ""});
+//  });
+
+
+
+
+
+});
