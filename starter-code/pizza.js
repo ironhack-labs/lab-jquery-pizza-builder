652793dae02a584cce5466a80a163dd72d6ea341
Rocio Garcia <gooblyn@gmail.com>
2017-10-24 13:04:14 +0200

    Pepperonni hidden

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,20 @@
 // Write your Pizza Builder JavaScript in this file.
+function init() {
+  // Add or remove pepperoni
+  // Check the pepperoni button status
+  var buttonPep = $(".btn-pepperonni");
+  buttonPep.click(function(event) {
+    if ($(".btn-pepperonni").hasClass("active")) {
+      $("#pizza .pep").css({
+        display: "none"
+      });
+    } else {
+      $("#pizza .pep").css({
+        display: " "
+      });
+    }
+    $(".btn-pepperonni").toggleClass("active");
+  });
+}
+
+  $(document).ready(init);
