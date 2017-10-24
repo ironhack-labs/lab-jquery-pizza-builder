458b783a45c330ac4910e1ff9d993078a696663b
Nikolaas Verlee <nikotomad@gmail.com>
2017-10-24 13:11:05 +0200

    Iteration 2 in progress

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,41 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+  // On document load
+  $('.green-pepper').css('display','none');
+  $('.mushroom').css('display','none');
+  $('.pep').css('display','none');
+  $('.crust.crust-gluten-free').removeClass('crust-gluten-free');
+  $('.sauce.sauce-white').removeClass('sauce-white');
+
+  // Button clicks
+  $('.btn-pepperonni').click(function() {
+    $('.pep').css('display','block');
+    $('.btn-pepperonni').addClass('active');
+  });
+
+  // $( "#clickme" ).click(function() {
+  //   $( "#" ).toggle( "slow", function() {
+  //     // Animation complete.
+  //   });
+  // });
+
+  $('.btn-mushrooms').click(function() {
+    $('.mushroom').css('display','block');
+    $('.btn-pepperonni').addClass('active');
+  });
+
+  $('.btn-green-peppers').click(function() {
+    $('.green-pepper').css('display','block');
+    $('.btn-pepperonni').addClass('active');
+  });
+
+  $('.btn-sauce').click(function() {
+    $('.sauce').toggleClass('sauce sauce-white');
+    $('.btn-pepperonni').addClass('active');
+  });
+
+  $('.btn-crust').click(function() {
+    $('.crust').addClass('crust-gluten-free');
+    $('.btn-pepperonni').addClass('active');
+  });
+});
