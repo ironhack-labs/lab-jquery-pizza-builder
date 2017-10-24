23812875d4b9a74637fa03170cba981a9ca4eceb
sdediego <sergio.dediego@outlook.com>
2017-10-24 13:10:07 +0200

    Iteracion 4 completada

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -12,30 +12,38 @@ function init() {
     $('.btn-sauce').removeClass('active');
     $('.btn-crust').removeClass('active');
 
+    $('.panel.price li:last').prev().hide();
+    $('.panel.price li:last').hide();
+
     $('.btn-pepperonni').click(function(event) {
         peps.toggle();
         $(this).toggleClass('active');
+        $('.panel.price li:first').toggle();
     });
 
     $('.btn-mushrooms').click(function(event) {
         mushrooms.toggle();
         $(this).toggleClass('active');
+        $('.panel.price li:nth-child(2)').toggle();
     });
 
     $('.btn-green-peppers').click(function(event) {
         greenPeppers.toggle();
         $(this).toggleClass('active');
+        $('.panel.price li:nth-child(3)').toggle();
     });
 
     $('.btn-sauce').click(function(event) {
         sauce.toggleClass('sauce-white');
         $(this).toggleClass('active');
+        $('.panel.price li:nth-child(4)').toggle();
 
     });
 
     $('.btn-crust').click(function(event) {
         crust.toggleClass('crust-gluten-free');
         $(this).toggleClass('active');
+        $('.panel.price li:nth-child(5)').toggle();
     });
 
 
