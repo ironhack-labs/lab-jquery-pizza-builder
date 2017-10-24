f1092f22561b02d591b623802745b7c825d895bb
Angel Sirodey Alonso <xiro@MacBook-Pro-de-Angel.local>
2017-10-24 13:09:27 +0200

    second iteration done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -4,6 +4,11 @@
 var btnMushroom = $('.btn-mushrooms');
 var btnGreenPepper = $('.btn-green-peppers');
 var btnPepperonni = $('.btn-pepperonni');
+var btnSauce = $('.btn-sauce');
+var btnCrust =$('.btn-crust');
+
+$('#pizza .crust').removeClass('crust-gluten-free');
+$('#pizza .crust .sauce').removeClass('sauce-white');
 
 btnGreenPepper.click(function(){
     $('#pizza .green-pepper').toggle('green-pepper')
@@ -15,3 +20,21 @@ btnMushroom.click(function(){
 btnPepperonni.click(function(){
     $('#pizza .pep').toggle('pep')
 })
+
+
+btnSauce.click(function(){
+    $('#pizza .sauce').toggle('sauce-white')
+})
+
+btnCrust.click(function(){
+
+    if($('#pizza .crust').hasClass('crust-gluten-free')){
+        $('#pizza .crust').removeClass('crust-gluten-free');
+    } else {
+        $('#pizza .crust').addClass('crust-gluten-free');
+    }
+    //toggle not working
+    // $('#pizza .crust').toggle('crust-gluten-free')
+})
+
+
