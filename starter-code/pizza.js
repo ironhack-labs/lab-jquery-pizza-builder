e4dfa2cd4e08e4ba0cebe73ac960ee0b86fdb725
Angel Sirodey Alonso <xiro@MacBook-Pro-de-Angel.local>
2017-10-24 12:44:49 +0200

    first iteration done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,17 @@
 // Write your Pizza Builder JavaScript in this file.
+
+
+var btnMushroom = $('.btn-mushrooms');
+var btnGreenPepper = $('.btn-green-peppers');
+var btnPepperonni = $('.btn-pepperonni');
+
+btnGreenPepper.click(function(){
+    $('#pizza .green-pepper').toggle('green-pepper')
+})
+btnMushroom.click(function(){
+    $('#pizza .mushroom').toggle('mushroom')
+})
+
+btnPepperonni.click(function(){
+    $('#pizza .pep').toggle('pep')
+})
