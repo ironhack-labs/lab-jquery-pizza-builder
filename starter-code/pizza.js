c7659216e35231826251e58506f7b0cc95ce2278
Juan Vallejo-Nagera <juanvallejo-nagera@Juans-MacBook-Pro.local>
2017-10-24 16:53:27 +0200

    Iteration 4

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,46 +1,35 @@
 $(document).ready(function(){
 
-var pimiento = $("section").hasClass('green-pepper');
+  $(".crust").removeClass("crust-gluten-free");
+  $(".sauce").removeClass("sauce-white");
+
+  $(".btn-pepperonni").click(function(){
+  $(".pep").toggle();
+  $(".btn-pepperonni").toggleClass('active');
+  $(".panel price ul li:first").toggle();
+  });
+
+  $(".btn-green-peppers").click(function(){
+  $(".green-pepper").toggle();
+  $(".btn-green-peppers").toggleClass('active');
+  });
+
+  $(".btn-mushrooms").click(function(){
+  $(".mushroom").toggle();
+  $(".btn-mushrooms").toggleClass('active');
+  });
+
+  $(".btn-crust").click(function(){
+  $(".crust").toggleClass('crust-gluten-free');
+  $(".btn-crust").toggleClass('active');
+  });
+
+  $(".btn-sauce").click(function(){
+  $(".sauce").toggleClass('sauce-white');
+  $(".btn-sauce").toggleClass('active');
+  });
 
 
-if(pimiento == true){
-      $(".btn-green-peppers").click(function(){
-      $("[class^='green-pepper']").css({"display": "none"});
-      });
-    }
-if(pimiento == false){
-      $(".btn-green-peppers").click(function(){
-      $("[class^='green-pepper']").css({"display": "block"});
-      });
-    }
-
-
-  // $(".btn-green-peppers").click(function(){
-  // $("[class^='green-pepper']").css({"display": "none"});
-  // });
-
-
-
-
-
-
-
-
-
-
-
-
-// $("#menu ul li:first").addClass("selected");
-// $(".container article:nth-child(2)").addClass("middle");
-// $(".list-item:odd").addClass("highlighted");
-// $("#list-container list-item:last").css({"border":"1px solid #eee"});
-//
- // $("#form-container input").focus(function(){
- //   $(this).css({"background-color": "green"});
- // });
-//  $("#form-container input").blur(function(){
-//    $(this).css({"background-color": ""});
-//  });
 
 
 
