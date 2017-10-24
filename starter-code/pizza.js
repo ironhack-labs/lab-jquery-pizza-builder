c3c6163ccb2f6e57cac01ce68439b3dc2c86d798
Clementina <mataclemy@gmail.com>
2017-10-24 13:11:50 +0200

    button one function

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,27 @@
+$(document).ready(function() {
+
+  var mush = $(".btn-mushrooms");
+  mush.click(function(){
+    $('#pizza .mushroom').toggle('mushroom');
+  });
+
+
+
+    var greenp = $('btn-green-peppers');
+    greenp.click (function (){
+        $('#pizza .green-pepper').toggle(green-pepper)
+    });
+
+});
+
+// $( "#dataTable tbody tr" ).on( "click", function() {
+//   console.log( $( this ).text() );
+// });
+
+
+
+
+
+
+
 // Write your Pizza Builder JavaScript in this file.
