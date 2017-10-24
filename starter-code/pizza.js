dd702f19726695c8e61782e23bc0de09b4ed6911
ventdlnord <ventdlnord@gmail.com>
2017-10-24 16:20:21 +0200

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,42 @@
 // Write your Pizza Builder JavaScript in this file.
+'use strict';
+//$(document).ready(main) tambe per começar
+
+function main () {
+
+  $().removeClass(["sauce-white"]);
+
+  $(".btn-pepperonni").on("click", function(){
+      $(this).toggleClass("active");
+      $(".pep").toggle();
+  });
+
+  $(".btn-mushrooms").click(function(){
+    $(this).toggleClass("active");
+    $(".mushroom").toggle();
+  });
+
+  $(".btn-green-peppers").click(function(){
+    $(this).toggleClass("active");
+    $(".green-pepper").toggle();
+  });
+
+
+/* WE WERE TRYING TO REMOVE THE WHITE
+SAUCE AS DEFAULT
+
+
+  $(".btn-sauce").click(function(){
+    $(this).toggleClass("active");
+    $(".sauce-white").toggle();
+  });
+
+  $(".crust").click(function(){
+    $(this).toggleClass("active");
+    $(".crust-gluten-free").toggle();
+  });
+
+}
+*/
+
+$(document).ready(main);
