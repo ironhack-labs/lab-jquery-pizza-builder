babcbbe7e5dfea95f6cd21e65b976306b4d589ca
Riftland <fran.mscr@gmail.com>
2017-10-24 16:40:17 +0200

    Iteration 3 Done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -3,17 +3,37 @@
 $(function(){
 
   //Hide toppings
-  $(".green-pepper").css({"visibility":"hidden"});
-  $(".mushroom").css({"visibility":"hidden"});
-  $(".pep").css({"visibility":"hidden"});
+  $(".green-pepper").toggle();
+  $(".mushroom").toggle();
+  $(".pep").toggle();
+
+  //Remove sauce and crust
+  $('.btn-sauce').click(function(){
+    $('.sauce').toggleClass('sauce-white');
+    $(this).toggleClass("active");
+    //$('.price ul li:nth-child(5)').toggle();
+   });
+  $('.btn-crust').click(function(){
+    $('.crust').toggleClass('crust-gluten-free');
+    $(this).toggleClass("active");
+    //$('.price ul li:nth-child(5)').toggle();
+   });
 
 
   //Show toppings
   $(".btn-pepperonni").click(function(){
-    $(".pep").each(function(e){
-      $(this).text().css({"visibility":"visible"});
-      console.log("Soy el " + e);
-    });
+    $(".pep").toggle();
+    $(this).toggleClass("active");
   });
+  $(".btn-mushrooms").click(function(){
+    $(".mushroom").toggle();
+    $(this).toggleClass("active");
+  });
+  $(".btn-green-peppers").click(function(){
+    $(".green-pepper").toggle();
+    $(this).toggleClass("active");
+  });
+
+
 
 });
