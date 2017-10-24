b4231fd4c8970adadfcf34ae1f9b649fd2087b1f
Manuel Ortega <coolhunting@MacBook-Pro-de-Manuel.local>
2017-10-24 13:10:42 +0200

    iteracion 1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,25 @@
 // Write your Pizza Builder JavaScript in this file.
+$( document ).ready(function() {
+
+var btnPepperonni = $( ".btn-pepperonni" );
+
+   btnPepperonni.on( "click", function() {
+     $(this).toggleClass("active");
+
+var pepperonni = $( ".pep" );
+
+  if (btnPepperonni.hasClass( "active" )){
+     pepperonni.addClass( "pepito" );
+     pepperonni.removeClass( "pep" );
+
+  } else {
+
+    pepperonni.addClass( "pep" );
+    pepperonni.removeClass( "pepito" );
+
+  }
+
+  });
+
+
+});
