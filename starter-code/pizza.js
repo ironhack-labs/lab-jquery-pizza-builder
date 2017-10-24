babaa707a8b15b7388c2e64c2b7af50c1ba0f0df
Abraham Castro <cucharadepalo@gmail.com>
2017-10-24 11:59:19 +0200

    First iteration

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,20 @@
 // Write your Pizza Builder JavaScript in this file.
+function init() {
+  //Iteration 1: Add and remove toppings
+  // There are five buttons on the left of the pizza builder. Three of those have to add or remove toppings from the pizza. Write the JavaScript necessary for those three buttons to add and remove pepperonni, mushrooms and green peppers from the pizza. Don't worry about updating the price. We will do it later.
+  var pepperonnis = $('.pep');
+  var peppers = $('.green-pepper');
+  var mushrooms = $('.mushroom');
+  $('.green-pepper, .mushroom, .pep').css('display', 'none');
+  $('.btn-pepperonni').click(function (){
+    pepperonnis.css('display', 'block');
+  });
+  $('.btn-mushrooms').click(function (){
+    mushrooms.css('display', 'block');
+  });
+  $('.btn-green-peppers').click(function (){
+    peppers.css('display', 'block');
+  });
+}
+
+$(document).ready(init);
