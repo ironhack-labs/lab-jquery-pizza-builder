bd485a620a0c28322490f997af09da6eb0edfbf2
Rocio Garcia <gooblyn@gmail.com>
2017-10-24 17:19:31 +0200

    Finished

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,5 +1,10 @@
 // Write your Pizza Builder JavaScript in this file.
 function init() {
+  // Setting the initial price of the pizza
+  $("strong").html("$13");
+  var price = $("strong").html();
+  price = price.substr(1);
+
 // Remove the extra ingredients (cheese & gluten-free crust)
   // White sauce - remove picture and desactive button
   $(".sauce-white").toggle();
@@ -17,42 +22,74 @@ function init() {
   $(".btn-pepperonni").click(function() {
     if ($(".pep").attr("style")) {
       $(".pep").css("display","");
+      price = price + 1;
     }
     else {
       $(".pep").css("display","none");
+      price = price-1;
     }
     $(".btn-pepperonni").toggleClass("active");
     $(".price ul li:nth-child(1)").toggle();
+    $("strong").html("$"+price);
   });
 
   // Add or remove mushrooms
   $(".btn-mushrooms").click(function(){
-    $(".mushroom").toggle();
+    if ($(".mushroom").attr("style")) {
+      $(".mushroom").css("display","");
+      price = price + 1;
+    }
+    else {
+      $(".mushroom").css("display","none");
+      price = price-1;
+    }
     $(".btn-mushrooms").toggleClass("active");
     $(".price ul li:nth-child(2)").toggle();
+    $("strong").html("$"+price);
   });
 
   // Add or remove green-pepper
   $(".btn-green-peppers").click(function(){
-    $(".green-pepper").toggle();
+    if ($(".green-pepper").attr("style")) {
+      $(".green-pepper").css("display","");
+      price = price + 1;
+    }
+    else {
+      $(".green-pepper").css("display","none");
+      price = price-1;
+    }
     $(".btn-green-peppers").toggleClass("active");
     $(".price ul li:nth-child(3)").toggle();
+    $("strong").html("$"+price);
   });
 
   // select white sauce and gluten free crust
   $(".btn-sauce").click(function(){
-    $(".sauce-white").toggle();
+    if ($(".sauce-white").attr("style")) {
+      $(".sauce-white").css("display","");
+      price = price + 3;
+    }
+    else {
+      $(".sauce-white").css("display","none");
+      price = price-3;
+    }
     $(".btn-sauce").toggleClass("active");
+    $(".price ul li:nth-child(4)").toggle();
+    $("strong").html("$"+price);
   });
 
   $(".btn-crust").click(function(){
     if ($(".btn-crust").hasClass("active")){
       $(".crust").removeClass("crust-gluten-free");
+      price = price-5;
     }
     else {
       $(".crust").addClass("crust-gluten-free");
+      price = price +5;
     }
     $(".btn-crust").toggleClass("active");
+    $(".price ul li:nth-child(5)").toggle();
+    $("strong").html("$"+price);
   });
 }
 
