240955506ee18c2cb05448d947623171e05b900e
Rocio Garcia <gooblyn@gmail.com>
2017-10-24 16:10:08 +0200

    Fouth iteration finished

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,5 +1,18 @@
 // Write your Pizza Builder JavaScript in this file.
 function init() {
+// Remove the extra ingredients (cheese & gluten-free crust)
+  // White sauce - remove picture and desactive button
+  $(".sauce-white").toggle();
+  $(".btn-sauce").toggleClass("active");
+  // Remove ul items from the list
+  $(".price ul li:nth-child(4)").toggle();
+
+  // Gluten-free crust - remove picture and desactive button
+  $(".crust-gluten-free").removeClass("crust-gluten-free");
+  $(".btn-crust").toggleClass("active");
+  // Remove ul items from the list
+  $(".price ul li:nth-child(5)").toggle();
+
   // Add or remove pepperoni
   $(".btn-pepperonni").click(function() {
     if ($(".pep").attr("style")) {
@@ -9,26 +22,36 @@ function init() {
       $(".pep").css("display","none");
     }
     $(".btn-pepperonni").toggleClass("active");
+    $(".price ul li:nth-child(1)").toggle();
   });
+
   // Add or remove mushrooms
   $(".btn-mushrooms").click(function(){
     $(".mushroom").toggle();
     $(".btn-mushrooms").toggleClass("active");
+    $(".price ul li:nth-child(2)").toggle();
   });
+
   // Add or remove green-pepper
   $(".btn-green-peppers").click(function(){
     $(".green-pepper").toggle();
     $(".btn-green-peppers").toggleClass("active");
+    $(".price ul li:nth-child(3)").toggle();
   });
 
-  // select white sauce adn gluten free crust
+  // select white sauce and gluten free crust
   $(".btn-sauce").click(function(){
     $(".sauce-white").toggle();
     $(".btn-sauce").toggleClass("active");
   });
 
   $(".btn-crust").click(function(){
-    $(".cheese").toggle();
+    if ($(".btn-crust").hasClass("active")){
+      $(".crust").removeClass("crust-gluten-free");
+    }
+    else {
+      $(".crust").addClass("crust-gluten-free");
+    }
     $(".btn-crust").toggleClass("active");
   });
 }
