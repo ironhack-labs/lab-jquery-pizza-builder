15b2a29aa82a6cdca75451c7952b22d59cfcf6c0
anajules94 <32312980+anajules94@users.noreply.github.com>
2017-10-24 16:41:12 +0200

    done 1 y 2 (button sauce y crust al reves)

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,18 +1,52 @@
+//1st iteration
 function init() {
-$(".pep").css({"display":"none"});
 $( ".btn-pepperonni" ).on( "click", function() {
-  $('.pep').css({"display":"inline"});
+  $(".pep").toggle();
 });
-$(".mushroom").css({"display":"none"});
 $( ".btn-mushrooms" ).on( "click", function() {
-  $('.mushroom').css({"display":"inline"});
+  $('.mushroom').toggle();
 });
-$(".green-pepper").css({"display":"none"});
 $( ".btn-green-peppers" ).on( "click", function() {
-  $('.green-pepper').css({"display":"inline"});
+  $('.green-pepper').toggle();
 });
-};
+//2nd Iteration
+$(".crust").removeClass("crust-gluten-free");
+$(".btn-crust").on( "click", function() {
+$('.crust').toggleClass("crust-gluten-free");
+});
 
+$(".sauce").removeClass("sauce-white");
+$(".btn-sauce").on( "click", function() {
+
+$('.sauce').toggleClass("sauce-white");
+});
+
+$(".btn-pepperonni").removeClass("active");
+$(".btn-pepperonni").on( "click", function() {
+  $('.btn-pepperonni').toggleClass("active");
+});
+  $(".btn-mushrooms").removeClass("active");
+  $(".btn-mushrooms").on( "click", function() {
+    $('.btn-mushrooms').toggleClass("active");
+});
+    $(".btn-green-peppers").removeClass("active");
+    $(".btn-green-peppers").on( "click", function() {
+    $('.btn-green-peppers').toggleClass("active");
+
+});
+$(".btn-crust").removeClass("active");
+$(".btn-crust").on( "click", function() {
+$('.btn-crust').toggleClass("active");
+
+});
+$(".btn-sauce").removeClass("active");
+$(".btn-sauce").on( "click", function() {
+$('.btn-sauce').toggleClass("active");
+
+});
+//iteration 3
+
+}
 $(document).ready(function() {
   init();
-})
+});
