5398041ad1c0e74c84f46e506ebe8995761ecccb
Rocio Garcia <gooblyn@gmail.com>
2017-10-24 15:31:37 +0200

    Second iteration finished

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,7 +1,6 @@
 // Write your Pizza Builder JavaScript in this file.
 function init() {
   // Add or remove pepperoni
-  // Check the pepperoni button status
   $(".btn-pepperonni").click(function() {
     if ($(".pep").attr("style")) {
       $(".pep").css("display","");
@@ -9,14 +8,28 @@ function init() {
     else {
       $(".pep").css("display","none");
     }
+    $(".btn-pepperonni").toggleClass("active");
   });
-
+  // Add or remove mushrooms
   $(".btn-mushrooms").click(function(){
     $(".mushroom").toggle();
+    $(".btn-mushrooms").toggleClass("active");
   });
-
+  // Add or remove green-pepper
   $(".btn-green-peppers").click(function(){
     $(".green-pepper").toggle();
+    $(".btn-green-peppers").toggleClass("active");
+  });
+
+  // select white sauce adn gluten free crust
+  $(".btn-sauce").click(function(){
+    $(".sauce-white").toggle();
+    $(".btn-sauce").toggleClass("active");
+  });
+
+  $(".btn-crust").click(function(){
+    $(".cheese").toggle();
+    $(".btn-crust").toggleClass("active");
   });
 }
 
