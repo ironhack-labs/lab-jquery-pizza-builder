3f0299b1d9d99c37514e5441e4b151c04ca36c09
popelini <ccastillon92@gmail.com>
2017-10-24 14:33:44 +0200

    Finished and complete

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -4,37 +4,46 @@ function init() {
   $(".btn-pepperonni").click(function() {
     if($(".pep").attr("style")) {
       $(".pep").css("display", "");
-      $(".btn-pepperonni").addClass("active");
-      $(".price ul li:first").css("display", "");
+      $(this).addClass("active");
+      $(".price ul li:first").show(500);
+      sum += pep;
     } else {
       $(".pep").css("display", "none");
-      $(".btn-pepperonni").removeClass("active");
-      $(".price ul li:first").css("display", "none");
-    }
+      $(this).removeClass("active");
+      $(".price ul li:first").hide(500);
+      sum -= pep;
+    }total.html("$" + (sum))
+
   });
 
   $(".btn-mushrooms").click(function() {
     if($(".mushroom").attr("style")) {
       $(".mushroom").css("display", "");
-      $(".btn-mushrooms").addClass("active");
-      $(".price ul li:nth-child(2)").css("display", "");
+      $(this).addClass("active");
+      $(".price ul li:nth-child(2)").show(500);
+      sum += mush;
     } else {
       $(".mushroom").css("display", "none");
-      $(".btn-mushrooms").removeClass("active");
-      $(".price ul li:nth-child(2)").css("display", "none");
+      $(this).removeClass("active");
+      $(".price ul li:nth-child(2)").hide(500);
+      sum -= mush;
     }
+    total.html("$" + (sum))
   });
 
   $(".btn-green-peppers").click(function() {
     if($(".green-pepper").attr("style")) {
       $(".green-pepper").css("display", "");
-      $(".btn-green-peppers").addClass("active");
-      $(".price ul li:nth-child(3)").css("display", "");
+      $(this).addClass("active");
+      $(".price ul li:nth-child(3)").show(500);
+      sum += green;
     } else {
       $(".green-pepper").css("display", "none");
-      $(".btn-green-peppers").removeClass("active");
-      $(".price ul li:nth-child(3)").css("display", "none");
+      $(this).removeClass("active");
+      $(".price ul li:nth-child(3)").hide(500);
+      sum -= green;
     }
+    total.html("$" + (sum))
   });
 
   //ITERATION 2
@@ -45,25 +54,31 @@ function init() {
   $(".btn-crust").click(function() {
     if($(".crust").hasClass("crust-gluten-free")) {
       $(".crust").removeClass("crust-gluten-free");
-      $(".btn-crust").removeClass("active");
-      $(".price ul li:nth-child(5)").css("display", "none");
+      $(this).removeClass("active");
+      $(".price ul li:nth-child(5)").hide(500);
+      sum-= glutCrust;
     } else {
       $(".crust").addClass("crust-gluten-free");
-      $(".btn-crust").addClass("active");
-      $(".price ul li:nth-child(5)").css("display", "");
+      $(this).addClass("active");
+      $(".price ul li:nth-child(5)").show(500);
+      sum+= glutCrust;
     }
+    total.html("$" + (sum));
   });
 
   $(".btn-sauce").click(function() {
     if($(".sauce").hasClass("sauce-white")) {
       $(".sauce").removeClass("sauce-white");
-      $(".btn-sauce").removeClass("active");
-      $(".price ul li:nth-child(4)").css("display", "none");
+      $(this).removeClass("active");
+      $(".price ul li:nth-child(4)").hide(500);
+      sum -= white;
     } else {
       $(".sauce").addClass("sauce-white");
-      $(".btn-sauce").addClass("active");
-      $(".price ul li:nth-child(4)").css("display", "");
+      $(this).addClass("active");
+      $(".price ul li:nth-child(4)").show(500);
+      sum += white;
     }
+    total.html("$" + (sum));
   });
 
   //ITERATION 3
@@ -79,7 +94,15 @@ function init() {
   // The list items iterations are inside the click event handlers.
 
   //ITERATION 5
-  
+  var pep = 1;
+  var mush = 1;
+  var green = 1;
+  var white = 3;
+  var glutCrust = 5;
+  var sum = 10 +(pep+mush+green);
+  var total = $(".price strong");
+
+  total.html("$" + (sum));
 }
 
 $(document).ready(init());
