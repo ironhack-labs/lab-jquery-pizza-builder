eb8a2d2385e54bf473aa0403701aef6766fe01d2
Dominik Lasch <dl@exutec.de>
2017-10-24 16:26:33 +0200

    done until 5.

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,51 @@
 // Write your Pizza Builder JavaScript in this file.
+
+function main() {
+
+
+  function bindIngredientClick(buttonSelector, ingridientSelector, ingridient) {
+    $(buttonSelector).click(function() {
+      $(ingridientSelector).toggle();
+      $(buttonSelector).toggleClass('active');
+      $("li:contains('" + ingridient + "')").toggle();
+    });
+  }
+
+  bindIngredientClick('.btn-pepperonni', '.pep', 'pepperonni');
+
+  $('.btn-green-peppers').click(function() {
+    $('.green-pepper').toggle();
+    $('.btn-green-peppers').toggleClass('active');
+    $("li:contains('green peppers')").toggle();
+  });
+
+  $('.btn-mushrooms').click(function() {
+    $('.cap').toggle();
+    $('.stem').toggle();
+    $('.btn-mushrooms').toggleClass('active');
+    $("li:contains('mushrooms')").toggle();
+  });
+
+  $('.sauce-white').attr('class', 'sauce');
+
+  $('.btn-sauce').click(function() {
+    $('.sauce').toggleClass('sauce-white');
+    $('.btn-sauce').toggleClass('active');
+    $("li:contains('white sauce')").toggle();
+  });
+
+  $('.crust-gluten-free').attr('class', 'crust');
+
+  $('.btn-crust').click(function() {
+    $('.crust').toggleClass('crust-gluten-free');
+    $('.btn-crust').toggleClass('active');
+    $("li:contains('gluten-free crust')").toggle();
+  });
+
+
+}
+
+
+
+
+$(main);
