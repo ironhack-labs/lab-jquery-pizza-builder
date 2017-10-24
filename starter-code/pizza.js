1aededac011e56af152a9c63081b19ab8ef41571
Riftland <fran.mscr@gmail.com>
2017-10-24 13:11:48 +0200

    Iteration 1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,19 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(function(){
+
+  //Hide toppings
+  $(".green-pepper").css({"visibility":"hidden"});
+  $(".mushroom").css({"visibility":"hidden"});
+  $(".pep").css({"visibility":"hidden"});
+
+
+  //Show toppings
+  $(".btn-pepperonni").click(function(){
+    $(".pep").each(function(e){
+      $(this).text().css({"visibility":"visible"});
+      console.log("Soy el " + e);
+    });
+  });
+
+});
