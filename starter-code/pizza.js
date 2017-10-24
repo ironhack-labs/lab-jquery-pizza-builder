bd2dac96c0505f0a65c326b0a08bc5f4087b4cb9
sdediego <sergio.dediego@outlook.com>
2017-10-24 12:33:55 +0200

    Iteracion 1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,23 @@
 // Write your Pizza Builder JavaScript in this file.
+function init() {
+
+    var peps = $('.pep');
+    var mushrooms = $('.mushroom');
+    var greenPeppers = $('.green-pepper');
+
+    $('.btn-pepperonni').click(function(event) {
+        peps.toggle();
+    });
+
+    $('.btn-mushrooms').click(function(event) {
+        mushrooms.toggle();
+    });
+
+    $('.btn-green-peppers').click(function(event) {
+        greenPeppers.toggle();
+    });
+
+
+}
+
+$(document).ready(init);
