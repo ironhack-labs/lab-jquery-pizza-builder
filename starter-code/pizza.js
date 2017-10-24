54be9228383394dce520e90276520627795c2ec8
Manuel Ortega <coolhunting@MacBook-Pro-de-Manuel.local>
2017-10-24 16:56:34 +0200

    Completadas iteraciones 1 y 2

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,6 +1,7 @@
 // Write your Pizza Builder JavaScript in this file.
 $( document ).ready(function() {
 
+// ITERACION 1
 var btnPepperonni = $( ".btn-pepperonni" );
 
    btnPepperonni.on( "click", function() {
@@ -9,17 +10,79 @@ var btnPepperonni = $( ".btn-pepperonni" );
 var pepperonni = $( ".pep" );
 
   if (btnPepperonni.hasClass( "active" )){
-     pepperonni.addClass( "pepito" );
-     pepperonni.removeClass( "pep" );
+     pepperonni.css ( "z-index", 0);
 
   } else {
-
-    pepperonni.addClass( "pep" );
-    pepperonni.removeClass( "pepito" );
+    pepperonni.css ( "z-index", 15);
 
   }
+  });
 
+  var btnMushroom = $( ".btn-mushrooms" );
+
+  btnMushroom.on( "click", function() {
+       $(this).toggleClass("active");
+
+    var mushroom = $( ".mushroom" );
+
+    if (btnMushroom.hasClass ("active")) {
+      mushroom.css ("display", "none");
+    } else {
+      mushroom.css ("display", "block");
+    }
   });
 
 
+  var btnGreenPeppers = $( ".btn-green-peppers" );
+
+  btnGreenPeppers.on( "click", function() {
+       $(this).toggleClass("active");
+
+    var greenPeppers = $( ".green-pepper" );
+
+    if (btnGreenPeppers.hasClass ("active")) {
+      greenPeppers.css ("display", "none");
+    } else {
+      greenPeppers.css ("display", "block");
+    }
+  });
+
+// ITERACION 2
+
+var btnSauce = $( ".btn-sauce" );
+
+
+btnSauce.on( "click", function() {
+     $(this).toggleClass("active");
+
+var sauce = $(".sauce-white");
+
+  if (btnSauce.hasClass ("active")) {
+    sauce.css ("background", "inherit");
+
+  } else {
+    sauce.css ("background", "#fff4aa");
+
+  }
 });
+
+var btnCrust = $( ".btn-crust" );
+
+
+btnCrust.on( "click", function() {
+     $(this).toggleClass("active");
+
+var crust = $(".crust-gluten-free");
+
+  if (btnCrust.hasClass ("active")) {
+    crust.css ("background-color", "inherit");
+
+  } else {
+    crust.css ("background-color", "#e0d5cf");
+
+  }
+});
+
+
+
+  });
