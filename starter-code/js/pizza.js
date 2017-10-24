b98db7f34840da7b6d924ac60392ef000e4a5b67
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-10-24 13:12:30 +0200

    pizza buttons added

--- starter-code/js/pizza.js
+++ starter-code/js/pizza.js
@@ -1,1 +1,15 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+  var pepperoni = $(".pep");
+  var greenPepper = $(".green-pepper");
+  var mushroom = $(".mushroom");
+  $('.btn-pepperonni').click(function(){
+    pepperoni.toggleClass('pep');
+  });
+  $('.btn-mushrooms').click(function(){
+    mushroom.toggleClass('mushroom');
+  });
+  $('.btn-green-peppers').click(function(){
+    greenPepper.toggleClass('green-pepper');
+  });
+});
