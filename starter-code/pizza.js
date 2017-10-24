ca78a288208bb495345adf59039ebd60d116aa5d
sdediego <sergio.dediego@outlook.com>
2017-10-24 12:44:33 +0200

    Iteracion 2 completa

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -4,6 +4,11 @@ function init() {
     var peps = $('.pep');
     var mushrooms = $('.mushroom');
     var greenPeppers = $('.green-pepper');
+    var sauce = $('.sauce');
+    var crust = $('.crust');
+
+    sauce.removeClass('sauce-white');
+    crust.removeClass('crust-gluten-free');
 
     $('.btn-pepperonni').click(function(event) {
         peps.toggle();
@@ -15,6 +20,14 @@ function init() {
 
     $('.btn-green-peppers').click(function(event) {
         greenPeppers.toggle();
+    });
+
+    $('.btn-sauce').click(function(event) {
+        sauce.toggleClass('sauce-white');
+    });
+
+    $('.btn-crust').click(function(event) {
+        crust.toggleClass('crust-gluten-free');
     });
 
 
