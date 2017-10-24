1f1e620eda74f15f622a16c1d52a1b00e69ca1bd
Asier Cisneros <ascisneros14@gmail.com>
2017-10-24 15:51:00 +0200

    first iteraction done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,13 +1,15 @@
 // Write your Pizza Builder JavaScript in this file.
-function init() {
-$(".btn").toggle(function() {
-  if($(".pep").attr("style")){
-    $(".pep").css("display", "");
-  }else{
-    $(".pep").css("display", "none");
-  }
-});
-// $(".btn .btn-pepperonni:active").removeClass("green-pepper two");
-}
+$(document).ready(function() {
 
-$(document).ready(init);
+  $(".btn-pepperonni").click(function(){
+      $(".pep").toggle();
+  });
+
+  $(".btn-mushrooms").click(function(){
+      $(".mushroom").toggle();
+  });
+
+  $(".btn-green-peppers").click(function(){
+      $(".green-pepper").toggle();
+  });
+});
