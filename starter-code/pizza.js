397add7a8bbf392bfe3da1dc1faa526cf262265b
anajules94 <32312980+anajules94@users.noreply.github.com>
2017-10-24 13:11:47 +0200

    iteration 1 done (need to make ingridients disapear)

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,18 @@
-// Write your Pizza Builder JavaScript in this file.
+function init() {
+$(".pep").css({"display":"none"});
+$( ".btn-pepperonni" ).on( "click", function() {
+  $('.pep').css({"display":"inline"});
+});
+$(".mushroom").css({"display":"none"});
+$( ".btn-mushrooms" ).on( "click", function() {
+  $('.mushroom').css({"display":"inline"});
+});
+$(".green-pepper").css({"display":"none"});
+$( ".btn-green-peppers" ).on( "click", function() {
+  $('.green-pepper').css({"display":"inline"});
+});
+};
+
+$(document).ready(function() {
+  init();
+})
