d20540464bf1b45384e68d56fff80f50c2355452
Alejandro Roman <aromanarguello@gmail.com>
2017-10-26 17:42:42 -0400

    add default red sauce and regular crust + toggle those classes

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -13,4 +13,13 @@ $(document).ready( function () {
     $('.green-pepper').toggle();
   });
 
+  $('.sauce').removeClass('sauce-white');
+  $('.btn-sauce').click( function () {
+    $('.sauce').toggleClass('sauce-white');
+  });
+  $('.crust').removeClass('crust-gluten-free');
+  $('.btn-crust').click( function () {
+    $('.crust').toggleClass('crust-gluten-free');
+  });
+
 });
