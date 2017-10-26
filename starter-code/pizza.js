bdbb4e3b0a05c7d197b0bcfe29e520ec16b78513
Alex Del Valle <amd407@gmail.com>
2017-10-26 18:17:38 -0400

    Completed Iteration 1, 2 and part of 3

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,34 @@
-// Write your Pizza Builder JavaScript in this file.
+
+$(document).ready(function() {
+    $('.btn-pepperonni').click(function() {
+        $('.pep').slideToggle(3000);
+    });
+    $('.btn-mushrooms').click(function() {
+        $('.mushroom').slideToggle(3000);
+    });
+    $('.btn-green-peppers').click(function() {
+        $('.green-pepper').slideToggle(3000);
+    });
+});
+
+
+$(document).ready(function() {
+    var mySauce = $('.sauce');
+    var sauceBtn = $('.btn-sauce');
+    var myCrust = $('.crust');
+    var crustBtn = $('.btn-crust');
+    mySauce.removeClass('sauce-white');
+    sauceBtn.removeClass('active');
+    myCrust.removeClass('crust-gluten-free');
+    crustBtn.removeClass('active');
+
+    sauceBtn.click(function() {
+      sauceBtn.toggleClass('active');
+      mySauce.toggleClass('sauce-white');
+    });
+
+    crustBtn.click(function() {
+      crustBtn.toggleClass('active');
+      myCrust.toggleClass('crust-gluten-free');
+    });
+  });
