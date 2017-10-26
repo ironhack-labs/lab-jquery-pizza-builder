f22178bc2c83d7561517cb0035524a2f96d5fa26
Alex Del Valle <amd407@gmail.com>
2017-10-26 19:27:20 -0400

    Added price list functionality for show/hide

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,51 +1,66 @@
 $(document).ready(function() {
     var roniButton = $('.btn-pepperonni');
     var myRoni = $('.pep');
+    var roniPrice = $('.price li:first-child');
+
     roniButton.removeClass('active');
-    myRoni.removeClass('active');
     myRoni.hide();
+    roniPrice.hide();
     roniButton.click(function(){
       myRoni.fadeToggle();
       roniButton.toggleClass('active');
+      roniPrice.toggle();
     });
 
     var mushroomButton = $('.btn-mushrooms');
     var myMushroom = $('.mushroom');
+    var mushroomPrice = $('.price li:nth-child(2)');
+
     mushroomButton.removeClass('active');
     myMushroom.hide();
+    mushroomPrice.hide();
     mushroomButton.click(function(){
       myMushroom.fadeToggle();
       mushroomButton.toggleClass('active');
+      mushroomPrice.toggle();
     });
-    
+
     var greenButton = $('.btn-green-peppers');
     var greenPeppers = $('.green-pepper');
+    var greenPrice = $('.price li:nth-child(3)');
+
     greenButton.removeClass('active');
     greenPeppers.hide();
+    greenPrice.hide();
     greenButton.click(function(){
       greenPeppers.fadeToggle();
       greenButton.toggleClass('active');
+      greenPrice.toggle();
+    });
+
+    var mySauce = $('.sauce');
+    var sauceButton = $('.btn-sauce');
+    var saucePrice = $('.price li:nth-child(4)');
+
+    mySauce.removeClass('sauce-white');
+    sauceButton.removeClass('active');
+    saucePrice.hide();
+    sauceButton.click(function(){
+      mySauce.toggleClass('sauce-white');
+      sauceButton.toggleClass('active');
+      saucePrice.toggle();
+    });
+
+    var myCrust = $('.crust');
+    var crustButton = $('.btn-crust');
+    var crustPrice = $('.price li:nth-child(5)');
+
+    myCrust.removeClass('crust-gluten-free');
+    crustButton.removeClass('active');
+    crustPrice.hide();
+    crustButton.click(function(){
+      myCrust.toggleClass('crust-gluten-free');
+      crustButton.toggleClass('active');
+      crustPrice.toggle();
     });
 });
-
-
-$(document).ready(function() {
-    var mySauce = $('.sauce');
-    var sauceBtn = $('.btn-sauce');
-    var myCrust = $('.crust');
-    var crustBtn = $('.btn-crust');
-    mySauce.removeClass('sauce-white');
-    sauceBtn.removeClass('active');
-    myCrust.removeClass('crust-gluten-free');
-    crustBtn.removeClass('active');
-
-    sauceBtn.click(function() {
-      sauceBtn.toggleClass('active');
-      mySauce.toggleClass('sauce-white');
-    });
-
-    crustBtn.click(function() {
-      crustBtn.toggleClass('active');
-      myCrust.toggleClass('crust-gluten-free');
-    });
-  });
