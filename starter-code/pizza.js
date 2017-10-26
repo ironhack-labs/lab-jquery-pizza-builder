37f13005c7dfccf762bae1ec819347a2486568b3
Alejandro Roman <aromanarguello@gmail.com>
2017-10-26 17:47:08 -0400

    add active state to buttons

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -4,22 +4,28 @@
 $(document).ready( function () {
   $('.btn-pepperonni').click( function () {
     $('.pep').toggle();
+    $('.btn-pepperonni').toggleClass('active');
 
   });
   $('.btn-mushrooms').click( function () {
     $('.mushroom').toggle();
+    $('.btn-mushrooms').toggleClass('active');
   });
+
   $('.btn-green-peppers').click( function () {
     $('.green-pepper').toggle();
+    $('.btn-green-peppers').toggleClass('active');
   });
 
   $('.sauce').removeClass('sauce-white');
   $('.btn-sauce').click( function () {
     $('.sauce').toggleClass('sauce-white');
+    $('.btn-sauce').toggleClass('active');
   });
   $('.crust').removeClass('crust-gluten-free');
   $('.btn-crust').click( function () {
     $('.crust').toggleClass('crust-gluten-free');
+    $('.btn-crust').toggleClass('active');
   });
 
 });
