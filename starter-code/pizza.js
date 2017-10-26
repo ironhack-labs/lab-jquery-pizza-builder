a11567ffa70bd10936b2fb1a45b8caaa46886a96
Alejandro Roman <aromanarguello@gmail.com>
2017-10-26 18:04:56 -0400

    add toggle function to price list

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -5,27 +5,33 @@ $(document).ready( function () {
   $('.btn-pepperonni').click( function () {
     $('.pep').toggle();
     $('.btn-pepperonni').toggleClass('active');
+    $('.panel.price ul li:nth-child(1)').toggle();
 
   });
   $('.btn-mushrooms').click( function () {
     $('.mushroom').toggle();
     $('.btn-mushrooms').toggleClass('active');
+    $('.panel.price ul li:nth-child(2)').toggle();
   });
 
   $('.btn-green-peppers').click( function () {
     $('.green-pepper').toggle();
     $('.btn-green-peppers').toggleClass('active');
+    $('.panel.price ul li:nth-child(3)').toggle();
   });
 
   $('.sauce').removeClass('sauce-white');
   $('.btn-sauce').click( function () {
     $('.sauce').toggleClass('sauce-white');
     $('.btn-sauce').toggleClass('active');
+    $('.panel.price ul li:nth-child(4)').toggle();
   });
+
   $('.crust').removeClass('crust-gluten-free');
   $('.btn-crust').click( function () {
     $('.crust').toggleClass('crust-gluten-free');
     $('.btn-crust').toggleClass('active');
+    $('.panel.price ul li:nth-child(5)').toggle();
   });
 
 });
