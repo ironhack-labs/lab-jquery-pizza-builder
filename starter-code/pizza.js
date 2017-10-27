58bc8151ede42051cbb4e0ef53c3f56ac6acd442
Alex Del Valle <amd407@gmail.com>
2017-10-26 20:32:01 -0400

    DONE!! Added iteration 5

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+    var totalCost = 10;
+    var priceNumber = $('strong').empty();
+    priceNumber.append('$' + totalCost);
+    
+    var roniCost = 1;
+    var greenCost = 1;
+    var mushroomCost = 1;
+    var crustCost = 5;
+    var sauceCost = 3;
+
     var roniButton = $('.btn-pepperonni');
     var myRoni = $('.pep');
     var roniPrice = $('.price li:first-child');
@@ -6,11 +16,22 @@ $(document).ready(function() {
     roniButton.removeClass('active');
     myRoni.hide();
     roniPrice.hide();
+
     roniButton.click(function(){
       myRoni.fadeToggle();
       roniButton.toggleClass('active');
       roniPrice.toggle();
-    });
+
+      if (roniButton.hasClass('active')) {
+          totalCost += roniCost;
+          priceNumber.empty();
+      }
+      else {
+          totalCost -= roniCost;
+          priceNumber.empty();
+      }
+      priceNumber.append('$' + totalCost);
+  });
 
     var mushroomButton = $('.btn-mushrooms');
     var myMushroom = $('.mushroom');
@@ -19,10 +40,21 @@ $(document).ready(function() {
     mushroomButton.removeClass('active');
     myMushroom.hide();
     mushroomPrice.hide();
+
     mushroomButton.click(function(){
       myMushroom.fadeToggle();
       mushroomButton.toggleClass('active');
       mushroomPrice.toggle();
+
+      if (mushroomButton.hasClass('active')) {
+          totalCost += mushroomCost;
+          priceNumber.empty();
+      }
+      else {
+          totalCost -= mushroomCost;
+          priceNumber.empty();
+      }
+      priceNumber.append('$' + totalCost);
     });
 
     var greenButton = $('.btn-green-peppers');
@@ -32,10 +64,21 @@ $(document).ready(function() {
     greenButton.removeClass('active');
     greenPeppers.hide();
     greenPrice.hide();
+
     greenButton.click(function(){
       greenPeppers.fadeToggle();
       greenButton.toggleClass('active');
       greenPrice.toggle();
+
+      if (greenButton.hasClass('active')) {
+          totalCost += greenCost;
+          priceNumber.empty();
+      }
+      else {
+          totalCost -= greenCost;
+          priceNumber.empty();
+      }
+      priceNumber.append('$' + totalCost);
     });
 
     var mySauce = $('.sauce');
@@ -45,10 +88,21 @@ $(document).ready(function() {
     mySauce.removeClass('sauce-white');
     sauceButton.removeClass('active');
     saucePrice.hide();
+
     sauceButton.click(function(){
       mySauce.toggleClass('sauce-white');
       sauceButton.toggleClass('active');
       saucePrice.toggle();
+
+      if (sauceButton.hasClass('active')) {
+          totalCost += sauceCost;
+          priceNumber.empty();
+      }
+      else {
+          totalCost -= sauceCost;
+          priceNumber.empty();
+      }
+      priceNumber.append('$' + totalCost);
     });
 
     var myCrust = $('.crust');
@@ -58,9 +112,20 @@ $(document).ready(function() {
     myCrust.removeClass('crust-gluten-free');
     crustButton.removeClass('active');
     crustPrice.hide();
+
     crustButton.click(function(){
       myCrust.toggleClass('crust-gluten-free');
       crustButton.toggleClass('active');
       crustPrice.toggle();
+
+      if (crustButton.hasClass('active')) {
+          totalCost += crustCost;
+          priceNumber.empty();
+      }
+      else {
+          totalCost -= crustCost;
+          priceNumber.empty();
+      }
+      priceNumber.append('$' + totalCost);
     });
 });
