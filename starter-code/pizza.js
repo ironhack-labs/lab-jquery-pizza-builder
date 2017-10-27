64ac46bb733023150774c5b50f311c9c8f4090a5
Alejandro Poggi <apoggi419@gmail.com>
2017-10-27 09:02:44 -0400

    finished, except iteration 5

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,33 @@
-// Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+
+  $('.crust').toggleClass('crust-gluten-free');
+  $('.sauce').toggleClass('sauce-white');
+  $('ul:last > :last ').toggle();
+
+  $('.btn-pepperonni').click(function(){
+    $('.pep').toggle();
+    $('.btn-pepperonni').toggleClass('active');
+    $('ul:last > :first ').toggle();
+  });
+  $('.btn-mushrooms').click(function(){
+    $('.mushroom').toggle();
+    $('.btn-mushrooms').toggleClass('active');
+    $('ul:last > :nth-child(2) ').toggle();
+  });
+  $('.btn-green-peppers').click(function(){
+    $('.green-pepper').toggle();
+    $('.btn-green-peppers').toggleClass('active');
+    $('ul:last > :nth-child(3) ').toggle();
+  });
+  $('.btn-sauce').click(function(){
+    $('.sauce').toggleClass('sauce-white');
+    $('.btn-sauce').toggleClass('active');
+    $('ul:last > :nth-child(4) ').toggle();
+  });
+  $('.btn-crust').click(function(){
+    $('.crust').toggleClass('crust-gluten-free');
+    $('.btn-crust').toggleClass('active');
+    $('ul:last > :last ').toggle();
+  });
+
+});//document ready
