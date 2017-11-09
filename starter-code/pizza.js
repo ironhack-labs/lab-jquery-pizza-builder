8f04612300a1732afd0bbf717cae8f835f10529a
Antoine Marc <antoinefl_marc@yahoo.fr>
2017-11-09 12:33:34 +0100

    total fail at iteration 5....

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,11 +1,12 @@
 // Write your Pizza Builder JavaScript in this file.
 
-
+var p1 = 1
 //Three ingredients
 $(".btn-pepperonni").click(function () {
   $(".pep").toggle(".pep");
   $(this).toggleClass("active");
   $("aside ul li:nth-child(1)").toggle();
+
 });
 
 
@@ -46,41 +47,37 @@ $(".btn-sauce").click(function () {
 });
 
 
-// .replace(/[^0-9]/g,'')
-
-if ($(".btn-pepperonni").hasClass("active")) {
-  var temp1 = $("aside ul li:nth-child(1)").text();
-  var p1 = "temp1".replace(/[^0-9]/g,'');
-}
-else {
-  p1 = 0
-}
-
-if ($(".btn-mushrooms").hasClass("active")) {
-  var temp2 = $("aside ul li:nth-child(2)").text();
-  var p2 = "temp2".replace(/[^0-9]/g,'');
-}
-else {
-  p2 = 0
+function priceOne() {
+  if ($(".btn-pepperonni").hasClass("active")) {
+    var p1 = 1
+  }
+  else {
+    p1 = 0
+  }
+  return p1;
 }
 
-if ($(".btn-green-peppers").hasClass("active")) {
-  var temp3 = $("aside ul li:nth-child(3)").text();
-  var p3 = "temp3".replace(/[^0-9]/g,'');
-}
-else {
-  p3 = 0
+function priceTwo() {
+  if ($(".btn-mushrooms").hasClass("active")) {
+    var p2 = 1
+  }
+  else {
+    p2 = 0
+  }
+  return p2;
 }
 
-var tot = p1 + p2 + p3;
-$("aside strong").text("$" + tot);
 
-// if ($(".btn-sauce").hasClass("active")) {
-//   var temp4 = $("aside ul li:nth-child(4)");
-//   var p4 = "temp4".replace(/[^0-9]/g,'');
-// }
-//
-// if ($(".btn-crust").hasClass("active")) {
-//   var temp5 = $("aside ul li:nth-child(5)");
-//   var p5 = "temp5".replace(/[^0-9]/g,'');
-// }
+function priceThree() {
+  if ($(".btn-green-peppers").hasClass("active")) {
+    var p3 = 1
+  }
+  else {
+    p3 = 0
+  }
+  return p3;
+}
+
+
+var tot = priceOne() + priceTwo() + priceThree();
+$("aside strong").text("$" + (10 + tot));
