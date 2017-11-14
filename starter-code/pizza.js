aed755f7d555c3dde3ee35c1253b16b9d33a60c0
asalria <ameliasalria@gmail.com>
2017-11-14 21:02:50 +0100

    feat: Hide/Unhide pizza elements - I1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,17 @@
 // Write your Pizza Builder JavaScript in this file.
+$(function(){
+
+$(".btn-pepperonni").click(function(){
+        $(".pep").toggle();
+});
+
+$(".btn-green-peppers").click(function(){
+    $(".green-pepper").toggle();
+});
+
+$(".btn-mushrooms").click(function(){
+    $(".mushroom").toggle();
+});
+
+
+});
