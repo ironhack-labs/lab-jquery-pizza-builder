e26797d801cc3eeff02819590adfeeaef4d65319
asalria <ameliasalria@gmail.com>
2017-11-14 21:11:33 +0100

    feat: Gluten free crust + Sauce - I2

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,6 +1,7 @@
 // Write your Pizza Builder JavaScript in this file.
 $(function(){
 
+//Iteration 1
 $(".btn-pepperonni").click(function(){
         $(".pep").toggle();
 });
@@ -13,5 +14,18 @@ $(".btn-mushrooms").click(function(){
     $(".mushroom").toggle();
 });
 
+//Iteration 2
+
+$(".crust").removeClass("crust-gluten-free");
+
+$(".btn-crust").click(function(){
+    $(".crust").addClass("crust-gluten-free");
+});
+
+$(".sauce").removeClass("sauce-white");
+
+$(".btn-sauce").click(function(){
+    $(".sauce").addClass("sauce-white");
+});
 
 });
