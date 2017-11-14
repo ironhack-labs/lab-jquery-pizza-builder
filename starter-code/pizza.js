b81ed656873f2c7a91600481a3f349a0cf40d466
Juancho Arregui <juan.arregui.lopez@gmail.com>
2017-11-14 21:53:27 +0100

    feat: clickIngredient function created.  works fine

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,33 @@
 // Write your Pizza Builder JavaScript in this file.
+
+function clickIngrendient(buttonName, ingredientName){
+    if ( $(buttonName).hasClass("active") ) {
+        $(buttonName).removeClass("active");
+        $(ingredientName).hide();
+    }
+    else {
+        $(buttonName).addClass("active");
+        $(ingredientName).show();
+    }
+}
+
+
+$(function(){
+
+    
+    $(".btn-pepperonni").click(function(){
+        clickIngrendient(".btn-pepperonni", ".pep")
+    });
+
+
+    $("button.btn-mushrooms").click(function(){
+        clickIngrendient(".btn-mushrooms", ".mushroom")
+    });
+
+
+
+
+
+
+
+})
