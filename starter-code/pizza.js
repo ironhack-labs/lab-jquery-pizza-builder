837316c0249096fb37a8774556e195f18a5bcb8a
Pablo Laso <plasocortabitarte@gmail.com>
2017-11-14 21:10:44 +0100

    feat: three first buttons toggle done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,16 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(document).ready(function() {
+
+  function toogleBtn(btnClassName, ingredientClass) {
+    $(btnClassName).click(function() {
+      $(this).toggleClass("active");
+      $(ingredientClass).toggle();
+    });
+  }
+
+  toogleBtn(".btn-pepperonni", ".pep");
+  toogleBtn(".btn-mushrooms", ".mushroom");
+  toogleBtn(".btn-green-peppers", ".green-pepper");
+
+});
