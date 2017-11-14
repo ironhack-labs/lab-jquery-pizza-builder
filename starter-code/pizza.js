0c224ad3b82ba20a1a47b3c47afc064ebc4cc616
asalria <ameliasalria@gmail.com>
2017-11-14 21:30:51 +0100

    feat: Buttons - I3

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,16 +1,21 @@
 // Write your Pizza Builder JavaScript in this file.
 $(function(){
 
+
 //Iteration 1
 $(".btn-pepperonni").click(function(){
-        $(".pep").toggle();
+    ingActive(".pep",".btn-pepperonni");
+    $(".pep").toggle();
+        
 });
 
 $(".btn-green-peppers").click(function(){
+    ingActive(".green-pepper",".btn-green-peppers");
     $(".green-pepper").toggle();
 });
 
 $(".btn-mushrooms").click(function(){
+    ingActive(".mushroom",".btn-mushrooms");
     $(".mushroom").toggle();
 });
 
@@ -19,13 +24,34 @@ $(".btn-mushrooms").click(function(){
 $(".crust").removeClass("crust-gluten-free");
 
 $(".btn-crust").click(function(){
+    eleClass(".crust","crust-gluten-free",".btn-crust");
     $(".crust").addClass("crust-gluten-free");
 });
 
 $(".sauce").removeClass("sauce-white");
 
 $(".btn-sauce").click(function(){
+    eleClass(".sauce","sauce-white",".btn-sauce");
     $(".sauce").addClass("sauce-white");
 });
 
+//Iteration 3
+
+
 });
+
+function ingActive(element,button){
+    if($(element+":visible").length>0)
+    {
+       $(button).addClass("active"); 
+    }
+    else $(button).removeClass("active");
+}
+
+function eleClass(element,classs,button){
+    if($(element).hasClass(classs))
+    {
+       $(button).addClass("active"); 
+    }
+    else $(button).removeClass("active");
+}
