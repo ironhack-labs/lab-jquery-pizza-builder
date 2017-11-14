fcbc6c7d7c8cab51e85fe5ac946339786508ad1a
Juancho Arregui <juan.arregui.lopez@gmail.com>
2017-11-15 00:41:03 +0100

    feat: price update ok.  stilll missing iteration 5

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,30 +1,46 @@
 // Write your Pizza Builder JavaScript in this file.
 
 function clickIngrendient(buttonName, ingredientName){
+    pizzaPrice = parseFloat(( $("strong").text() ).replace('$', '')) || 0;
+
     if ( $(buttonName).hasClass("active") ) {
         $(buttonName).removeClass("active");
         $(ingredientName).hide();
+        pizzaPrice -=3;
+        $("strong").text("$" + pizzaPrice);
     }
     else {
         $(buttonName).addClass("active");
         $(ingredientName).show();
+        pizzaPrice +=3;
+        $("strong").text("$" + pizzaPrice);
     }
 }
 
 
+
+
 $(function(){
 
-    
     $(".btn-pepperonni").click(function(){
-        clickIngrendient(".btn-pepperonni", ".pep")
+        clickIngrendient(this, ".pep")
     });
 
-
-    $("button.btn-mushrooms").click(function(){
+    $(".btn-mushrooms").click(function(){
         clickIngrendient(".btn-mushrooms", ".mushroom")
     });
 
+    $(".btn-green-peppers").click(function(){
+        clickIngrendient(".btn-green-peppers", ".green-pepper")
+    });
 
+    $(".btn-sauce").click(function(){
+        clickIngrendient(".btn-sauce", ".sauce")
+    });
+
+    $(".btn-crust").click(function(){
+        clickIngrendient(".btn-crust", ".crust")
+    });
 
 
 
