58fc14fcd6c406e56ed0fe23a3c8d92987a367ac
asalria <ameliasalria@gmail.com>
2017-11-14 23:11:37 +0100

    feat: Price update T5

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -3,24 +3,25 @@ $(function(){
     ingActive(".pep",".btn-pepperonni");
     ingActive(".green-pepper",".btn-green-peppers");
     ingActive(".mushroom",".btn-mushrooms");
+    
 
 //Iteration 1
 $(".btn-pepperonni").click(function(){
-    $(".pep").toggle();
+    showElements(".pep","pepperonni");
     ingActive(".pep",".btn-pepperonni");
-        
+    totalPrice();    
 });
 
 $(".btn-green-peppers").click(function(){
-    $(".green-pepper").toggle();
+    showElements(".green-pepper","green");
     ingActive(".green-pepper",".btn-green-peppers");
-    
+    totalPrice();
 });
 
 $(".btn-mushrooms").click(function(){
-    $(".mushroom").toggle();
+    showElements(".mushroom","mush");
     ingActive(".mushroom",".btn-mushrooms");
-    
+    totalPrice();
 });
 
 //Iteration 2
@@ -29,14 +30,18 @@ $(".crust").removeClass("crust-gluten-free");
 
 $(".btn-crust").click(function(){
     eleClass(".crust","crust-gluten-free",".btn-crust");
+    showElements2("gluten-free");
     $(".crust").toggleClass("crust-gluten-free");
+    totalPrice();
 });
 
 $(".sauce").removeClass("sauce-white");
 
 $(".btn-sauce").click(function(){
     eleClass(".sauce","sauce-white",".btn-sauce");
+    showElements2("white sauce");
     $(".sauce").toggleClass("sauce-white");
+    totalPrice();
 });
 
 //Iteration 3
@@ -58,4 +63,26 @@ function eleClass(element,classs,button){
        $(button).addClass("active"); 
     }
     else $(button).removeClass("active");
+}
+
+function showElements(element,identText){
+    $(element).toggle();
+    $("li:contains('"+identText+"')").toggle();
+}
+
+function showElements2(identText){
+    $("li:contains('"+identText+"')").toggle();
+}
+
+function totalPrice(){
+    var list = $(".price li:visible").toArray();
+    var mainEle = $("b").toArray()[0].innerText;
+    var total = Number(mainEle.substring(1,mainEle.indexOf(" ")));
+    for(var i=0;i<list.length;i++){
+        element = list[i].innerText;
+        total += Number(element.substring(1,element.indexOf(" ")));
+    }
+
+    $("strong").text("$"+total);
+    
 }
