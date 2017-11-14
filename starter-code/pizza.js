032fcde5f2546546e5dbbda3cf6807ae2dd77701
mouyaq <mouyaq@gmail.com>
2017-11-14 21:12:13 +0100

    feat: Iteration 1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,27 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(".btn-pepperonni").click(function(){
+    $(".pep").hide();
+});
+
+$(".btn-mushrooms").click(function(){
+    $(".mushroom").hide();
+});
+
+$(".btn-green-peppers").click(function(){
+    $(".green-pepper").hide();
+});
+
+$(".btn-sauce").click(function(){
+    $(".sauce").hide();
+});
+
+$(".btn-crust").click(function(){
+    $(".crust").hide();
+});
+
+
+
+
+
+
