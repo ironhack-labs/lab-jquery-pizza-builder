520a1c05f7fee47e26b22b11491a3d94b13f1915
Mario Velasco Alonso <mario1velasco@gmail.com>
2017-11-15 11:55:52 +0100

    Feat: Add gitignore, pizza.js toggleclass works weird

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,33 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(function() {
+  // clickbool = true;
+  // $('.btn-green-peppers').click(function() {
+  //   if (clickbool) {
+  //     $('.green-pepper').hide('slow');
+  //     // $('.green-pepper').toggleClass('green-pepper');
+  //     clickbool=false;
+  //   }else{
+  //     $('.green-pepper').show('slow');
+  //     clickbool=true;
+  //   }
+  // });
+
+$('.btn-green-peppers').click(function() {
+  // debugger;
+  $("#pizza .pepe-verde").toggleClass("green-pepper");
+});
+$('.btn-mushrooms').click(function() {
+  // debugger;
+  $("#pizza .champi").toggleClass("mushroom");
+});
+$('.btn-pepperonni').click(function() {
+  // debugger;
+  $("#pizza .peperoni").toggleClass("pep");
+});
+
+
+
+
+
+});
