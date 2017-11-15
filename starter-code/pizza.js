d6f3e44bf7add612e54ed448b8393d8b9bb616ed
mouyaq <mouyaq@gmail.com>
2017-11-15 21:02:55 +0100

    Iteration 2 & 3 done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,27 +1,49 @@
 // Write your Pizza Builder JavaScript in this file.
 
+function toggleVisibility(classButton, classIngredient) {
+    $(classButton).toggleClass("active");
+    if($(classButton).hasClass("active")) {
+        $(classIngredient).show();
+    }
+    else {
+        $(classIngredient).hide();
+    }
+    console.log($(classButton));    
+}
 $(".btn-pepperonni").click(function(){
-    $(".pep").hide();
+    toggleVisibility(".btn-pepperonni",".pep");
 });
 
 $(".btn-mushrooms").click(function(){
-    $(".mushroom").hide();
+    toggleVisibility(".btn-mushrooms",".mushroom");
 });
 
 $(".btn-green-peppers").click(function(){
-    $(".green-pepper").hide();
+    toggleVisibility(".btn-green-peppers",".green-pepper");
 });
 
 $(".btn-sauce").click(function(){
-    $(".sauce").hide();
+    $("#pizza > .crust > .sauce").toggleClass("sauce-white");
+    if($("#pizza > .crust > .sauce").hasClass("sauce-white")) {
+        $(".btn-sauce").html("Sauce");
+    }
+    else {
+        $(".btn-sauce").html("White sauce");
+    }
 });
 
 $(".btn-crust").click(function(){
-    $(".crust").hide();
+    $("#pizza > .crust").toggleClass("crust-gluten-free");
+    if($("#pizza > .crust").hasClass("crust-gluten-free")) {
+        $(".btn-crust").html("Crust");
+    }
+    else {
+        $(".btn-crust").html("Gluten-free crust");
+    }
 });
 
-
-
-
-
-
+// on ready set sauce and crust instead of sauce-white and crust-gluten-free
+$(function(){
+    $("#pizza > .crust").toggleClass("crust-gluten-free");
+    $("#pizza > .crust > .sauce").toggleClass("sauce-white");
+});
