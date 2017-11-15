67f875c7250d573edc87c67ab624141c2fc46607
Pablo Laso <plasocortabitarte@gmail.com>
2017-11-15 15:21:43 +0100

    feat: show price for each ingredient done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -2,32 +2,49 @@
 
 $(document).ready(function() {
 
-  function toogleIngredientsBtn(btnClassName, ingredientClass) {
+  // Peperoni, mushrooms and green pepper toogle buttons and show ingredients in pizza
+  function toogleIngredientsBtn(btnClassName, ingredientClass, priceSelector) {
     $(btnClassName).click(function() {
       $(this).toggleClass("active");
       $(ingredientClass).toggle();
+      $(priceSelector).toggle();
     });
   }
 
-  function toogleSauceCrustBtn(btnClassName, permanentClass, addedClass) {
+  //Sauce and crust toggle buttons and show ingredients in pizza
+  function toogleSauceCrustBtn(btnClassName, permanentClass, addedClass, priceSelector) {
     $(btnClassName).click(function() {
       $(this).toggleClass("active");
       $(permanentClass).toggleClass(addedClass);
+      $(priceSelector).toggle();
     });
   }
 
+  //Default values for sauce and crust
   function setDefaultSauceCrust(permanentClass, addedClass) {
     $(permanentClass).removeClass(addedClass);
   }
 
+  function defaultHidePrices(selector) {
+    $(selector).hide();
+  }
+
+
+
   setDefaultSauceCrust(".sauce", "sauce-white");
   setDefaultSauceCrust(".crust", "crust-gluten-free");
 
-  toogleIngredientsBtn(".btn-pepperonni", ".pep");
-  toogleIngredientsBtn(".btn-mushrooms", ".mushroom");
-  toogleIngredientsBtn(".btn-green-peppers", ".green-pepper");
-  toogleSauceCrustBtn(".btn-sauce", ".sauce", "sauce-white");
-  toogleSauceCrustBtn(".btn-crust", ".crust", "crust-gluten-free");
+  setDefaultSauceCrust(".btn-sauce", "active");
+  setDefaultSauceCrust(".btn-crust", "active");
+
+  defaultHidePrices("aside li:contains('white sauce')");
+  defaultHidePrices("aside li:contains('gluten-free crust')");
+
+  toogleIngredientsBtn(".btn-pepperonni", ".pep", "aside li:contains('pepperonni')");
+  toogleIngredientsBtn(".btn-mushrooms", ".mushroom", "aside li:contains('mushrooms')");
+  toogleIngredientsBtn(".btn-green-peppers", ".green-pepper", "aside li:contains('green peppers')");
+  toogleSauceCrustBtn(".btn-sauce", ".sauce", "sauce-white", "aside li:contains('white sauce')");
+  toogleSauceCrustBtn(".btn-crust", ".crust", "crust-gluten-free", "aside li:contains('gluten-free crust')");
 
 
 });
