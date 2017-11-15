7dc23dfc9a4df8175634791f85e044d2b6bb61e8
Mario Velasco Alonso <mario1velasco@gmail.com>
2017-11-15 19:38:36 +0100

    Feat: Sum and subtraction working

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -18,32 +18,49 @@ function hideAndShowOrAdd(button, ingredient, extraIngredient) {
         $(this).addClass('active');
       }
     }
-    addAndRemoveIngredientsFromList(ingredient, !$(this).hasClass("active"));
+    addAndRemoveIngredientsFromList(ingredient, $(this).hasClass("active"));
   });
 }
 
 function addAndRemoveIngredientsFromList(ingredient, boolValue) {
   liPosition = 0;
+  unitPrice = 0;
+
   switch (ingredient) {
     case ".green-pepper":
       liPosition = 1;
+      unitPrice = 1;
       break;
     case ".mushroom":
       liPosition = 2;
+      unitPrice = 1;
       break;
     case ".pep":
       liPosition = 3;
+      unitPrice = 1;
       break;
     case ".sauce":
       liPosition = 4;
+      unitPrice = 3;
       break;
     case ".crust":
       liPosition = 5;
+      unitPrice = 5;
       break;
     default:
       break;
   }
-  (boolValue) ? $(".panel-price-ul li:nth-child(" + liPosition + ")").hide('fast'): $(".panel-price-ul li:nth-child(" + liPosition + ")").show('fast');
+  textFromLi = $(".price strong").text();
+  numberStrong = textFromLi[1]+textFromLi[2];
+  if (boolValue) {
+    $(".panel-price-ul li:nth-child(" + liPosition + ")").show('fast');
+    numberStrong=parseInt(numberStrong)+unitPrice;
+    $(".price strong").text("$"+numberStrong);
+  } else {
+    $(".panel-price-ul li:nth-child(" + liPosition + ")").hide('fast');
+    numberStrong=parseInt(numberStrong)-unitPrice;
+    $(".price strong").text("$"+numberStrong);
+  }
 }
 $(function() {
   //Gluten Free and White Sauce doesn't show at the beggining
@@ -51,6 +68,8 @@ $(function() {
   $(".btn-sauce").removeClass('active');
   $(".panel-price-ul li:nth-child(4)").hide('fast');
   $(".panel-price-ul li:nth-child(5)").hide('fast');
+
+  $(".price strong").text("$13");
   //We add the listeners to the buttons
   hideAndShowOrAdd('.btn-green-peppers', '.green-pepper');
   hideAndShowOrAdd('.btn-mushrooms', '.mushroom');
