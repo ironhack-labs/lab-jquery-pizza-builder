002368b84470cbc5a78faba21037c83f34890a61
Mario Velasco Alonso <mario1velasco@gmail.com>
2017-11-15 12:52:08 +0100

    Feat: Bottoms working properly

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,30 +1,43 @@
 // Write your Pizza Builder JavaScript in this file.
+function hideAndShowOrAdd(button, ingredient, boolValue, extraIngredient) {
+  $(button).click(function() {
+    // debugger;
+      if(extraIngredient){
+        if (!boolValue) {
+          $(ingredient).addClass(extraIngredient);
+          $(this).removeClass('active');
+          // $(ingredient).toggleClass('green-pepper');
+        } else {
+          $(ingredient).removeClass(extraIngredient);
+          $(this).addClass('active');
+        }
+      }else{
+        if (boolValue) {
+          $(ingredient).hide('slow');
+          $(this).removeClass('active');
+          // $(ingredient).toggleClass('green-pepper');
+        } else {
+          $(ingredient).show('slow');
+          $(this).addClass('active');
+        }
+      }
+      boolValue = !boolValue;
+  });
+}
 
 $(function() {
-  // clickbool = true;
-  // $('.btn-green-peppers').click(function() {
-  //   if (clickbool) {
-  //     $('.green-pepper').hide('slow');
-  //     // $('.green-pepper').toggleClass('green-pepper');
-  //     clickbool=false;
-  //   }else{
-  //     $('.green-pepper').show('slow');
-  //     clickbool=true;
-  //   }
-  // });
+  showPeppers = true;
+  showMushrooms = true;
+  showPepperonni = true;
+  showGlutenFree = false;
+  showSauceWhite = false;
+  hideAndShowOrAdd('.btn-green-peppers', '.green-pepper', showPeppers);
+  hideAndShowOrAdd('.btn-mushrooms', '.mushroom', showMushrooms);
+  hideAndShowOrAdd('.btn-pepperonni', '.pep', showPepperonni);
+  hideAndShowOrAdd('.btn-crust', '.crust', showGlutenFree, 'crust-gluten-free');
+  hideAndShowOrAdd('.btn-sauce', '.sauce', showGlutenFree, 'sauce-white');
+  // hideAndShowOrAdd('.btn-sauce', '.sauce-white', showSauceWhite);
 
-$('.btn-green-peppers').click(function() {
-  // debugger;
-  $("#pizza .pepe-verde").toggleClass("green-pepper");
-});
-$('.btn-mushrooms').click(function() {
-  // debugger;
-  $("#pizza .champi").toggleClass("mushroom");
-});
-$('.btn-pepperonni').click(function() {
-  // debugger;
-  $("#pizza .peperoni").toggleClass("pep");
-});
 
 
 
