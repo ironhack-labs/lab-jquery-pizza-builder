4d93736dd4d023f0fdb89374cc4f972bd7328e40
Javier Torres <javier.torres@nfq.es>
2017-11-16 14:22:19 +0100

    feat: reset pizza ingredients and price manager done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,4 +1,20 @@
 // Write your Pizza Builder JavaScript in this file.
+function reset() {
+  $('.btn').removeClass('active');
+  $('.btn-pepperonni').removeClass('active');
+  $('.pep').hide();
+  $('.mushroom').hide();
+  $('.green-pepper').hide();
+  $('.sauce').hide();
+  $('.crust').removeClass('crust-gluten-free');
+  $('.panel>ul>li:contains($1 pepperonni)').hide();
+  $('.panel>ul>li:contains($1 mushrooms)').hide();
+  $('.panel>ul>li:contains($1 green peppers)').hide();
+  $('.panel>ul>li:contains($3 white sauce)').hide();
+  $('.panel>ul>li:contains($5 gluten-free crust)').hide();
+  $('.panel>strong').text('$10');
+}
+reset();
 
 $('.btn-pepperonni').click(function() {
   if ($(this).hasClass('active')) {
@@ -10,6 +26,7 @@ $('.btn-pepperonni').click(function() {
     $(this).addClass('active');
     $('.panel>ul>li:contains($1 pepperonni)').show();
   }
+  $('.panel>strong').text('$' + totalPizza());
 });
 $('.btn-mushrooms').click(function() {
   if ($(this).hasClass('active')) {
@@ -21,6 +38,7 @@ $('.btn-mushrooms').click(function() {
     $(this).addClass('active');
     $('.panel>ul>li:contains($1 mushrooms)').show();
   }
+  $('.panel>strong').text('$' + totalPizza());
 });
 $('.btn-green-peppers').click(function() {
   if ($(this).hasClass('active')) {
@@ -32,9 +50,10 @@ $('.btn-green-peppers').click(function() {
     $(this).addClass('active');
     $('.panel>ul>li:contains($1 green peppers)').show();
   }
+  $('.panel>strong').text('$' + totalPizza());
 });
 
-$('.btn-sauce.active').click(function() {
+$('.btn-sauce').click(function() {
   if ($(this).hasClass('active')) {
     $('.sauce-white').hide();
     $(this).removeClass('active');
@@ -44,9 +63,10 @@ $('.btn-sauce.active').click(function() {
     $(this).addClass('active');
     $('.panel>ul>li:contains($3 white sauce)').show();
   }
+  $('.panel>strong').text('$' + totalPizza());
 });
 
-$('.btn-crust.active').click(function() {
+$('.btn-crust').click(function() {
   if ($(this).hasClass('active')) {
     $('.crust').removeClass('crust-gluten-free');
     $(this).removeClass('active');
@@ -56,7 +76,28 @@ $('.btn-crust.active').click(function() {
     $(this).addClass('active');
     $('.panel>ul>li:contains($5 gluten-free crust)').show();
   }
+  $('.panel>strong').text('$' + totalPizza());
 });
+
+totalPizza = function() {
+  pizzaPrice = 10;
+  if ($('.btn-pepperonni').hasClass('active')) {
+    pizzaPrice += 1;
+  }
+  if ($('.btn-mushrooms').hasClass('active')) {
+    pizzaPrice += 1;
+  }
+  if ($('.btn-green-peppers').hasClass('active')) {
+    pizzaPrice += 1;
+  }
+  if ($('.btn-sauce').hasClass('active')) {
+    pizzaPrice += 3;
+  }
+  if ($('.btn-crust').hasClass('active')) {
+    pizzaPrice += 5;
+  }
+  return pizzaPrice;
+};
 
 // addClass()
 // removeClass()
