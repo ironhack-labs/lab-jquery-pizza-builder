0b631360182cfc5d11d8048c4705802818220630
Jorge Yago <jorge.yago.meniz@gmail.com>
2017-11-16 17:19:29 +0100

    Iteration 1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,29 @@
-// Write your Pizza Builder JavaScript in this file.
+$(function(){
+  $(".btn-pepperonni").click(function(){
+    $(this).toggleClass("active");
+if ($(this).hasClass("active")){
+  $(".pep").show();
+} else {
+  $(".pep").hide();
+}
+  });
+
+$(".btn-mushrooms").click(function(){
+    $(this).toggleClass("active");
+if ($(this).hasClass("active")){
+  $(".mushroom").show();
+} else {
+  $(".mushroom").hide();
+}
+  });
+
+$(".btn-green-peppers").click(function(){
+      $(this).toggleClass("active");
+  if ($(this).hasClass("active")){
+    $(".green-pepper").show();
+  } else {
+    $(".green-pepper").hide();
+  }
+    });
+
+});
