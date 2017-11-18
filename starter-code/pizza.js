9ee6909d568f2124a3ccc822f191426ea13b0c55
diana-91 <diana.hervasmoya@gmail.com>
2017-11-18 08:43:28 +0100

    feat: price ingredients - lists ingredients

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -4,10 +4,17 @@ $(document).ready(function(){
   /*Por defecto la pizza es sin gluten-free y sin sauce-white*/
   $('.crust').removeClass('crust-gluten-free');
   $('.sauce').removeClass('sauce-white');
+  $('.btn-crust').removeClass('active');
+  $('.btn-sauce').removeClass('active');
+  price();
+
+
 
   addRemIngredients('.btn-pepperonni','.pep');
   addRemIngredients('.btn-green-peppers','.green-pepper');
   addRemIngredients('.btn-mushrooms','.mushroom');
+  addRemIngredients('.btn-sauce','.sauce','sauce-white');
+  addRemIngredients('.btn-crust','.crust','crust-gluten-free');
 
 });
 
@@ -19,10 +26,34 @@ function addRemIngredients(btn,target,toggle){
     $(this).toggleClass("active");
 
     if(toggle){
-      $(target).toggleClass(toggleClass);
+      $(target).toggleClass(toggle);
     }else{
       $(target).toggle();
     }
+    price();
+  });
+}
 
-  })
+function price(){
+  var total = 10;
+  $(".price li").hide();
+
+
+ $.each($(".btn.active"), function(index, btn) {
+   var ingredient = $(btn).text().toLowerCase();
+   var $listElement = $("li:contains('" + ingredient + "')");
+
+   var ingredientPrice = parseInt(
+      $listElement.text().split(" ")[0].replace("$", "")
+    );
+
+    total += ingredientPrice;
+
+
+    $listElement.show();
+ });
+
+ $(".price strong").text("$" + total);
+
+
 }
