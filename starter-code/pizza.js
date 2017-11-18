a72964bfc3e54d10529cb3d8152632d14eb4d7f3
Cesar Aparicio <contacto@cesaraparicio.es>
2017-11-18 17:00:15 +0100

    Itaration 2

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,5 +1,10 @@
 // Write your Pizza Builder JavaScript in this file.
 
+$(function() {
+  $(".btn-crust").removeClass("active");
+  $("button.btn-sauce").removeClass("active");
+});
+
 $("button.btn-crust").click(function() {
   $(this).toggleClass("active");
     $("section.crust > section.cheese").toggle();
@@ -22,3 +27,9 @@ $("button.btn-sauce").click(function() {
   $(this).toggleClass("active");
     $("section.sauce").toggle();
 });
+//$("button.btn-crust").toggleClass("active");
+//$(this).toggleClass("active");
+//if ($(".btn-crust").hasClas("active")) {
+//$(".btn-crust").removeClass("active");
+//}
+//});
