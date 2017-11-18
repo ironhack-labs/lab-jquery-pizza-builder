3e46803d1fbf087616cb5e53f18d7cada05b63cc
Cesar Aparicio <contacto@cesaraparicio.es>
2017-11-18 16:01:55 +0100

    first commit

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,18 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$("button.btn-crust").click(function() {
+  $(this).toggleClass("active");
+  if ($(this).hasClass("active")) {
+    $("section.crust > section.cheese").toggle();
+  } else {
+    $(".section.crust > section.cheese").toggle();
+  }
+});
+$("button.btn-pepperonni").click(function() {
+  $(this).toggleClass("active");
+  if ($(this).hasClass("active")) {
+    $("section.pep").toggle();
+  } else {
+    $(".section.pep").toggle();
+  }
+});
