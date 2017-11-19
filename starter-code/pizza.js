7e41201c45d2ad105e8056ae32ffd89397a9f655
christiangar <32417561+christiangar@users.noreply.github.com>
2017-11-19 14:41:31 +0100

    Pizza Done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,45 +1,68 @@
 // Write your Pizza Builder JavaScript in this file.
 $(function() {
-  //$(".pep, .mushroom, .green-pepper").hide()
-  //$(".btn-pepperonni, .btn-mushrooms, .btn-green-peppers").toggleClass("active")
+
+
   $(".crust").removeClass("crust-gluten-free")
   $(".sauce").removeClass("sauce-white")
   $(".price ul li:contains('$5 gluten-free crust')").hide()
   $(".price ul li:contains('$3 white sauce')").hide()
 
-  //Quiero que si el BTN de sauce esta activado, tenga la class de sauce normal, y si no lo esta, que tenga
-  // la clase de sauce-white
-
   $(".btn-crust").click(function() {
     $(this).toggleClass("active");
     $(this).hasClass("active") ? $(".crust").removeClass("crust-gluten-free") : $(".crust").addClass("crust-gluten-free")
     $(this).hasClass("active") ? $(".price ul li:contains('$5 gluten-free crust')").hide() : $(".price ul li:contains('$5 gluten-free crust')").show()
+    getPrice();
   });
 
   $(".btn-sauce").click(function() {
     $(this).toggleClass("active");
     $(this).hasClass("active") ? $(".sauce").removeClass("sauce-white") : $(".sauce").addClass("sauce-white")
     $(this).hasClass("active") ? $(".price ul li:contains('$3 white sauce')").hide() : $(".price ul li:contains('$3 white sauce')").show()
+    getPrice();
   });
 
   $(".btn-pepperonni").click(function() {
     $(this).hasClass("active") ? $(".pep").hide() : $(".pep").show();
-    $(this).hasClass("active") ? $(".price ul li:contains('$1 pepperonni')").remove() : $(".price ul").append("<li>$1 pepperonni</li>")
+    $(this).hasClass("active") ? $(".price ul li:contains('$1 pepperonni')").hide() : $(".price ul li:contains('$1 pepperonni')").show()
     $(this).toggleClass("active");
+    getPrice();
 
   });
 
   $(".btn-mushrooms").click(function() {
     $(this).hasClass("active") ? $(".mushroom").hide() : $(".mushroom").show();
-    $(this).hasClass("active") ? $(".price ul li:contains('$1 mushrooms')").remove() : $(".price ul").append("<li>$1 mushrooms</li>")
+    $(this).hasClass("active") ? $(".price ul li:contains('$1 mushrooms')").hide() : $(".price ul li:contains('$1 mushrooms')").show()
     $(this).toggleClass("active");
+    getPrice();
   });
 
   $(".btn-green-peppers").click(function() {
     $(this).hasClass("active") ? $(".green-pepper").hide() : $(".green-pepper").show();
-    $(this).hasClass("active") ? $(".price ul li:contains('$1 green peppers')").remove() : $(".price ul").append("<li>$1 green peppers</li>")
+    $(this).hasClass("active") ? $(".price ul li:contains('$1 green peppers')").hide() : $(".price ul li:contains('$1 green peppers')").show()
     $(this).toggleClass("active");
+    getPrice();
   });
-
-
+  // Funcion para iterar sobre selector, que me pille los lis visibles y dentro de estos lis visibles
+  function getPrice() {
+    prices = [];
+    $(".price").find("li:visible").addClass("added")
+    $(".price").find("li:hidden").addClass("hiding").removeClass("added")
+    $('.added').each(function() {
+      prices += $(this).text();
+    })
+    if (prices.length > 0) {
+      toppingPrice = prices.replace(/\D/g, '');
+      toppingArray = toppingPrice.split('').map(Number);
+      var total = 0;
+      for(i=0; i<toppingArray.length; i++){
+       total += toppingArray[i];
+   }
+    } else {
+      toppingPrice = 0;
+      total = 0;
+    }
+    basePrice = 10;
+    finalPrice = basePrice + total
+    $("strong").text("$" + finalPrice)
+  }
 });
