50e320091c3b8408f9ff7c6aa5ac5f54fa272bb1
rafalopezgit <rafael.lopezgm@gmail.com>
2017-11-20 23:55:04 +0100

    No he conseguido acabarlo. Estoy bloqueado en la suma de los precios porque no consigo que la variable se actualice correctamente.

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,16 +1,30 @@
 // Write your Pizza Builder JavaScript in this file.
+
 $(function(){
 
-  $("button").removeClass("active")
-  $(".price li").hide();
+var priceBase = 10;
+
+
+  $("button.btn-sauce").removeClass("active")
+  $("button.btn-crust").removeClass("active")
+  $(".price li:nth-child(4)").hide();
+  $(".price li:nth-child(5)").hide();
+  $(".sauce").removeClass("sauce-white");
+  $(".crust").removeClass("crust-gluten-free");
+
 
   $(".btn-green-peppers").click(function() {
     if (!$("button.btn-green-peppers").hasClass("active")) {
       $("button.btn-green-peppers").addClass("active");
       $("aside.panel li:nth-child(3)").show();
+      $(".green-pepper").toggle();
+      priceText();
+
     } else {
-        $("button.btn-green-peppers").removeClass("active");
-        $("aside.panel li:nth-child(3)").hide();
+      $("button.btn-green-peppers").removeClass("active");
+      $("aside.panel li:nth-child(3)").hide();
+      $(".green-pepper").toggle();
+      priceText();
     }
 
   })
@@ -18,9 +32,13 @@ $(function(){
     if (!$("button.btn-pepperonni").hasClass("active")) {
       $("button.btn-pepperonni").addClass("active");
       $("aside.panel li:nth-child(1)").show();
+      $(".pep").toggle();
+      priceText();
     } else {
-        $("button.btn-pepperonni").removeClass("active");
-        $("aside.panel li:nth-child(1)").hide();
+      $("button.btn-pepperonni").removeClass("active");
+      $("aside.panel li:nth-child(1)").hide();
+      $(".pep").toggle();
+      priceText();
     }
   })
 
@@ -28,19 +46,31 @@ $(function(){
     if (!$("button.btn-mushrooms").hasClass("active")) {
       $("button.btn-mushrooms").addClass("active");
       $("aside.panel li:nth-child(2)").show();
+      $(".mushroom").toggle();
+      priceText();
     } else {
-        $("button.btn-pepperonni").removeClass("active");
-        $("aside.panel li:nth-child(2)").hide();
+      $("button.btn-mushrooms").removeClass("active");
+      $("aside.panel li:nth-child(2)").hide();
+      $(".mushroom").toggle();
+      priceText();
     }
   })
 
   $(".btn-sauce").click(function() {
+    // $(this).toggleClass("active");
+    // $(".sauce").toggleClass("sauce-white");
+    // $("aside.panel li:nth-child(4)").toggle();
+
     if (!$("button.btn-sauce").hasClass("active")) {
       $("button.btn-sauce").addClass("active");
       $("aside.panel li:nth-child(4)").show();
+      $(".sauce").toggleClass("sauce-white");
+      priceText();
     } else {
-        $("button.btn-sauce").removeClass("active");
-        $("aside.panel li:nth-child(4)").hide();
+      $("button.btn-sauce").removeClass("active");
+      $("aside.panel li:nth-child(4)").hide();
+      $(".sauce").toggleClass("sauce-white");
+      priceText();
     }
   })
 
@@ -48,37 +78,46 @@ $(function(){
     if (!$("button.btn-crust").hasClass("active")) {
       $("button.btn-crust").addClass("active");
       $("aside.panel li:nth-child(5)").show();
+      $(".crust").toggleClass("crust-gluten-free");
+      priceText();
+
     } else {
-        $("button.btn-crust").removeClass("active");
-        $("aside.panel li:nth-child(5)").hide();
+      $("button.btn-crust").removeClass("active");
+      $("aside.panel li:nth-child(5)").hide();
+      $(".crust").toggleClass("crust-gluten-free");
+      priceText();
     }
   })
 
-  // $("button.btn-green-peppers").addClass("active")
-  //   $(".green-pepper").toggle();
-  //     $("aside.panel li:nth-child(3)").toggle();
+// $(function priceText(){
 
 
-  // $(".btn-pepperonni").click(function() {
-  //  $(".pep").toggle();
-  //   $("aside.panel li:nth-child(1)").toggle();
+function priceText(){
 
+  if ($("aside.panel li:nth-child(3)").is(":visible") === true){priceBase +=1
+  } else {
+    priceBase -=1
+  }
+  if ($("aside.panel li:nth-child(1)").is(":visible") === true){priceBase +=1
+  } else {
+    priceBase -=1
+  }
+  if ($("aside.panel li:nth-child(2)").is(":visible") === true){priceBase +=1
+  } else {
+    priceBase -=1
+  }
+  if ($("aside.panel li:nth-child(4)").is(":visible") === true){priceBase +=3
+  } else {
+    priceBase -=3
+  }
+  if ($("aside.panel li:nth-child(5)").is(":visible") === true){priceBase +=5
+  } else {
+    priceBase -=5
+  } return priceBase
+};
 
+var finalPrice = priceText();
 
- //  $(".btn-mushrooms").click(function() {
- //    $(".mushroom").toggle();
- //      $("aside.panel li:nth-child(2)").toggle();
- // })
-
-  // $(".btn-sauce").click(function() {
-  //   // $(".crust-gluten-free .sauce-white").toggle();
-  //       $("aside.panel li:nth-child(4)").toggle();
-  // })
-
-  // $(".btn-crust").click(function() {
-  //   // $(".crust-gluten-free").toggle();
-  //       $("aside.panel li:nth-child(5)").toggle();
-  // })
-
+$("aside.price strong").text("$ " + finalPrice);
 
 });
