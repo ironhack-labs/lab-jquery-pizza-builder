17257f6a4bfbdbcb89305d2e9ad1bcdb6566f35a
Alvaro <amartinezfernandez.61@gmail.com>
2018-01-16 16:44:24 +0100

    Iteración 1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,21 @@
 // Write your Pizza Builder JavaScript in this file.
+ $(document).ready(function(){
+    $(".btn-pepperonni.active").click(function() {
+        $(".pep").toggle('display');
+     });
+});
+$(document).ready(function(){
+    $(".btn-mushrooms.active").click(function() {
+        $(".mushroom").toggle('display');
+     });
+});
+$(document).ready(function(){
+    $(".btn-green-peppers.active").click(function() {
+        $(".green-pepper").toggle('display');
+     });
+});
+// $(document).ready(function(){
+//     $(".btn-sauce.active").click(function() {
+//         $(".sauce-white").toggle('display');
+//      });
+// });
