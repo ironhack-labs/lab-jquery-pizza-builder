6ed2c20921219f28eabd2019eeeaa4348e117278
Ana Recuero <anarecsa@gmail.com>
2018-01-16 16:44:26 +0100

    iterations 3 & 4 completed

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,25 +1,31 @@
 // Write your Pizza Builder JavaScript in this file.
 $(document).ready(function(){
 
-  $(".btn-green-peppers").click(function(){
-    $(".btn-green-peppers").toggleClass("active");
-    $(".green-pepper").toggle();
-  })
+  $(".price li").hide();
+
   $(".btn-pepperonni").click(function(){
-    $(".btn-pepperonni").toggleClass("active");
+    $(".btn-pepperonni").toggleClass("active");  $(".price li:first-child").toggle();
     $(".pep").toggle();
   })
   $(".btn-mushrooms").click(function(){
     $(".btn-mushrooms").toggleClass("active");
+    $(".price li:nth-child(2)").toggle();
     $(".mushroom").toggle();
+  })
+  $(".btn-green-peppers").click(function(){
+    $(".btn-green-peppers").toggleClass("active");
+    $(".green-pepper").toggle();
+    $(".price li:nth-child(3)").toggle();
   })
   $(".btn-sauce").click(function() {
     $(".btn-sauce").toggleClass("active");
     $(".sauce").toggleClass("sauce-white");
+    $(".price li:nth-child(4)").toggle();
   })
   $(".btn-crust").click(function() {
     $(".btn-crust").toggleClass("active");
     $(".crust").toggleClass("crust-gluten-free");
+    $(".price li:last-child").toggle();
   })
   
 
