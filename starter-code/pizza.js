7d2864abf3eb1a1cb7c081aeedb60134ec734cbb
Alvaro <amartinezfernandez.61@gmail.com>
2018-01-16 18:05:02 +0100

    casi Iteración 3

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,25 +1,67 @@
 // Write your Pizza Builder JavaScript in this file.
- $(document).ready(function(){
-    $(".btn-pepperonni.active").click(function() {
+$(document).ready(function(){
+    $(".btn-pepperonni.active").removeClass("active");
+    $(".pep").toggle('display');
+    $(".btn-mushrooms.active").removeClass("active");
+    $(".mushroom").toggle('display');
+    $(".btn-green-peppers.active").removeClass("active");
+    $(".green-pepper").toggle('display');
+    $(".btn-sauce.active").removeClass("active");
+    $(".btn-crust.active").removeClass("active");
+}) 
+
+$(document).ready(function(){
+    $(".btn-pepperonni").click(function() {
+        $(".btn-pepperonni").addClass("active");
         $(".pep").toggle('display');
      });
 });
+
+
+//NO FUNCIONAAAAAAA 
+    //Si dejo solamente el if/else, cambia todo el documento y 
+    //no entiendo muy bien por qué
+
+
+// $(document).ready(function(){
+//     $(".btn-pepperonni").click(function() {
+//         $(".btn-pepperonni").click(function() {
+//         $(".btn-pepperonni").addClass("active");
+//         $(".pep").toggle('display');
+//         });
+//         if(display === true){
+//         $(".btn-pepperonni").addClass("active");
+//         $(".pep").toggle('display');
+//         }
+//         else(display === false){
+//         $(".btn-pepperonni.active").removeClass("active");
+//         $(".pep").toggle('display');
+//         }
+//      });
+// });
+
+
+
+
 $(document).ready(function(){
-    $(".btn-mushrooms.active").click(function() {
+    $(".btn-mushrooms").click(function() {
+        $(".btn-mushrooms").addClass("active");
         $(".mushroom").toggle('display');
      });
 });
 $(document).ready(function(){
-    $(".btn-green-peppers.active").click(function() {
+    $(".btn-green-peppers").click(function() {
+        $(".btn-green-peppers").addClass("active");
         $(".green-pepper").toggle('display');
      });
 });
+
 $(document).ready(function(){
     $(".sauce-white").removeClass("sauce-white");
 });
 
 $(document).ready(function(){
-    $(".btn-sauce.active").click(function() {
+    $(".btn-sauce").click(function() {
         $(".sauce").addClass("sauce-white");;
      });
 });
@@ -29,7 +71,7 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-    $(".btn-crust.active").click(function() {
+    $(".btn-crust").click(function() {
         $(".crust").addClass("crust-gluten-free");;
      });
 });
