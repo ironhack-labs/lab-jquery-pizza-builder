8c9886fd0bab8b8cbc630843e6291b5fcd9aaa5e
Alvaro <echanovevaro@gmail.com>
2018-01-16 21:37:23 +0100

    Hasta iteracion 3

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,11 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function () {
+    $('.green-pepper, .mushroom, .pep').hide();
+    $('section.crust').removeClass('crust-gluten-free');
+    $('section.sauce').removeClass('sauce-white');
+    $('button').removeClass('active');
+
+    $("button").click(function(){
+        $(this).toggleClass("active");
+    });
+});
