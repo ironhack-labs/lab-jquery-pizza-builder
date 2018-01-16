04121a16d139b85bfe16f164cf861e50eb785180
Ana Recuero <anarecsa@gmail.com>
2018-01-16 16:13:48 +0100

    iteration 1 completed

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,19 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+
+  $(".btn-green-peppers").click(function(){
+    $(".btn-green-peppers").toggleClass("active");
+    $(".green-pepper").toggle();
+  })
+  $(".btn-pepperonni").click(function(){
+    $(".btn-pepperonni").toggleClass("active");
+    $(".pep").toggle();
+  })
+  $(".btn-mushrooms").click(function(){
+    $(".btn-mushrooms").toggleClass("active");
+    $(".mushroom").toggle();
+  })
+
+  
+
+})
