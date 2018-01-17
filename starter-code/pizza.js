6b1168543d4429c6c778e610a12fa2a56de9bf1b
theRealSheng <sheng.huchai@gmail.com>
2018-01-17 15:04:19 +0100

    first commit

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,50 @@
 // Write your Pizza Builder JavaScript in this file.
+
+
+var $pizza = $('#pizza');
+
+/***** Pepperonni ****/
+
+var $peppeButton = $('.btn-pepperonni');
+var $allPeppe = $('.pep');
+
+$peppeButton.on('click', function(){
+    $allPeppe.toggle();
+});
+
+
+/***** Mushroom ****/
+
+var $mushButton = $('.btn-mushrooms');
+var $allMush = $('.mushroom');
+
+$mushButton.on('click', function () {
+        $allMush.toggle();
+})
+
+/***** Green Peppers ****/
+
+var $greenButton = $('.btn-green-peppers');
+var $allGreen = $('.green-pepper');
+
+$greenButton.on('click', function () {
+    $allGreen.toggle();
+});
+
+/***** Crush ****/
+
+var $crushButton = $('.btn-crust');
+$pizza.removeClass('crust-gluten-free');
+$pizza.addClass('crust');
+
+$crushButton.on('click', function(){
+    if ($pizza.hasClass('crust')){
+        $pizza.removeClass('crust')
+        $pizza.addClass('crust-gluten-free'); 
+    } else{
+        $pizza.removeClass('crust-gluten-free');
+        $pizza.addClass('crust'); 
+    }
+})
+
+/***** Sauce ****/
