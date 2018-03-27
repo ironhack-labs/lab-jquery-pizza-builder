6212c52bb53e56f1f7f13c29e8c49980f5fe8be0
Tanis Pan <e.pan.gar@gmail.com>
2018-03-27 16:50:11 +0200

    Iteracion 1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,38 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(document).ready(function(){
+  $(".green-pepper, .pep, .mushroom, .sauce").hide();
+  $("crust").removeClass("crust-gluten-free");
+
+  var btnPep =$(".btn-pepperonni");
+  var btnMus =$(".btn-mushrooms");
+  var btnGre =$(".btn-green-peppers");
+  var btnSau =$(".btn-sauce");
+  var btnCru =$(".btn-crust")
+  
+  btnPep.click(function(){
+    $(".pep").fadeToggle(100)
+    btnPep.toggleClass("active");
+  });
+   
+  btnMus.click(function(){
+    $(".mushroom").fadeToggle(100)
+    btnMus.toggleClass("active");
+  });
+
+  btnGre.click(function(){
+    $(".green-pepper").fadeToggle(100)
+    btnGre.toggleClass("active");
+  });
+ 
+  btnSau.click(function(){
+    $(".sauce").fadeToggle(100)
+    btnSau.toggleClass("active");
+  });
+
+  btnCru.click(function(){
+    $(".crust").toggleClass("crust-gluten-free");
+    btnCru.toggleClass("active");
+  })
+
+});
