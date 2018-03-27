dac140bdd50141e2e1951c40b2840ef3e322f549
Germán Rodríguez <germanrodriguez@MacBook-Pro-de-German.local>
2018-03-27 16:07:53 +0200

    toggle is shit

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,20 @@
 // Write your Pizza Builder JavaScript in this file.
+$(".btn.btn-pepperonni.active").click(function(){
+  $(".pep").toggle("pep");
+}); //pepperoni
+
+$(".btn.btn-mushrooms.active").click(function() {
+  $(".mushroom").toggle("mushroom");
+}); //mushrooms
+
+$(".btn.btn-green-peppers.active").click(function() {
+  $(".green-pepper").toggle("green-pepper");
+}); //green-pepprers
+
+$(".btn.btn-sauce.active").click(function() {
+  $(".sauce-white").toggle("sauce-white");
+}); //sauce
+
+$(".btn.btn-crust.active").click(function() {
+  $(".crust").toggleClass("crust-gluten-free");
+}); //crust
