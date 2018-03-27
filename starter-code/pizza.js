9a1a25db7c30a8232e2d0baa772f2ef02aae6a88
lenniez <lenniezhu@gmail.com>
2018-03-27 16:08:08 +0200

    >:((((

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,35 @@
 // Write your Pizza Builder JavaScript in this file.
+"use strict"
+
+$(".pep, .green-pepper, .mushroom").hide();
+
+$(".btn-pepperonni").click(function(){
+  $('.pep').toggle();
+});
+
+$(".btn-mushrooms").click(function() {
+  $(".mushroom").toggle();
+});
+
+$(".btn-green-peppers").click(function() {
+  $(".green-pepper").toggle();
+});
+
+$(".btn-crust").click(function() {
+  $(".crust-gluten-free").toggle();
+});
+
+$(".btn-sauce").click(function() {
+  $(".sauce-white").toggle();
+});
+
+
+
+
+/*
+btn - mushrooms;
+btn - green - peppers;
+btn - sauce;
+btn - crust;
+
+*/
