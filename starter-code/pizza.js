9b99695168de4ff290ec483be1fed2ae4d3dc3ea
luisnueda <nueda.luis@gmail.com>
2018-03-27 16:38:36 +0200

    Iteracion 2 completada

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,18 +1,37 @@
 // Write your Pizza Builder JavaScript in this file.
 
 $().ready(function() {
+  //Dejamos la pizza vacia
+  $(".pep").hide();
+  $(".mushroom").hide();
+  $(".green-pepper").hide();
+
+  $(".crust").removeClass("crust-gluten-free");
+  $(".sauce").removeClass("sauce-white");
+
+  /* Iteración 1 */
+
   // Peperoni
-  $(".btn-pepperonni").click(function(){
+
+  $(".btn-pepperonni").click(function() {
     $(".pep").fadeToggle(10);
-  })
+  });
   // Mushrooms
-  $(".btn-mushrooms").click(function(){
+  $(".btn-mushrooms").click(function() {
     $(".mushroom").fadeToggle(10);
-  })
+  });
   //Peper
-  $(".btn-green-peppers").click(function(){
+  $(".btn-green-peppers").click(function() {
     $(".green-pepper").fadeToggle(10);
-  })
+  });
 
+  /* Iteración 2 */
+
+  $(".btn-crust").click(function() {
+    $(".crust").toggleClass("crust-gluten-free");
+  });
+
+  $(".btn-sauce").click(function() {
+    $(".sauce").toggleClass("sauce-white");
+  });
 });
-
