9026b1dce4ed0ab105563d94b2b57550e9d7d10d
Luna Fidalgo <lunafidalgo@gmail.com>
2018-03-27 15:51:40 +0200

    Iteracion 1 completada

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,30 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+  borrarAnadirPeperoni();
+  borrarAnadirMushrooms();
+  borrarAnadirGreenPeppers();
+
+
+}
+)
+
+function borrarAnadirPeperoni(){
+   $('.btn-pepperonni').click(function(){
+     $('.pep').fadeToggle(100);
+   }  
+   )
+}
+
+function borrarAnadirMushrooms(){
+  $('.btn-mushrooms').click(function(){
+    $('.mushroom').fadeToggle(100);
+  }  
+  )
+}
+
+function borrarAnadirGreenPeppers(){
+  $('.btn-green-peppers').click(function(){
+    $('.green-pepper').fadeToggle(100);
+  }  
+  )
+}
