4d6d86d32ff6806151519332553743644d96631a
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-03-27 15:56:07 +0200

    Iteration 1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,26 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready( function() {
+  var pep = $(".pep");
+  var mushroom = $(".mushroom");
+  var greenPepper = $(".green-pepper");
+  var cheese = $(".cheese");
+  var sauce = $(".sauce");
+
+  pep.addClass("invisible");
+  mushroom.addClass("invisible");
+  greenPepper.addClass("invisible");
+  sauce.addClass("invisible");
+
+  $(".btn-pepperonni").on("click", function() {
+    pep.toggleClass("invisible");
+  });
+
+  $(".btn-mushrooms").on("click", function() {
+    mushroom.toggleClass("invisible");
+  });
+
+  $(".btn-green-peppers").on("click", function() {
+    greenPepper.toggleClass("invisible");
+  });
+
+});
