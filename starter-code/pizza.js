0727d0d74d6bb7ccb0b130d73cb97cf6fb866666
muflonex <muflonex@gmail.com>
2018-03-27 19:06:45 +0200

    Your pizza's ready, sir. La solución de precio es un poco básic pero funciona(TM)

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,3 +1,5 @@
+var total     = 21
+
 // Write your Pizza Builder JavaScript in this file.
 $('document').ready(function(){
   var pepBtn    = $('.btn-pepperonni')
@@ -12,21 +14,81 @@ $('document').ready(function(){
   var crust     = $('.crust')
   var active    = $('.active')
 
+
+
   crust.removeClass('crust-gluten-free')
 
-  mushBtn.click(function(){
-    $(this).toggleClass('active')
-    mush.toggle()})
+
   pepBtn.click(function(){
     $(this).toggleClass('active')
-    pep.toggle()})
+      pep.toggle()
+    $(".price ul li:first").toggle();
+    if ($(this).hasClass('active')){
+      total += 1
+    }
+    else{
+      total -= 1
+    }
+    $('.price strong').html('$'+total)
+  })
+  mushBtn.click(function(){
+    $(this).toggleClass('active')
+    mush.toggle()
+    $(".price ul li:nth-child(2)").toggle()
+    if ($(this).hasClass('active')){
+      total += 1
+    }    
+    else{
+      total -= 1
+    }
+    $('.price strong').html('$'+total)
+  })
   grnPepBtn.click(function(){
     $(this).toggleClass('active')
-    grnPep.toggle()})
+    grnPep.toggle()
+    $(".price ul li:nth-child(3)").toggle()
+    if ($(this).hasClass('active')){
+      total += 1
+    }
+    else{
+      total -= 1
+    }
+    $('.price strong').html('$'+total)
+  })
   sauceBtn.click(function(){
     $(this).toggleClass('active')
-    sauce.toggle()})
+    sauce.toggle()
+    $(".price ul li:nth-child(4)").toggle()
+    if ($(this).hasClass('active')){
+      total += 3
+    }
+    else{
+      total -= 3
+    }
+    $('.price strong').html('$'+total)
+  })
+
   crustBtn.click(function(){
     $(this).toggleClass('active')
-    crust.toggleClass('crust-gluten-free')})
+    crust.toggleClass('crust-gluten-free')
+    $(".price ul li:nth-child(5)").toggle()
+    if ($(this).hasClass('active')){
+      total += 5
+    }
+    else{
+      total -= 5
+    }
+    $('.price strong').html('$'+total)
+  })
 })
+
+//var getPrice = function(){
+//  result = 0
+//$(".price ul > li").each(function(){
+//  price.push(parseInt($( this ).html().slice(1,2)))
+//});
+//  for(i = 0; i < price.length ; i++){
+//    result += price[i]
+//  }
+//  return (result + 10)
+//}
