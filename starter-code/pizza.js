a2c1d997dcbf2160ce887337077efd8fc9af09a2
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-03-27 16:06:15 +0200

    Iteration 3

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -5,29 +5,36 @@ $(document).ready( function() {
   var greenPepper = $(".green-pepper");
   var cheese = $(".cheese");
   var sauce = $(".sauce");
+  var btn = $("button");
 
   pep.addClass("invisible");
   mushroom.addClass("invisible");
   greenPepper.addClass("invisible");
+  btn.removeClass("active");
 
-  $(".btn-pepperonni").on("click", function() {
+  $(".btn-pepperonni").on("click", function(e) {
     pep.toggleClass("invisible");
+    $(this).toggleClass("active");
   });
 
   $(".btn-mushrooms").on("click", function() {
     mushroom.toggleClass("invisible");
+    $(this).toggleClass("active");
   });
 
   $(".btn-green-peppers").on("click", function() {
     greenPepper.toggleClass("invisible");
+    $(this).toggleClass("active");
   });
 
   $(".btn-sauce").on("click", function() {
     sauce.toggleClass("invisible");
+    $(this).toggleClass("active");
   });
 
   $(".btn-crust").on("click", function() {
     cheese.toggleClass("invisible");
+    $(this).toggleClass("active");
   });
 
 });
