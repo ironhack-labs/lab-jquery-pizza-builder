c57c8fc63008e6c1887cc4094b6a9476b3ea7d5d
Rapha Montenegro <raphaelmmontenegro@gmail.com>
2018-03-27 16:07:05 +0200

    almost finished

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -10,17 +10,19 @@ var $mushroomsButton = $(".btn-mushrooms");
 $greenPepperButton.on('click',function (){
   $(this).toggleClass("active");
   $(".green-pepper").toggle();
+  $(".panel.price li:contains(green peppers)").toggle();
 });
 
 $pepperonniButton.on('click', function(){
   $(this).toggleClass("active");
   $(".pep").toggle();
-  //$("li:contains()").text("pepperonni").toggle();
+  $(".panel.price li:contains(pepperonni)").toggle();
 });
 
 $mushroomsButton.on('click', function(){
   $(this).toggleClass("active");
   $(".mushroom").toggle();
+  $(".panel.price li:contains(mushrooms)").toggle();
 });
 
 
@@ -30,12 +32,25 @@ var $crustButton = $(".btn-crust");
 $crustButton.on('click', function() {
   $(this).toggleClass("active")
   $(".crust").toggleClass("crust-gluten-free")
+  $(".panel.price li:contains(crust)").toggle();
 });
 
 $sauceButton.on('click', function(){
   $(this).toggleClass("active");
   $(".sauce").toggleClass("sauce-white")
+  $(".panel.price li:contains(sauce)").toggle();
 })
+
+/* Byron no nos dejo acabar esto
+var pepperonniPrice = 1;
+var mushroomPrice = 1;
+var greenPepperPrice = 1;
+var whiteSaucePrice = 3;
+var glutenFreePrice = 5;
+
+var $finalPrice = $(".panel.price strong").
+*/
+
 
 
 
