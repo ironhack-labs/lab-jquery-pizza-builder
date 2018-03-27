67fa4536f94f23fa9b4f4301b9c60032ef63f8d8
muflonex <muflonex@gmail.com>
2018-03-27 16:38:54 +0200

    Mecanismo preparado para hacer 4ta parte

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,32 @@
 // Write your Pizza Builder JavaScript in this file.
+$('document').ready(function(){
+  var pepBtn    = $('.btn-pepperonni')
+  var mushBtn   = $('.btn-mushrooms')
+  var grnPepBtn = $('.btn-green-peppers')
+  var sauceBtn  = $('.btn-sauce')
+  var crustBtn  = $('.btn-crust')
+  var pep       = $('.pep')
+  var grnPep    = $('.green-pepper')
+  var mush      = $('.mushroom')
+  var sauce     = $('.sauce-white')
+  var crust     = $('.crust')
+  var active    = $('.active')
+
+  crust.removeClass('crust-gluten-free')
+
+  mushBtn.click(function(){
+    $(this).toggleClass('active')
+    mush.toggle()})
+  pepBtn.click(function(){
+    $(this).toggleClass('active')
+    pep.toggle()})
+  grnPepBtn.click(function(){
+    $(this).toggleClass('active')
+    grnPep.toggle()})
+  sauceBtn.click(function(){
+    $(this).toggleClass('active')
+    sauce.toggle()})
+  crustBtn.click(function(){
+    $(this).toggleClass('active')
+    crust.toggleClass('crust-gluten-free')})
+})
