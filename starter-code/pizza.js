74e89e2e479c3c34a4121cd97089dece454b1a04
Dauv Bafna <dauvbafna1@gmail.com>
2018-03-27 16:26:02 +0200

    BCN Samantha Jordi Dauv

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,26 @@
 // Write your Pizza Builder JavaScript in this file.
+$( ".btn.btn-pepperonni.active" ).click(function() {
+  $('.pep').toggle();
+  $('.btn.btn-pepperonni').toggleClass("active");
+});
+
+$( ".btn.btn-green-peppers.active" ).click(function() {
+  $('.green-pepper').toggle();
+  $('.btn.btn-green-peppers').toggleClass("active");
+});
+
+$( ".btn.btn-mushrooms.active" ).click(function() {
+  $('.mushroom').toggle();
+  $('.btn.btn-mushrooms').toggleClass("active");
+
+});
+
+$( ".btn.btn-sauce" ).click(function() {
+  $('.sauce').toggleClass("sauce-white");
+  $('.btn.btn-sauce').toggleClass("active");
+});
+
+$( ".btn.btn-crust" ).click(function() {
+  $('.crust').toggleClass("crust-gluten-free");
+  $('.btn.btn-crust').toggleClass("active");
+});
