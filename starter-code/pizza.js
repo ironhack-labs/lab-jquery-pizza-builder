53cbf1a0aa7caf0fcb350ccc1d512213cebabc93
aacecandev <aacecan.dev@gmail.com>
2018-03-27 16:11:53 +0200

    Iteracion 3 completada

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,23 @@
-// Write your Pizza Builder JavaScript in this file.
+// PIZZA & BUTTONS RESET
+
+$("#pizza > section:not(:last-child").toggleClass("hide");
+$("#pizza section:last-child").toggleClass("crust-gluten-free sauce-white");
+$(".btn").toggleClass("active");
+
+// ADD TOPINGS TO PIZZA
+
+$(".btn-pepperonni").on("click", function(){
+  $("#pizza > section.pep").toggleClass("hide");
+
+});
+
+$(".btn-mushrooms").on("click", function(){
+  $("#pizza > section.mushroom").toggleClass("hide");
+});
+
+$(".btn-green-peppers").on("click", function(){
+  $("#pizza > section.green-pepper").toggleClass("hide");
+});
+
+
+
