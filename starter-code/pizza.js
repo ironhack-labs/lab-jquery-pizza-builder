6ae93ae60123ceb34a97768b440d3e143403cc5d
luisnueda <nueda.luis@gmail.com>
2018-03-27 17:54:16 +0200

    iteración 4 completada

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -3,40 +3,54 @@
 $().ready(function() {
   //Dejamos la pizza vacia
   $(".pep").hide();
+  $("li:contains('$1 pepperonni')").hide();
+
   $(".mushroom").hide();
+  $("li:contains('$1 mush')").hide();
+
   $(".green-pepper").hide();
+  $("li:contains('$1 green')").hide();
 
   $(".crust").removeClass("crust-gluten-free");
   $(".sauce").removeClass("sauce-white");
 
   $("button").removeClass("active");
-
   encendido()
-  
+
+  $("li:contains('$3')").attr("id","masa").hide();
+  $("li:contains('$5')").attr("id","salsa").hide();
+  $("strong").hide();
+
   /* Iteración 1 */
 
   // Peperoni
 
   $(".btn-pepperonni").click(function() {
     $(".pep").fadeToggle(10);
+    $("li:contains('$1 pepperonni')").fadeToggle();
   });
   // Mushrooms
   $(".btn-mushrooms").click(function() {
     $(".mushroom").fadeToggle(10);
+    $("li:contains('$1 mush')").fadeToggle(10);
   });
   //Peper
   $(".btn-green-peppers").click(function() {
     $(".green-pepper").fadeToggle(10);
+    $("li:contains('$1 green')").fadeToggle(10);
   });
 
   /* Iteración 2 */
 
   $(".btn-crust").click(function() {
     $(".crust").toggleClass("crust-gluten-free");
+    $("#masa").fadeToggle();
+    
   });
 
   $(".btn-sauce").click(function() {
     $(".sauce").toggleClass("sauce-white");
+    $("#salsa").fadeToggle();
   });
 });
 
@@ -45,3 +59,5 @@ function encendido(){
     $(this).toggleClass("active");
   })
 };
+
+// base price $10, and the price for the pepperonni, mushrooms, and green pepper.
