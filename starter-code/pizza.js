c9596922757338fc21f219a8ff0330a3826b65b0
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-03-27 16:23:39 +0200

    almost done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,66 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+
+    //Pepperonni
+    $(".btn-pepperonni").on("click", function(){
+        pepperonni();
+        toggleButton(".btn-pepperonni")
+    });
+    //Mushrooms
+    $(".btn-mushrooms").on("click", function(){
+       mushrooms();
+       toggleButton(".btn-mushrooms")
+    });
+    //Green-pepper
+    $(".btn-green-peppers").on("click", function(){
+        greenPepper();
+        toggleButton(".btn-green-peppers")
+    });
+
+    //Add and remove toppings
+    function pepperonni(){
+        $(".pep").fadeToggle(200);
+    }
+
+    function mushrooms(){
+        $(".mushroom").fadeToggle(200);
+    }
+
+    function greenPepper(){
+        $(".green-pepper").fadeToggle(200);
+    }
+
+
+    //Sauce and crust options
+    function crustGluten(){
+        $(".crust").toggleClass("crust-gluten-free");
+    }
+    function sauceWhite(){
+        $(".sauce").toggleClass("sauce-white");
+    }
+
+    $(".crust").removeClass("crust-gluten-free");
+    $(".btn-crust").removeClass("active");
+    $(".sauce").removeClass("sauce-white");
+    $(".btn-sauce").removeClass("active");
+
+    $(".btn-crust").on("click", function(){
+        crustGluten();
+        toggleButton(".btn-crust");
+    });
+
+    $(".btn-sauce").on("click", function(){
+        sauceWhite();
+        toggleButton(".btn-sauce");
+    });
+    
+
+    //Change the button's state
+    function toggleButton(receivedClass){
+        $(receivedClass).toggleClass("active");
+    }
+
+    //Ingredients and prices
+    $(".panel.price > ul").hide("li");
+
+});
