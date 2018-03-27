ab68fc8a5692a2bea100a2f03d9c7637c907f4c7
Jorge Terol <jorgetd92@gmail.com>
2018-03-27 16:06:58 +0200

    BCN - Ioana - N & Jorge - D

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,63 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function () {
+
+    var $greenPepper = $('.green-pepper');
+    var $mushroom = $('.mushroom');
+    var $pep = $('.pep');
+
+
+    $('.btn-green-peppers').on('click', greenPepper);
+    $('.btn-mushrooms').on('click', mushrooms);
+    $('.btn-pepperonni').on('click', pepperonni);
+
+
+
+    function greenPepper() {
+        $('.green-pepper').toggle();
+    }
+    function mushrooms() {
+        $('.mushroom').toggle();
+    }
+
+    function pepperonni() {
+        $('.pep').toggle();
+    }
+
+    $('.btn-sauce').on('click', whiteSauce);
+    
+    
+    function whiteSauce() {
+        var sauceName = $('.btn-sauce').text();
+        
+        if (sauceName === 'White sauce') {
+            $('.sauce').removeClass('sauce-white')
+            $('.btn-sauce').text('Regular sauce')
+        }
+        else {
+            $('.sauce').addClass('sauce-white')
+            $('.btn-sauce').text('White sauce')
+            
+        }
+        
+
+        
+     }
+
+    function crustFunction() {
+        var crustName = $('.btn-crust').text();
+
+        if (crustName === 'Gluten-free crust') {
+            $('.crust').removeClass('crust-gluten-free')
+            $('.btn-crust').text('Crust')
+        }
+
+        else {
+            $('.crust').addClass('crust-gluten-free')
+            $('.btn-crust').text('Gluten-free crust')
+        }
+
+    }
+
+    $('.btn-crust').on('click', crustFunction);
+
+});
