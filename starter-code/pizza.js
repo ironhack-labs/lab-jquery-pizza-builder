745ad6ca622b270a3ccaeac28ff10004e8388f1c
luisnueda <nueda.luis@gmail.com>
2018-03-27 15:50:32 +0200

    iteración 1 completada

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,3 +1,18 @@
 // Write your Pizza Builder JavaScript in this file.
 
+$().ready(function() {
+  // Peperoni
+  $(".btn-pepperonni").click(function(){
+    $(".pep").fadeToggle(10);
+  })
+  // Mushrooms
+  $(".btn-mushrooms").click(function(){
+    $(".mushroom").fadeToggle(10);
+  })
+  //Peper
+  $(".btn-green-peppers").click(function(){
+    $(".green-pepper").fadeToggle(10);
+  })
+
+});
 
