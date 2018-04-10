dd9e55b96b58af68d8c0c462ee1a62fed05e413a
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-04-10 17:56:08 -0400

    WDFT-MIA-Claudia-Hernandez-Lab-Jquery-pizza-builder

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,93 @@
-// Write your Pizza Builder JavaScript in this file.
+$( document ).ready(function() {
+$(".active").toggleClass("active");
+
+$(".crust").removeClass("crust-gluten-free")
+
+$(".sauce").removeClass("sauce-white");
+
+$(".price li").toggle();
+
+$(".crust").click(function(event) {
+  //<-----------default
+});
+
+var totalPrice = 10;
+$("strong").html("$" + totalPrice);
+
+
+//<-----------green-pepper adds&removes
+
+$(".green-pepper").toggle();
+$(".btn-green-peppers").click(function(event) {
+  $(".green-pepper").toggle();
+  $(".btn-green-peppers").toggleClass("active");
+  $(".price li:nth-child(3)").toggle();
+if($(".btn-green-peppers").hasClass('active')) {
+  totalPrice += 1
+} else {
+  totalPrice -= 1
+}
+$("strong").html("$" + totalPrice);
+
+//<----use for if/else statement in each ingredient https://stackoverflow.com/questions/5345374/jquery-hasclass-in-if-statement-not-playing-nicely 
+});
+//<-----------mushrooms adds&removes
+
+$(".mushroom").toggle();
+$(".btn-mushrooms").click(function(event) {
+  $(".mushroom").toggle();
+  $(".btn-mushrooms").toggleClass("active");
+  $(".price li:nth-child(2)").toggle();
+  if($(".btn-mushrooms").hasClass('active')) {
+    totalPrice += 1
+  } else {
+    totalPrice -= 1
+  }
+  $("strong").html("$" + totalPrice);
+
+});
+//<-----------pepp adds&removes
+
+$(".pep").toggle();
+$(".btn-pepperonni").click(function(event) {
+  $(".pep").toggle();
+  $(".btn-pepperonni").toggleClass("active");
+  $(".price li:first").toggle();
+  if($(".btn-pepperonni").hasClass('active')) {
+    totalPrice += 1
+  } else {
+    totalPrice -= 1
+  }
+  $("strong").html("$" + totalPrice);
+});
+
+//<----------- option
+
+$(".btn-sauce").click(function(event) {
+  $(".sauce").toggleClass("sauce-white");
+  $(".btn-sauce").toggleClass("active");
+  $(".price li:nth-child(4)").toggle();
+  if($(".btn-sauce").hasClass('active')) {
+    totalPrice += 3
+  } else {
+    totalPrice -= 3
+  }
+  $("strong").html("$" + totalPrice);
+});
+
+//<-----------crust
+
+$(".btn-crust").click(function(event) {
+    $(".crust").toggleClass("crust-gluten-free");
+    $(".btn-crust").toggleClass("active");
+    $(".price li:last").toggle();
+    if($(".btn-crust").hasClass('active')) {
+      totalPrice += 5
+    } else {
+      totalPrice -= 5
+    }
+    $("strong").html("$" + totalPrice);
+    
+});
+
+});
