e3ff99198544c11090425fd898e0ccec9aefe7db
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-04-22 16:19:24 -0400

    Add functionality to toppings buttons, add visibility class css

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,13 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$('.btn-pepperonni').on('click', () => {
+  $(".pep").toggleClass('hidden');
+});
+
+$('.btn-mushrooms').on('click', () => {
+  $(".mushroom").toggleClass('hidden');
+});
+
+$('.btn-green-peppers').on('click', () => {
+  $(".green-pepper").toggleClass('hidden');
+});
