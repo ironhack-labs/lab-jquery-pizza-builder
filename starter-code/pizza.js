6537d82b3778c5b3412362fdcfe090efc5e2487c
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-04-23 16:30:20 -0400

    Toggle ingredient buttons, remove arrow functions

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,26 +1,31 @@
 // Write your Pizza Builder JavaScript in this file.
 
 // Add/remove pepperonni topping
-$('.btn-pepperonni').on('click', () => {
+$('.btn-pepperonni').on('click', function () {
   $(".pep").toggleClass('hidden');
+  $(this).toggleClass('active');
 });
 
 // Add/remove mushroom topping
-$('.btn-mushrooms').on('click', () => {
+$('.btn-mushrooms').on('click', function () {
   $(".mushroom").toggleClass('hidden');
+  $(this).toggleClass('active');
 });
 
 // Add/remove green pepper topping
-$('.btn-green-peppers').on('click', () => {
+$('.btn-green-peppers').on('click', function () {
   $(".green-pepper").toggleClass('hidden');
+  $(this).toggleClass('active');
 });
 
 // Change from regular to white sauce
-$('.btn-sauce').on('click', () => {
+$('.btn-sauce').on('click', function () {
   $('.sauce').toggleClass('sauce-white');
+  $(this).toggleClass('active');
 });
 
 // Change from regular to gluten free crust
-$('.btn-crust').on('click', () => {
+$('.btn-crust').on('click', function () {
   $('.crust').toggleClass('crust-gluten-free');
+  $(this).toggleClass('active');
 });
