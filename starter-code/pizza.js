0190716684f166f00ca706263160b292e3982611
ahmouda12 <ahmouda12@ufl.edu>
2018-04-23 20:24:51 -0400

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,69 @@
-// Write your Pizza Builder JavaScript in this file.
+// Remove active class and hide toppings and their li sections 
+// when first load the pizza builder
+$(".btn-pepperonni").removeClass("active");
+$('.pep').hide();
+$("li:contains('$1 pepperonni')").hide();
+$(".btn-mushrooms").removeClass("active");
+$('.mushroom').hide();
+$("li:contains('$1 mushrooms')").hide();
+$(".btn-green-peppers").removeClass("active");
+$('.green-pepper').hide();
+$("li:contains('$1 green peppers')").hide();
+
+// Add, remove, activate, and update price for pepperonni topping
+$(".btn-pepperonni").on("click", function () {
+  $(".pep").toggle();
+  $(".btn-pepperonni").toggleClass("active");
+  $("li:contains('pepperonni')").toggle();
+  totalPrice()
+});
+
+// Add, remove, activate, and update price for mushroom topping
+$(".btn-mushrooms").on("click", function () {
+  $(".mushroom").toggle();
+  $(".btn-mushrooms").toggleClass("active");
+  $("li:contains('mushrooms')").toggle();
+  totalPrice()
+});
+
+// Add, remove, activate, and update price for green pepper topping
+$(".btn-green-peppers").on("click", function () {
+  $(".green-pepper").toggle();
+  $(".btn-green-peppers").toggleClass("active");
+  $("li:contains('green peppers')").toggle();
+  totalPrice()
+});
+// Add, remove, activate, and update price for white sauce
+$(".btn-sauce").on("click", function () {
+  $(".sauce").toggle();
+  $(".btn-sauce").toggleClass("active");
+  $("li:contains('white sauce')").toggle();
+  totalPrice()
+});
+// Add, remove, activate, and update price for Gluten-free crust
+$(".btn-crust").on("click", function () {
+  $(".btn-crust").toggleClass("active");
+  $("li:contains('gluten-free crust')").toggle();
+  totalPrice()
+});
+
+// Calculate total price
+function totalPrice(){
+ let totalPrice = 10;
+ if ($(".btn-pepperonni").hasClass("active")){
+   totalPrice += 1;
+ }
+ if ($(".btn-mushrooms").hasClass("active")){
+  totalPrice += 1;
+}
+ if ($(".btn-green-peppers").hasClass("active")){
+  totalPrice += 1;
+}
+ if ($(".btn-sauce").hasClass("active")){
+  totalPrice += 3;
+}
+ if ($(".btn-crust").hasClass("active")){
+  totalPrice += 5;
+}
+ $("aside > strong").text("$" + totalPrice);
+}
