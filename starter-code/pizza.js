291c63732b78cae4196b691ebf905b4e4d61d319
Georgina Pacey <paceygeorgina@gmail.com>
2018-05-16 23:11:34 +0200

    first iteration

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,23 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+    $(".btn-pepperonni").click(function(){
+        //$(".pep").toggleClass("hide");
+        //var $hide = $(".green-pepper").hasClass("hide");
+        $(".pep").toggle();
+
+    });
+
+    $(".btn-mushrooms").click(function(){
+        $(".mushroom").toggleClass("hide");
+        //var $hide = $(".green-pepper").hasClass("hide");
+        $(".mushroom.hide").hide();
+
+    });
+
+    $(".btn-green-peppers").click(function(){
+        $(".green-pepper").toggleClass("hide");
+        //var $hide = $(".green-pepper").hasClass("hide");
+        $(".green-pepper.hide").hide();
+
+    });
+});
