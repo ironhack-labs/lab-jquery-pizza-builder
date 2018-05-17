eea072f6bf5429971367d794d5906d1db4930bcf
Alma Alonso <alma.aa93@gmail.com>
2018-05-17 22:03:34 +0200

    ex in progress

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -2,10 +2,13 @@
 $(document).ready(function(){
     $(".crust").removeClass("crust-gluten-free");
     $(".btn-crust").removeClass("active");
+    $(".price li:contains('$5 gluten-free crust')").hide();
+   
 
     $(".sauce").removeClass("sauce-white");
     $(".sauce").addClass("regular-sauce");
     $(".btn-sauce").removeClass("active");
+    $(".price li:contains('$3 white sauce')").hide();
 
     $(".btn-sauce").click(function(){
         if ($(".sauce").hasClass("sauce-white")) {
@@ -23,18 +26,40 @@ $(document).ready(function(){
 
     $(".btn-pepperonni").click(function(){
         $(".pep").toggle();
+        $(".price li:contains('pepperonni')").toggle();
     });
 
     $(".btn-mushrooms").click(function(){
         $(".mushroom").toggle();
+        $(".price li:contains('mushrooms')").toggle();
     });
 
     $(".btn-green-peppers").click(function(){
         $(".green-pepper").toggle();
+        $(".price li:contains('peppers')").toggle();
     });
 
     $(".btn").click(function(){
         $(this).toggleClass("active");
+        updatePrice();
     });
+
+    function updatePrice(){
+        var sum =  Number(getPriceFromString ($(".price b").text()));
+        
+        var hijos = $(".price ul").children(":visible");
+        
+        for (var i = 0; i < hijos.length; i++) {
+            sum += getPriceFromString(hijos[i].innerText);
+        }
+
+        $(".price strong").text(sum);
+    }
+
+    function getPriceFromString(priceString){
+        return Number(priceString.split(" ")[0].slice(1));
+    }
 })
+
+
 
