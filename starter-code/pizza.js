6839496a0bf7b0d18675bc79af50016bb923c93e
Alma Alonso <alma.aa93@gmail.com>
2018-05-17 18:28:29 +0200

    exercise in progress

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,40 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+    $(".crust").removeClass("crust-gluten-free");
+    $(".btn-crust").removeClass("active");
+
+    $(".sauce").removeClass("sauce-white");
+    $(".sauce").addClass("regular-sauce");
+    $(".btn-sauce").removeClass("active");
+
+    $(".btn-sauce").click(function(){
+        if ($(".sauce").hasClass("sauce-white")) {
+            $(".sauce").removeClass("sauce-white");
+            $(".sauce").addClass("regular-sauce");
+        } else {
+            $(".sauce").addClass("sauce-white");
+            $(".sauce").removeClass("regular-sauce");
+        }
+    });
+
+    $(".btn-crust").click(function(){
+        $(".crust").toggleClass("crust-gluten-free");
+    });
+
+    $(".btn-pepperonni").click(function(){
+        $(".pep").toggle();
+    });
+
+    $(".btn-mushrooms").click(function(){
+        $(".mushroom").toggle();
+    });
+
+    $(".btn-green-peppers").click(function(){
+        $(".green-pepper").toggle();
+    });
+
+    $(".btn").click(function(){
+        $(this).toggleClass("active");
+    });
+})
+
