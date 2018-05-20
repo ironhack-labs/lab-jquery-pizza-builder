a7c21f429269c04d920097f4e2bb6e51617728f5
Georgina Pacey <paceygeorgina@gmail.com>
2018-05-20 23:30:36 +0200

    iteration 2

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,23 +1,78 @@
 // Write your Pizza Builder JavaScript in this file.
 $(document).ready(function(){
     $(".btn-pepperonni").click(function(){
-        //$(".pep").toggleClass("hide");
-        //var $hide = $(".green-pepper").hasClass("hide");
         $(".pep").toggle();
+        $(".pep-price").toggle();
+
 
     });
 
     $(".btn-mushrooms").click(function(){
-        $(".mushroom").toggleClass("hide");
-        //var $hide = $(".green-pepper").hasClass("hide");
-        $(".mushroom.hide").hide();
+        $(".mushroom").toggle();
+        $(".mushrooms-price").toggle();
+
 
     });
 
     $(".btn-green-peppers").click(function(){
-        $(".green-pepper").toggleClass("hide");
-        //var $hide = $(".green-pepper").hasClass("hide");
-        $(".green-pepper.hide").hide();
+        $(".green-pepper").toggle();
+        $(".peppers-price").toggle();
+
 
     });
+
+    //crust and sauce
+    $(".btn-sauce").click(function(){
+        $(".sauce").toggleClass("sauce-white");
+        $(".sauce-price").toggle();
+
+
+    });
+
+    $(".btn-crust").click(function(){
+        $(".crust").toggleClass("crust-gluten-free");
+        $(".crust-price").toggle();
+
+
+    });
+
+    //toggle button active state
+    $(".btn").click(function(){
+        $(this).toggleClass("active");
+
+    });
+
 });
+
+
+    //hide or show price depending on button state
+
+    if ($(".btn-crust").hasClass("active")) {
+        $(".crust-price").show();
+    } else {
+        $(".crust-price").hide();
+    }
+
+    if ($(".btn-pepperonni").hasClass("active")) {
+        $(".pep-price").show();
+    } else {
+        $(".pep-price").hide();
+    }
+
+    if ($(".btn-mushrooms").hasClass("active")) {
+        $(".mushrooms-price").show();
+    } else {
+        $(".mushrooms-price").hide();
+    }
+
+    if ($(".btn-green-peppers").hasClass("active")) {
+        $(".peppers-price").show();
+    } else {
+        $(".peppers-price").hide();
+    }
+
+    if ($(".btn-sauce").hasClass("active")) {
+        $(".sauce-price").show();
+    } else {
+        $(".sauce-price").hide();
+    }
