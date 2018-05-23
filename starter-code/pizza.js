19cf561444fe6be8727de0a06d74a88354d70266
Georgina Pacey <paceygeorgina@gmail.com>
2018-05-23 22:12:07 +0200

    clean up code

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,80 +1,63 @@
-    // Write your Pizza Builder JavaScript in this file.
-    $(document).ready(function(){
+// Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
 
-        // remove specific class names
-        $(".crust").removeClass("crust-gluten-free");
-        $(".sauce").removeClass("sauce-white");
+    // remove specific class names
+    $(".crust").removeClass("crust-gluten-free");
+    $(".sauce").removeClass("sauce-white");
 
-        // remove active for crust and sauce
-        $(".btn-crust, .btn-sauce").removeClass("active");
+    // remove active for crust and sauce
+    $(".btn-crust, .btn-sauce").removeClass("active");
 
-        //hide sauce and crust price
-        $(".crust-price, .sauce-price").hide();
+    //hide sauce and crust price
+    $(".crust-price, .sauce-price").hide();
 
-        //hide and show ingredient and price
-        //repeated functions below, maybe we can declare one function and call it many times?
+    //calculate price onload and below onclick too
+    getPrice();
 
-        getPrice();
+    // calling 1 function 3 times so don't repeat code, but the second parameter is weird
+    ingredientsToggle(".btn-pepperonni", ".pep", ".pep-price");
+    ingredientsToggle(".btn-mushrooms", ".mushroom", ".mushroom-price");
+    ingredientsToggle(".btn-green-peppers", ".green-pepper", ".pepper-price");
+    //seperate function for crust and sauce so I can toggle the class name
+    ingredientsToggleWithClass(".btn-sauce", ".sauce", ".sauce-price", "sauce-white");
+    ingredientsToggleWithClass(".btn-crust", ".crust", ".crust-price", "crust-gluten-free");
 
-        $(".btn-pepperonni").click(function(){
-            $(".pep, .pep-price").toggle();
-            getPrice();
-        });
-
-        $(".btn-mushrooms").click(function(){
-            $(".mushroom, .mushroom-price").toggle();
-            getPrice();
-        });
-
-        $(".btn-green-peppers").click(function(){
-            $(".green-pepper, .pepper-price").toggle();
-            getPrice();
-        });
-
-        //crust and sauce
-        $(".btn-sauce").click(function(){
-            $(".sauce").toggleClass("sauce-white");
-            $(".sauce-price").toggle();
-            getPrice();
-        });
-
-        $(".btn-crust").click(function(){
-            $(".crust").toggleClass("crust-gluten-free");
-            $(".crust-price").toggle();
-            getPrice();
-        });
-
-        //toggle button active state
-        $(".btn").click(function(){
-            $(this).toggleClass("active");
-
-        });
-
-    
-
-
-
-        
-
+    //toggle button active state
+    $(".btn").click(function(){
+        $(this).toggleClass("active");
     });
 
-//update price total 
-//get individual price from text:
-//need to calculate properly
-function getPrice(){
+});
 
+//how could I use three parameters, button, ingredient & price, and then "ingredient", "price"
+//hide and show ingredient and price
+function ingredientsToggle(button, ingredient, price) {
+    $(button).click(function(){
+        $(ingredient).toggle();
+        $(price).toggle();
+        getPrice();
+    });
+}
+
+//improve: make ingredientsToggle and ingredientsToggleWithClass into one function??
+function ingredientsToggleWithClass(button, ingredient, price, toggleClass) {
+    $(button).click(function(){
+        $(ingredient).toggleClass(toggleClass);
+        $(price).toggle();
+        getPrice();
+    });
+}
+
+function getPrice(){
+//pizza prce 
     var total = 10;
 
-
     $(".item-price:visible").each(function( index ) {
+        //get individual price from text:
         numberPrice = Number($( this ).text().split(" ")[0].replace("$", ""));
-
         total +=numberPrice;
-
-
     });
 
-    $(".panel.price strong").text(total)
-
+    $(".panel.price strong").text("$" + total)
 
 }
