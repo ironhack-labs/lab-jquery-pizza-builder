27a5db782b4106a1e35ca6265a4d317866ad015e
Marta Soler <marta@MacBook-Air-de-Barrabes.local>
2018-05-27 21:09:33 +0200

    Iterations 1,2,3

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,33 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+    $(".btn-green-peppers").click(function(event) {
+        $(".green-pepper").toggleClass("hidden");
+        $(".btn-green-peppers").toggleClass("active");
+    });
+
+    $(".btn-mushrooms").click(function(event) {
+        $(".mushroom").toggleClass("hidden");
+        $(".btn-mushrooms").toggleClass("active");
+    });
+
+    $(".btn-pepperonni").click(function(event) {
+        $(".pep").toggleClass("hidden");
+        $(".btn-pepperonni").toggleClass("active");
+    });
+
+    $(".crust").removeClass("crust-gluten-free");
+    $(".btn-crust").removeClass("active");
+
+    $(".btn-crust").click(function(event) {
+        $(".crust").toggleClass("crust-gluten-free");
+        $(".btn-crust").toggleClass("active");
+    });
+
+    $(".sauce").removeClass("sauce-white");
+    $(".btn-sauce").removeClass("active");
+
+    $(".btn-sauce").click(function(event) {
+        $(".sauce").toggleClass("sauce-white");
+        $(".btn-sauce").toggleClass("active");
+    });
+});
