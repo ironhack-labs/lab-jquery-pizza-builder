7644ab07dfcad4099e3000e02f3814d06d4e3df4
Chris Manthey <calined@gmx.de>
2018-05-29 14:33:57 +0200

    now the sauce and crust toggle properly

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,10 +1,14 @@
 // Write your Pizza Builder JavaScript in this file.
 $(() => {
 
+    $(".sauce").removeClass("sauce-white");
+    $(".crust").removeClass("crust-gluten-free");
+
     $(".btn-pepperonni").click(() => $(".pep").toggle());
     $(".btn-mushrooms").click(() => $(".mushroom").toggle());
     $(".btn-green-peppers").click(() => $(".green-pepper").toggle());
 
-
+    $(".btn-sauce").click(() => $(".sauce").toggleClass("sauce-white"));
+    $(".btn-crust").click(() => $(".crust").toggleClass("crust-gluten-free"));
 
 })
