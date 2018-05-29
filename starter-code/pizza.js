25f6c772393b6531a87d84075aa8bb27a3faa6ab
Chris Manthey <calined@gmx.de>
2018-05-29 14:59:06 +0200

    now the price list adds and removes ingredients

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,17 +1,41 @@
 // Write your Pizza Builder JavaScript in this file.
 $(() => {
 
-    $(".btn").removeClass("active");
+    $(".price li:contains(white sauce)").hide();
+    $(".price li:contains(gluten-free crust)").hide();
+
+    $(".btn-sauce").removeClass("active");
+    $(".btn-crust").removeClass("active");
 
     $(".sauce").removeClass("sauce-white");
     $(".crust").removeClass("crust-gluten-free");
 
-    $(".btn-pepperonni").click(() => $(".pep").toggle());
-    $(".btn-mushrooms").click(() => $(".mushroom").toggle());
-    $(".btn-green-peppers").click(() => $(".green-pepper").toggle());
+    $(".btn-pepperonni").click(function () {
+        $(".pep").toggle();
+        $(".price li:contains(pepperonni)").toggle();
 
-    $(".btn-sauce").click(() => $(".sauce").toggleClass("sauce-white"));
-    $(".btn-crust").click(() => $(".crust").toggleClass("crust-gluten-free"));
+
+    });
+
+    $(".btn-mushrooms").click(function () {
+        $(".mushroom").toggle();
+        $(".price li:contains(mushrooms)").toggle();
+    });
+
+    $(".btn-green-peppers").click(function () {
+        $(".green-pepper").toggle();
+        $(".price li:contains(green peppers)").toggle();
+    });
+
+    $(".btn-sauce").click(function () {
+        $(".sauce").toggleClass("sauce-white");
+        $(".price li:contains(white sauce)").toggle();
+    });
+
+    $(".btn-crust").click(function () {
+        $(".crust").toggleClass("crust-gluten-free");
+        $(".price li:contains(gluten-free crust)").toggle();
+    });
 
     $(".btn").click(function () { $(this).toggleClass("active"); });
 
