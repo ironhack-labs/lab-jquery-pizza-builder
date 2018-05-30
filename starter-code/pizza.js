8091bd62141ee57449048f3cbf0b28576d3c707a
Hannes Reinsch <hannes.reinsch99@gmail.com>
2018-05-30 13:07:44 +0200

    BER-Hannes-Reinsch

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -2,6 +2,9 @@
 
 $(document).ready(function() {
   
+  //Default price
+  var sumNum = 13
+  $(".panel.price strong").replaceWith("<strong>$"+sumNum+"</strong>")
   
 //make buttons active by default
   $(".btn-pepperonni").removeClass("active");
@@ -13,18 +16,36 @@ $(document).ready(function() {
     $(this).toggleClass("active");
     $(".pep").toggle();
     $(".price li:nth-of-type(1)").toggle();
+    if ($(".panel.controls ul li:nth-child(1) button").hasClass("active")) {
+      $("body aside strong").html("$"+parseInt(--sumNum))
+    }
+    else {
+      $("body aside strong").html("$"+parseInt(++sumNum))
+    }
   });
 
   $(".btn-mushrooms").on("click", function() {
     $(this).toggleClass("active");
     $(".mushroom").toggle();
     $(".price li:nth-of-type(2)").toggle();
+    if ($(".panel.controls ul li:nth-child(2) button").hasClass("active")) {
+      $("body aside strong").html("$"+parseInt(--sumNum))
+    }
+    else {
+      $("body aside strong").html("$"+parseInt(++sumNum))
+    }
   });
 
   $(".btn-green-peppers").on("click", function() {
     $(this).toggleClass("active");
     $(".green-pepper").toggle();
     $(".price li:nth-of-type(3)").toggle();
+    if ($(".panel.controls ul li:nth-child(3) button").hasClass("active")) {
+      $("body aside strong").html("$"+parseInt(--sumNum))
+    }
+    else {
+      $("body aside strong").html("$"+parseInt(++sumNum))
+    }
   });
 
   //white sauce + crust
@@ -39,29 +60,25 @@ $(document).ready(function() {
     $(this).toggleClass("active");
     $(".sauce").toggleClass("sauce-white", "sauce");
     $(".price li:nth-of-type(4)").toggle();
+    if ($(".panel.controls ul li:nth-child(4) button").hasClass("active")) {
+      $("body aside strong").html("$"+parseInt(sumNum-=3))
+    }
+    else {
+      $("body aside strong").html("$"+parseInt(sumNum+=3))
+    }
   });
 
   $(".btn-crust").on("click", function() {
     $(this).toggleClass("active");
     $(".crust").toggleClass("crust-gluten-free", "crust");
     $(".price li:nth-of-type(5)").toggle();
+    if ($(".panel.controls ul li:nth-child(5) button").hasClass("active")) {
+      $("body aside strong").html("$"+parseInt(sumNum-=5))
+    }
+    else {
+      $("body aside strong").html("$"+parseInt(sumNum+=5))
+    }
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
