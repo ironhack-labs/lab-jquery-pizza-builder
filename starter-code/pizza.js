236f1ab11de22ed4f0f4502f0402f9baba0f336d
Roberta Conrad <rjcnrd@gmail.com>
2018-05-30 08:21:39 +0200

    last iteration missing

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,53 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+
+$(".btn-pepperonni").removeClass("active");
+$(".btn-green-peppers").removeClass("active");
+$(".btn-mushrooms").removeClass("active");
+
+//remove toppings when clicked on buttons
+
+$(".btn-pepperonni").on("click", function() {
+  $(this).toggleClass("active");
+  $(".pep").toggle();
+  $(".price li:contains(pepperonni)").toggle()
+  });
+
+$(".btn-mushrooms").on("click", function() {
+  $(this).toggleClass("active");
+  $(".mushroom").toggle();
+  $(".price li:contains(mushrooms)").toggle()
+  })  
+
+$(".btn-green-peppers").on("click", function() {
+  $(this).toggleClass("active");
+  $(".green-pepper").toggle();
+  $(".price li:contains(peppers)").toggle();
+  });
+
+//white sauce + crust not shown by default
+$(".sauce").removeClass("sauce-white");
+$(".price li:contains(white)").hide();
+
+$(".crust").removeClass("crust-gluten-free");
+$(".price li:contains(crust)").hide();
+
+//when button clicked
+$(".btn-sauce").on("click", function() {
+  $(this).toggleClass("active");
+  $(".sauce").toggleClass("sauce-white", "sauce");
+  $(".price li:contains(white)").toggle();
+});
+
+$(".btn-crust").on("click", function() {
+  $(this).toggleClass("active");
+  $(".crust").toggleClass("crust-gluten-free", "crust");
+  $(".price li:contains(crust)").toggle();
+});
+
+$("strong").text("$13")
+
+//add or remove three if white sauce is added
+
+});
+
