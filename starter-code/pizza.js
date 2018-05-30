8004c68c7802db9e204427b8d19c1b2692f8f7b5
aparna pattathil <aparnapattathil@hotmail.com>
2018-05-30 14:46:35 +0200

    almost done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,38 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+
+    $('.crust').removeClass('crust-gluten-free');
+    $('.sauce').removeClass('sauce-white');  
+    $('.pep, .mushroom, .green-pepper').hide();
+    // $('.mushroom').hide();
+    // $('.green-pepper').hide();
+    $('.btn-pepperonni, .btn-mushrooms, .btn-green-peppers').removeClass('active');
+
+
+    $(".btn-pepperonni").click(function(){
+        $('.pep').toggle();
+        $(this).toggleClass('active');
+       
+    })
+
+    $(".btn-mushrooms").click(function(){
+        $('.mushroom').toggle();
+        $(this).toggleClass('active');
+       
+    })
+
+    $(".btn-green-peppers").click(function(){
+        $('.green-pepper').toggle();
+        $(this).toggleClass('active');
+       
+    })
+
+    $(".btn-crust").click(function () {
+        $('.crust').toggleClass('crust-gluten-free');
+    })
+
+    $(".btn-sauce").click(function () {
+        $('.sauce').toggleClass('sauce-white');
+    })
+
+})
