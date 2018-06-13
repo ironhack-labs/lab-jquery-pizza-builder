c6750eba0096903c2cc828db18badcfa38aedc7e
Robert V <vivanco.robert@gmail.com>
2018-06-13 20:09:16 +0200

    done2

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,40 +1,15 @@
 // Write your Pizza Builder JavaScript in this file.
-// var btnPepperonni = $('.btn-pepperonni');
-// var displayNone = css('')
 function calTotalPrice() {
-
   var htmlColecIngredients = $('.selected > .value');
-  // console.log("array ingredients:");
-  // var firstEl = htmlColecIngredients[0].textContent;
-  // console.log(firstEl);
-  
-  // console.log(htmlColecIngredients[0]);
-
-  // console.log($('.price-pepperonni > .value').html());
-  
-
   var arrayIngredients = [].slice.call(htmlColecIngredients)   
-  // console.log(arrayIngredients);
-  // console.log(parseInt(arrayIngredients[0].textContent));   
-  // console.log(parseInt(arrayIngredients[1].textContent));   
-  // console.log(parseInt(arrayIngredients[2].textContent)); 
   var finalPrice = 10;
   arrayIngredients.forEach(element => {
     finalPrice += parseInt(element.textContent);
     return finalPrice
   });
-  //Why it doesn't work with reduce ?
-  // var finalPrice = arrayIngredients.reduce((acc, e) => {
-  //   var newAcc = parseFloat(acc.textContent)
-  //   var newE = parseFloat(e.textContent)
-  //   console.log(typeof newAcc);
-  //   console.log(typeof newE);
-  //   return newAcc += newE;
-  // })
   console.log("The finale price is: " + parseInt(finalPrice));
   $('.finalPrice').html(finalPrice);
 }
-// console.log($('.price-pepperonni > .value').html());
 
 //Initialization
 $('.price-pepperonni').addClass('selected');
