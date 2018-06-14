329ad800521d402bd8d59dd5c332168788b3a94e
Crazyunicorn <revire.alexandre@gmail.com>
2018-06-14 14:09:31 +0200

    pizza

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,40 @@
 // Write your Pizza Builder JavaScript in this file.
+
+// $('document').ready(function(){
+
+
+  $(".btn-pepperonni").click(function(){
+      $('.pep').toggle();
+  });
+
+  $(".btn-mushrooms").click(function(){
+      $('.mushroom').toggle();
+  });
+
+  $(".btn-green-peppers").click(function(){
+      $('.green-pepper').toggle();
+  });
+
+
+  $(".btn-sauce").click(function(){
+    if ($('.btn-sauce')hasClass('.sauce-white')){
+      .removeClass('.sauce-white');
+    }else{
+      .addClass('.sauce-white');
+  }
+
+  });
+    $(".btn-crust").click(function(){
+      $('.crust-gluten-free').toggle();
+
+  });
+
+
+
+
+
+
+
+
+
+//});
