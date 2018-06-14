e113bce7dfb0ad84b44caeb4340220177f9dedeb
Pauline Ronez <Pauline.Ronez@weave.eu>
2018-06-14 19:41:50 +0200

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,36 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(".panel price").removeClass("panel price");
+
+$('.btn-green-peppers').click(function(){
+    $(".green-pepper").toggle();
+    $(".btn-green-peppers").toggleClass("active");
+    });
+
+$('.btn-mushrooms').click(function(){
+    $(".mushroom").toggle();
+    $(".btn-mushroom").toggleClass("active");
+    });
+
+$('.btn-pepperonni').click(function(){
+    $(".pep").toggle();
+    
+    });
+
+$('.crust').removeClass('crust-gluten-free');
+$(".sauce").removeClass('sauce-white');
+
+$('.btn-crust').click(function(){
+    $(".crust").toggleClass("crust-gluten-free");
+    });
+    
+$('.btn-sauce').click(function(){
+    $(".sauce").toggleClass("sauce-white");
+    $(".btn").toggleClass("active");
+    });
+
+
+
+
+
+
