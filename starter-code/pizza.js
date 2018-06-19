c1e2920d7463d37c3ddfaebd177463c201a32908
Miguel Rey Mallén <miguelreymallen@gmail.com>
2018-06-19 14:10:09 +0200

    Iteration 5

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,50 +1,53 @@
 // Write your Pizza Builder JavaScript in this file.
 
-$(document).ready(function(){
-
+$(document).ready(function() {
   /*INITIAL SETTINGS */
 
-  $('.green-pepper').hide();
-  $('.mushroom').hide();
-  $('.pep').hide();
-
+  $(".green-pepper").hide();
+  $(".mushroom").hide();
+  $(".pep").hide();
 
   /*ITERATION 1*/
 
-  $('.btn-pepperonni').click(function(){
-    $('.pep').toggleClass('show');
+  $(".btn-pepperonni").click(function() {
+    $(".pep").toggleClass("show");
     $(".panel.price li:nth-child(1)").toggleClass("show");
+    calcPrice($(this), 1);
   });
-  $('.btn-mushrooms').click(function(){
-    $('.mushroom').toggleClass('show');
+  $(".btn-mushrooms").click(function() {
+    $(".mushroom").toggleClass("show");
     $(".panel.price li:nth-child(2)").toggleClass("show");
+    calcPrice($(this), 1);
   });
-  $('.btn-green-peppers').click(function(){
-    $('.green-pepper').toggleClass('show');
+  $(".btn-green-peppers").click(function() {
+    $(".green-pepper").toggleClass("show");
     $(".panel.price li:nth-child(3)").toggleClass("show");
+    calcPrice($(this), 1);
   });
 
   /*ITERATION 2 */
 
-  $('.crust-gluten-free').removeClass("crust-gluten-free");
-  $('.sauce-white').removeClass("sauce-white");
+  $(".crust-gluten-free").removeClass("crust-gluten-free");
+  $(".sauce-white").removeClass("sauce-white");
 
-  $('.btn-sauce').click(function(){
-    $('.sauce').toggleClass("sauce-white");
+  $(".btn-sauce").click(function() {
+    $(".sauce").toggleClass("sauce-white");
     $(".panel.price li:nth-child(4)").toggleClass("show");
+    calcPrice($(this), 3);
   });
 
-  $('.btn-crust').click(function(){
-    $('.crust').toggleClass("crust-gluten-free");
+  $(".btn-crust").click(function() {
+    $(".crust").toggleClass("crust-gluten-free");
     $(".panel.price li:nth-child(5)").toggleClass("show");
+    calcPrice($(this), 5);
   });
 
   /*ITERATION 3 */
 
-  $('.btn').removeClass('active');
-  
-  $(".btn").click(function(){
-    $(this).toggleClass('active');
+  $(".btn").removeClass("active");
+
+  $(".btn").click(function() {
+    $(this).toggleClass("active");
   });
 
   /*ITERATION 4 */
@@ -55,7 +58,14 @@ $(document).ready(function(){
 
   $(".panel.price strong").text("$10");
 
+  var currentPrice = 10;
 
-
-
+  function calcPrice(btn, qty) {
+    if (btn.hasClass("active")) {
+      currentPrice -= qty;
+    } else {
+      currentPrice += qty;
+    }
+    $(".panel.price strong").text("$" + currentPrice);
+  }
 });
