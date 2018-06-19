4482136e3b2c4c210b845f5a7fbc2a74cb57f957
JuanGPC <juangpc@gmail.com>
2018-06-19 15:11:01 +0200

    funcionando con toggle

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,35 +1,54 @@
-console.log("Hola");
-
 $(document).ready(function () {
 
   gpSections = $("section.green-pepper");
   mSections = $("section.mushroom");
   pSections = $("section.pep");
+  crustSection = $("#pizza>section:last");
+  sauceSection = $("#pizza>section>section:last");
 
-  gpSections.css("visibility", "hidden");
-  mSections.css("visibility", "hidden");
-  pSections.css("visibility", "hidden");
+  gpSections.hide();
+  $("button.btn-green-peppers").removeClass("active");
+
+  mSections.hide();
+  $("button.btn-mushrooms").removeClass("active");
+
+  pSections.hide();
+  $("button.btn-pepperonni").removeClass("active");
+
+  crustSection.removeClass("crust-gluten-free");
+  $("button.btn-crust").removeClass("active");
+
+  sauceSection.removeClass("sauce-white");
+  $("button.btn-sauce").removeClass("active");
+  
+  ////////////////////////////
+  ////button configuration
+  $("button.btn-green-peppers").click(function () {
+    console.log("green-peper hola");
+    $(".green-pepper").fadeToggle(1000);
+    $(this).toggleClass("active");
+  });
+
+  $("button.btn-mushrooms").click(function () {
+    $(".mushroom").fadeToggle(500);
+    $(this).toggleClass("active");
+
+  });
+
+  $("button.btn-pepperonni").click(function () {
+    $(".pep").fadeToggle(500);
+    $(this).toggleClass("active");
+  });
+
+  $("button.btn-sauce").click(function () {
+    sauceSection.toggleClass("sauce-white");
+    $(this).toggleClass("active");
+  });
+
+  $("button.btn-crust").click(function () {
+    crustSection.toggleClass("crust-gluten-free");
+    $(this).toggleClass("active");
+  });
 
 });
 
-$("button.btn-green-peppers").click(function () {
-  if (gpSections.css("visibility") == "visible")
-    gpSections.css("visibility", "hidden");
-  else
-    gpSections.css("visibility", "visible");
-});
-
-$("button.btn-mushrooms").click(function () {
-  if(mSections.css("visibility") == "visible")
-    mSections.css("visibility", "hidden");
-  else
-    mSections.css("visibility","visible");
-});
-
-
-$("button.btn-pepperonni").click(function () {
-  if(pSections.css("visibility") == "visible")
-    pSections.css("visibility", "hidden");
-  else
-    pSections.css("visibility","visible");
-});
