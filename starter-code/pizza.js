6f5ebc33c47c8f04e44f2c521616bee06b032508
Rodrigo Ortiz <roloortizgarcia@gmail.com>
2018-06-19 15:14:57 +0200

    first commit con falta del ultimo iteration

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,68 @@
 // Write your Pizza Builder JavaScript in this file.
+
+
+//IERATION 1
+
+
+$(".btn-pepperonni").click(function(){
+  
+  $(".pep").toggle();
+  $(".btn-pepperonni").toggleClass("active");
+  $(".price ul :first-child").toggle();
+ 
+  
+});
+
+$(".btn-mushrooms").click(function(){
+  $(".mushroom").toggle();
+  $(".btn-mushrooms").toggleClass("active");
+  $(".price ul :nth-child(2)").toggle();
+  
+});
+
+$(".btn-green-peppers").click(function(){
+  $(".green-pepper").toggle();
+  $(".btn-green-peppers").toggleClass("active");
+  $(".price ul :nth-child(3)").toggle();
+  
+});
+
+
+
+//ITERATION 2
+
+$(".sauce").removeClass("sauce-white");
+//$('.sauce').hide("price ul :nth-child(4)");
+
+
+$(".btn-sauce").click(function(){
+  $(".sauce").toggleClass("sauce-white","sauce");
+  $(".btn-sauce").toggleClass("active");
+  //$(".price ul :nth-child(4)").show("price ul :nth-child(4)");
+  
+});
+
+$(".crust").removeClass("crust-gluten-free");
+$(".btn-crust").click(function(){
+  
+  $(".crust").toggleClass("crust-gluten-free");
+  
+  $(".btn-crust").toggleClass("active");
+  
+});
+
+
+//ITERATION 3 DONE
+
+//ITERATION 4
+
+
+
+
+
+
+  
+
+
+
+
