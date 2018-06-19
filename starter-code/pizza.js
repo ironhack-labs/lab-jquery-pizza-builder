ce0c9e1748d68ee53f1ef384832424cabdd1982b
Sandra Najera <snajera@ucm.es>
2018-06-19 19:57:48 +0200

    Siguiente iteración hecha

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,11 +1,13 @@
 // Write your Pizza Builder JavaScript in this file.
 $(document).ready(function(){
   $(".btn").removeClass("active");
-  //$(".green-pepper").hide();
-  //$(".mushroom").hide();
-  //$(".pep").hide();
+  $(".green-pepper").hide();
+  $(".mushroom").hide();
+  $(".pep").hide();
   $(".crust").removeClass("crust-gluten-free");
-  $(".sauce").removeClass("sauce-white")
+  $(".sauce").removeClass("sauce-white");
+  $("aside ul li:nth-child(4)").css("display", "none");
+  $("aside ul li:nth-child(5)").css("display", "none");
 })
 var btnPepper = $(".btn-green-peppers");
 btnPepper.click(function(){
@@ -30,10 +32,17 @@ var btnSauce= $(".btn-sauce");
 btnSauce.click(function(){
   $(".sauce").toggleClass("sauce-white");
   btnSauce.toggleClass("active");
+  if(btnSauce.hasClass("active")){
+    $("aside ul li:nth-child(4)").css("display", "");
+  }else  $("aside ul li:nth-child(4)").css("display", "none");
 })
 
 var btnCrust = $(".btn-crust");
 btnCrust.click(function(){
   $(".crust").toggleClass("crust-gluten-free");
   btnCrust.toggleClass("active");
+  if( btnCrust.hasClass("active")){
+    $("aside ul li:nth-child(5)").css("display", "");
+  }else 
+  $("aside ul li:nth-child(5)").css("display", "none");
 })
