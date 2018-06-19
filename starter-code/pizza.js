0bebca9d89e683999450a128a5aa9ac0ba4d405f
Paula <paulagarizcun@gmail.com>
2018-06-19 12:55:45 +0200

    Por la mitad

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,22 @@
 // Write your Pizza Builder JavaScript in this file.
+$(function(){
+  $("#pizza > section:not(.crust), .sauce-white").css({"display" : "none"});
+  $("#pizza section:last-child").removeClass("crust-gluten-free");
+  
+  $("button").removeClass("active");
+  
+  $("button").click(function(){
+    $(this).toggleClass("active");
+  });
+
+  $(".btn-pepperonni").click(function(){
+   $(".pep").css({"display" : ""});
+  });
+$(".btn-mushrooms").click(function(){
+  $(".mushroom").css({"display" : ""});
+});
+ 
+
+
+
+});
