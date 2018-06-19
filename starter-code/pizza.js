1732ab36bc65181c7d82cd8c50ac5fb0ddcdd626
Bruno <brunodomecq@gmail.com>
2018-06-19 12:51:39 +0200

    primer commit

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,28 @@
-// Write your Pizza Builder JavaScript in this file.
+$(document).ready(function () {
+
+        $('.green-pepper').hide();
+        $('.pep').css({display: 'none'});
+        $('.mushroom').css({display: 'none'});
+        $('.active').removeClass('active');
+        $('.price li').css({display : 'none'})
+        $('.panel strong').text($('.panel b').text().split(" ")[0]);
+
+ 
+ 
+    $('.btn-pepperonni').click(function(){
+
+        var element = $('pepperonni');
+        var visible = element.attr('display');
+
+        console.log(visible)
+
+     })
+
+
+
+
+
+
+
+
+})
