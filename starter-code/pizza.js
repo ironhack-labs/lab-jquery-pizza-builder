716e862151d521e8055e4af98d755350483bd4d5
Jesus Martinez <jesmart1969@gmail.com>
2018-06-19 12:52:44 +0200

    1era Iteracion

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,28 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+  $(".green-pepper").css("display", "none");
+  $(".mushroom").css("display", "none");
+  $(".pep").css("display", "none");
+  $(".price li").css("display", "none");
+  $("b").css("display", "none");
+  $("strong").css("display", "none");
+ // $("section").removeClass("crust-gluten-free");
+ // $("section").removeClass("sauce-white");
+ $(".btn-pepperonni").click(function(e){
+   if ($(".pep").css("display") == "none"){
+    $(".pep").css("display", "");
+   }  else {
+   $(".pep").css("display", "none");
+  }
+ })
+ $(".btn-mushrooms").click(function(e){
+  $(".mushroom").css("display", "");
+})
+$(".btn-green-peppers").click(function(e){
+  $(".green-pepper").css("display", "");
+})
+
+
+
+
+})
