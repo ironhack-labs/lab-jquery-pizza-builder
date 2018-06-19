8e7386938ae975a61af4eee1724dcb039995b814
Antonio Esteban <aecdigitaltransformer@gmail.com>
2018-06-19 13:05:01 +0200

    Intento1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,25 @@
-// Write your Pizza Builder JavaScript in this file.
+console.log("Hola");
+
+$(document).ready(function(){
+  var gpSections;
+  var mSections;
+  var pSections;
+
+  gpSections=$("section.green-pepper");
+  mSections= $("section.mushroom")
+  pSections=$("section.pep")
+  
+  mSections.css("visibility", "hidden")
+  gpSections.css("visibility", "hidden")
+  pSections.css("visibility", "hidden")
+  
+  $(":button.bnt-pepperoni").click(function(){  
+    $("section.green-pepper").css("visibility", "visible");
+  });
+  $(":button.bnt-mushrooms").click(function(){  
+    mSections.css("visibility", "visible");
+  });
+  $(":button.bnt-green-peppers").click(function(){  
+    gpSections.css("visibilty", "visible");
+  });
+});  
