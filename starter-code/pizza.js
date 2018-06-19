dd83a4dcc4f2a10cf6120ccc1fd43c72232d12c2
JuanGPC <juangpc@gmail.com>
2018-06-19 13:27:07 +0200

    botones OK

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,25 +1,35 @@
 console.log("Hola");
 
-$(document).ready(function(){
-  var gpSections;
-  var mSections;
-  var pSections;
+$(document).ready(function () {
 
-  gpSections=$("section.green-pepper");
-  mSections= $("section.mushroom")
-  pSections=$("section.pep")
-  
-  mSections.css("visibility", "hidden")
-  gpSections.css("visibility", "hidden")
-  pSections.css("visibility", "hidden")
-  
-  $(":button.bnt-pepperoni").click(function(){  
-    $("section.green-pepper").css("visibility", "visible");
-  });
-  $(":button.bnt-mushrooms").click(function(){  
-    mSections.css("visibility", "visible");
-  });
-  $(":button.bnt-green-peppers").click(function(){  
-    gpSections.css("visibilty", "visible");
-  });
-});  
+  gpSections = $("section.green-pepper");
+  mSections = $("section.mushroom");
+  pSections = $("section.pep");
+
+  gpSections.css("visibility", "hidden");
+  mSections.css("visibility", "hidden");
+  pSections.css("visibility", "hidden");
+
+});
+
+$("button.btn-green-peppers").click(function () {
+  if (gpSections.css("visibility") == "visible")
+    gpSections.css("visibility", "hidden");
+  else
+    gpSections.css("visibility", "visible");
+});
+
+$("button.btn-mushrooms").click(function () {
+  if(mSections.css("visibility") == "visible")
+    mSections.css("visibility", "hidden");
+  else
+    mSections.css("visibility","visible");
+});
+
+
+$("button.btn-pepperonni").click(function () {
+  if(pSections.css("visibility") == "visible")
+    pSections.css("visibility", "hidden");
+  else
+    pSections.css("visibility","visible");
+});
