2d1db9eeb4adf8ba544d3e6da91929b4b08f9846
Adolfo Vela <vela@tekplanner.com>
2018-08-14 22:27:22 -0500

    Primera iteracion ok

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,13 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function () {
+
+    $(".btn-pepperonni").click(function(){
+      $(".pep").toggleClass("hide");
+    })
+    $(".btn-mushrooms").click(function(){
+      $(".mushroom").toggleClass("hide");
+    })
+    $(".btn-green-peppers").click(function(){
+      $(".green-pepper").toggleClass("hide");
+    })
+})
