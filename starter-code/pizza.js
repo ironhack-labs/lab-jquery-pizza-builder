c47d7e217410ba3a401d1286741cefb3804a1f10
Ricardo Mayo <rmayo.1995@gmail.com>
2018-08-14 22:14:14 -0500

    primera iteracion

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,37 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+    $(".btn-pepperonni").click(function() {
+
+        if($(".pep").is(":visible")) {
+            $(".pep").hide();
+        } else {
+            $(".pep").show();
+        }
+    });
+
+    $(".btn-mushrooms").click(function() {
+
+        if($(".mushroom").is(":visible")) {
+            $(".mushroom").hide();
+        } else {
+            $(".mushroom").show();
+        }
+    });
+
+    $(".btn-green-peppers").click(function() {
+        if($(".green-pepper").is(":visible")) {
+            $(".green-pepper").hide();
+        } else {
+            $(".green-pepper").show();
+        }
+    });
+
+    $(".btn-sauce").click(function() {
+        console.log("Sauce");
+    });
+
+    $(".btn-crust").click(function() {
+        console.log("Crust");
+    });
+
+});
