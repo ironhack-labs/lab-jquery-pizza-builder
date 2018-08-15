b21a6e019b9ad88aa05c941ceac81312eb2cce78
Mauricio Gonzalez Robles <mauricio.gzrs@gmail.com>
2018-08-14 23:45:47 -0500

    Pizza builder update

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,42 +1,38 @@
 // Write your Pizza Builder JavaScript in this file.
 $(document).ready(function() {
+  
   getTotal();
 
   $('.btn-pepperonni').click(function() {
     $(this).toggleClass('active');
     $('.pep').toggleClass('hidden');
     $('.item-price:eq(0)').toggleClass('display');
-    getTotal();
   });
 
   $('.btn-mushrooms').click(function() {
     $(this).toggleClass('active');
     $('.mushroom').toggleClass('hidden');
     $('.item-price:eq(1)').toggleClass('display');
-    getTotal();
   });
 
   $('.btn-green-peppers').click(function() {
     $(this).toggleClass('active');
     $('.green-pepper').toggleClass('hidden');
     $('.item-price:eq(2)').toggleClass('display');
-    getTotal();
   });
 
   $('.btn-sauce').click(function() {
     $(this).toggleClass('active');
     $('.sauce').toggleClass('sauce-white');
     $('.item-price:eq(3)').toggleClass('display');
-    getTotal();
   });
 
   $('.btn-crust').click(function() {
     $(this).toggleClass('active');
     $('.crust').toggleClass('crust-gluten-free');
     $('.item-price:eq(4)').toggleClass('display');
-    getTotal();
   });
-  
+
   function getTotal() {
     let total = 10;
     $('.item-price').each(function() {
@@ -47,4 +43,9 @@ $(document).ready(function() {
     });
     return $('#total').html('$' + total);
   };
+
+  $('.btn').click(function() {
+    getTotal();
+  });
+
 });
