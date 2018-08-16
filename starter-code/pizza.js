8deb643e406d913dba43e588d20b0a17d94451ec
Adolfo Vela <vela@tekplanner.com>
2018-08-16 08:33:53 -0500

    Listo

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -2,6 +2,7 @@
 $(document).ready(function () {
 
   $(".btn-pepperonni").click(function () {
+    total();
     $(".pep").toggleClass("hide");
     // Coloco el precio o lo quito
     $(".panel-price li:nth-child(1)").toggleClass("hide");
@@ -9,6 +10,7 @@ $(document).ready(function () {
     $(".btn-pepperonni").toggleClass("active");
   })
   $(".btn-mushrooms").click(function () {
+    total();
     $(".mushroom").toggleClass("hide");
     // Coloco el precio o lo quito
     $(".panel-price li:nth-child(2)").toggleClass("hide");
@@ -16,6 +18,7 @@ $(document).ready(function () {
     $(".btn-mushrooms").toggleClass("active");
   })
   $(".btn-green-peppers").click(function () {
+    total();
     $(".green-pepper").toggleClass("hide");
     // Coloco el precio o lo quito
     $(".panel-price li:nth-child(3)").toggleClass("hide");
@@ -25,6 +28,7 @@ $(document).ready(function () {
 
   // Boton cambia para escoger la salsa
   $(".btn-sauce").click(function () {
+    total()
     $(".sauce").toggleClass("sauce-white");
     // Cambio el texto del boton 
     switch ($(".btn-sauce").text()) {
@@ -56,4 +60,33 @@ $(document).ready(function () {
     }
   })
 
+  var totalPrice;
+
+  function total() {
+    //Extracion del texto html
+    var pepPriceText = $(".panel-price li:nth-child(1)").text();
+    var musPriceText = $(".panel-price li:nth-child(2)").text();
+    var greenPriceText = $(".panel-price li:nth-child(3)").text();
+    var saucePriceText = $(".panel-price li:nth-child(4)").text();
+    var crustPriceText = $(".panel-price li:nth-child(5)").text();
+    // Se convierte texto en numero
+    var pepPrice = Number(pepPriceText.replace(/[^0-9.-]+/g, ""));
+    // Si no aparece el ingrediente, este toma valor de 0
+    if ($(".panel-price li:nth-child(1)").hasClass("hide")) pepPrice = 0;
+
+    var musPrice = Number(musPriceText.replace(/[^0-9.-]+/g, ""));
+    if ($(".panel-price li:nth-child(2)").hasClass("hide")) musPrice = 0;
+
+    var greenPrice = Number(greenPriceText.replace(/[^0-9.-]+/g, ""));
+    if ($(".panel-price li:nth-child(3)").hasClass("hide")) greenPrice = 0;
+
+    var saucePrice = Number(saucePriceText.replace(/[^0-9.-]+/g, ""));
+    var crustPrice = Number(crustPriceText.replace(/[^0-9.-]+/g, ""));
+
+    // Calulo del precio total
+    totalPrice = pepPrice + musPrice + greenPrice + saucePrice + crustPrice + 10;
+    console.log(totalPrice)
+     $("#totalPrice").text("$"+totalPrice);
+
+  }
 })
