6b6b50583979609b734492a0d75283fa4ff51df5
Alberto Martinez <jose_alberto12216@hotmail.com>
2018-08-15 23:07:09 -0500

    Se finaliza lab Pair Programming

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -2,41 +2,81 @@
 $(document).ready(function(){
 
   $('#pepperonni').click(function(){
-    if($('#peperoniiiiii').css("visibility")=='hidden')
+    var total = $('#totalPrice').text().replace('$','');
+    if($('#peperoniiiiii').css("visibility")=='hidden'){
       $('#peperoniiiiii').css('visibility','visible');
-    else
+      $('#pepePrice').css('visibility','visible');
+      total = parseInt(total) + 1;
+    }
+    else{
       $('#peperoniiiiii').css('visibility','hidden');
-
+      $('#pepePrice').css('visibility','hidden');
+      total = parseInt(total) - 1;
+    }
     $("#pepperonni").toggleClass("active");
+    $('#totalPrice').text('$'+total);
   });
 
   $('#Mushro').click(function(){
-    if($('#mushhh').css("visibility")=='hidden')
+    var total = $('#totalPrice').text().replace('$','');
+    if($('#mushhh').css("visibility")=='hidden'){
       $('#mushhh').css('visibility','visible');
-    else
+      $('#mushPrice').css('visibility','visible');
+      total = parseInt(total) + 1;
+    }
+    else{
       $('#mushhh').css('visibility','hidden');
-
+      $('#mushPrice').css('visibility','hidden');
+      total = parseInt(total) - 1;
+    }
     $("#Mushro").toggleClass("active");
+    $('#totalPrice').text('$'+total);
   });
 
   $('#greenPepp').click(function(){
-    if($('#pepperrr').css("visibility")=='hidden')
+    var total = $('#totalPrice').text().replace('$','');
+    if($('#pepperrr').css("visibility")=='hidden'){
       $('#pepperrr').css('visibility','visible');
-    else
+      $('#greePrice').css('visibility','visible');
+      total = parseInt(total) + 1;
+    }
+    else{
       $('#pepperrr').css('visibility','hidden');
-
+      $('#greePrice').css('visibility','hidden');
+      total = parseInt(total) - 1;
+    }
     $("#greenPepp").toggleClass("active");
+    $('#totalPrice').text('$'+total);
   });
 
   $('#white').click(function(){
+    var total = $('#totalPrice').text().replace('$','');
     $("#whitesouce").toggleClass("sauce-white");
     $("#white").toggleClass("active");
+    if($('#saucePrice').css("visibility")=='hidden'){
+      $('#saucePrice').css('visibility','visible');
+      total = parseInt(total) + 3;
+    }
+    else{
+      $('#saucePrice').css('visibility','hidden');
+      total = parseInt(total) - 3;
+    }
+    $('#totalPrice').text('$'+total);
   });
 
   $('#glutenFree').click(function(){
-    console.log("pico");
+    var total = $('#totalPrice').text().replace('$','');
     $("#crusss").toggleClass("crust-gluten-free");
     $("#glutenFree").toggleClass("active");
+    if($('#gluPrice').css("visibility")=='hidden'){
+      $('#gluPrice').css('visibility','visible');
+      total = parseInt(total) + 5;
+    }
+    else{
+      $('#gluPrice').css('visibility','hidden');
+      total = parseInt(total) - 5;
+    }
+    $('#totalPrice').text('$'+total);
   });
 
 });
