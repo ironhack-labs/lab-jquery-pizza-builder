0ae0a7fa262f6e645ebd4be50f88e6e43daaf1cd
Adolfo Vela <vela@tekplanner.com>
2018-08-15 21:13:24 -0500

    Listo iteracion 2

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,13 +1,43 @@
 // Write your Pizza Builder JavaScript in this file.
 $(document).ready(function () {
 
-    $(".btn-pepperonni").click(function(){
-      $(".pep").toggleClass("hide");
-    })
-    $(".btn-mushrooms").click(function(){
-      $(".mushroom").toggleClass("hide");
-    })
-    $(".btn-green-peppers").click(function(){
-      $(".green-pepper").toggleClass("hide");
-    })
+  $(".btn-pepperonni").click(function () {
+    $(".pep").toggleClass("hide");
+  })
+  $(".btn-mushrooms").click(function () {
+    $(".mushroom").toggleClass("hide");
+  })
+  $(".btn-green-peppers").click(function () {
+    $(".green-pepper").toggleClass("hide");
+  })
+
+  // Boton cambia para escoger la salsa
+$(".btn-sauce").click(function () {
+  $(".sauce").toggleClass("sauce-white");
+  // Cambio el texto del boton 
+  switch ($(".btn-sauce").text()) {
+    case "Regular sauce":
+      $(".btn-sauce").text("White sauce");
+      break;
+    case "White sauce":
+      $(".btn-sauce").text("Regular sauce");
+      break;
+  }
 })
+
+// Boton cambia para escoger la orilla
+  $(".btn-crust").click(function () {
+    $(".crust").toggleClass("crust-gluten-free");
+    // Cambio el texto del boton 
+    switch ($(".btn-crust").text()) {
+      case "Crust":
+        $(".btn-crust").text("Gluten-free crust");
+        break;
+      case "Gluten-free crust":
+        $(".btn-crust").text("Crust");
+        break;
+    }
+  })
+
+})
+
