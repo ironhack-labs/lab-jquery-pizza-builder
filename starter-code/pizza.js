bcf0f8eda94eb1917cef2d8a90b46d9a46acff90
Dennis <ratenniel@gmail.com>
2018-08-28 13:31:22 +0200

    borrado de ingredientes

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,25 @@
 // Write your Pizza Builder JavaScript in this file.
+
+function removePep() {
+  $('.pep').css('display', 'none');
+};
+
+function removeMush() {
+  $('.mushroom').css('display', 'none');
+};
+
+function removeGreen() {
+  $('.green-pepper').css('display', 'none');
+};
+
+
+
+
+
+function init() {
+  removePep();
+  removeMush();
+  removeGreen();
+};
+
+$(init());
