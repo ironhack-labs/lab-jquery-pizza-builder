7adf977ecbde317675e100cba1bf218387feef84
aemartos <aemartos@gmail.com>
2018-09-26 15:46:26 +0200

    Another way to do it :alien:

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,69 +1,133 @@
-// GLOBAL VARIABLES -----------------
+// // GLOBAL VARIABLES -----------------
 
-var $pepperoni = $('.pep');
-var $mushroom = $('.mushroom');
-var $greenPepper = $('.green-pepper');
-var $crust = $('.crust');
-var $sauce = $('.sauce');
+// var $pepperoni = $('.pep');
+// var $mushroom = $('.mushroom');
+// var $greenPepper = $('.green-pepper');
+// var $crust = $('.crust');
+// var $sauce = $('.sauce');
 
-var $btnPep = $('.btn-pepperonni');
-var $btnMush = $('.btn-mushrooms');
-var $btnGPep = $('.btn-green-peppers');
-var $btnSau = $('.btn-sauce');
-var $btnCrust = $('.btn-crust');
+// var $btnPep = $('.btn-pepperonni');
+// var $btnMush = $('.btn-mushrooms');
+// var $btnGPep = $('.btn-green-peppers');
+// var $btnSau = $('.btn-sauce');
+// var $btnCrust = $('.btn-crust');
+// var $allButtons = $('.btn');
+
+// var $liPep = $('.panel.price ul li:contains(pepperonni)');
+// var $liMush = $('.panel.price ul li:contains(mushrooms)');
+// var $liGPep = $('.panel.price ul li:contains(peppers)');
+// var $liSau = $('.panel.price ul li:contains(sauce)');
+// var $liCrust = $('.panel.price ul li:contains(crust)');
+
+// var $priceBox = $('.panel.price strong');
+// var price = 13;
+
+// $liSau.hide();
+// $liCrust.hide();
+
+
+// //FUNCTIONS -----------------
+
+// var toggleTopping = function (topping, elem) {
+//   topping.toggle();
+//   elem.toggle();
+//   updatePrice(elem);
+// }
+
+// var changeBase = function (base, type, elem) {
+//   base.toggleClass(type);
+//   elem.toggle();
+//   updatePrice(elem);
+// }
+
+// var updatePrice = function (elem) {
+//   var priceIngred = parseInt(elem.text().match(/\d+/)[0]);
+//   elem.is(":hidden") ? price -= priceIngred : price += priceIngred;
+//   $priceBox.text('$' + price);
+// }
+
+
+// //EVENTS -----------------
+
+// $btnPep.on("click", function(){
+//   toggleTopping($pepperoni, $liPep);
+// });
+// $btnMush.on("click", function(){
+//   toggleTopping($mushroom, $liMush);
+// });
+// $btnGPep.on("click", function(){
+//   toggleTopping($greenPepper, $liGPep);
+// });
+// $btnSau.on("click", function(){
+//   changeBase($sauce, 'sauce-white', $liSau);
+// });
+// $btnCrust.on("click", function(){
+//   changeBase($crust, 'crust-gluten-free', $liCrust);
+// });
+// $allButtons.on("click", function(){
+//   $(this).toggleClass('active');
+// });
+
+
+
+// ANOTHER WAY TO DO IT ------------------------------------------------
+
+var ingredient = {
+  'btn-pepperonni': {
+    pic: $('.pep'),
+    li: $('.panel.price ul li:contains(pepperonni)')
+  },
+  'btn-mushrooms': {
+    pic: $('.mushroom'),
+    li: $('.panel.price ul li:contains(mushrooms)')
+  },
+  'btn-green-peppers': {
+    pic: $('.green-pepper'),
+    li: $('.panel.price ul li:contains(peppers)')
+  },
+  'btn-sauce': {
+    pic: $('.sauce'),
+    li:  $('.panel.price ul li:contains(sauce)'),
+    clas: 'sauce-white'
+  },
+  'btn-crust': {
+    pic: $('.crust'),
+    li: $('.panel.price ul li:contains(crust)'),
+    clas: 'crust-gluten-free'
+  },
+}
+
+var ingredArr = ['btn-pepperonni', 'btn-mushrooms', 'btn-green-peppers'];
 var $allButtons = $('.btn');
-
-var $liPep = $('.panel.price ul li:contains(pepperonni)');
-var $liMush = $('.panel.price ul li:contains(mushrooms)');
-var $liGPep = $('.panel.price ul li:contains(peppers)');
-var $liSau = $('.panel.price ul li:contains(sauce)');
-var $liCrust = $('.panel.price ul li:contains(crust)');
-
 var $priceBox = $('.panel.price strong');
 var price = 13;
-
-$liSau.hide();
-$liCrust.hide();
+$('.panel.price ul li:contains(sauce)').hide();
+$('.panel.price ul li:contains(crust)').hide();
 
 
 //FUNCTIONS -----------------
-
 var toggleTopping = function (topping, elem) {
   topping.toggle();
   elem.toggle();
-  updatePrice(elem);
 }
-
 var changeBase = function (base, type, elem) {
   base.toggleClass(type);
   elem.toggle();
-  updatePrice(elem);
 }
-
 var updatePrice = function (elem) {
   var priceIngred = parseInt(elem.text().match(/\d+/)[0]);
   elem.is(":hidden") ? price -= priceIngred : price += priceIngred;
   $priceBox.text('$' + price);
 }
 
-
-//EVENTS -----------------
-
-$btnPep.on("click", function(){
-  toggleTopping($pepperoni, $liPep);
-});
-$btnMush.on("click", function(){
-  toggleTopping($mushroom, $liMush);
-});
-$btnGPep.on("click", function(){
-  toggleTopping($greenPepper, $liGPep);
-});
-$btnSau.on("click", function(){
-  changeBase($sauce, 'sauce-white', $liSau);
-});
-$btnCrust.on("click", function(){
-  changeBase($crust, 'crust-gluten-free', $liCrust);
-});
-$allButtons.on("click", function(){
+//EVENT -----------------
+$allButtons.on("click", function(e){
+  var btnClass = $(this).attr('class').match(/(\bbtn-\S+)/ig)[0];
+  if (ingredArr.includes(btnClass)) {
+    toggleTopping(ingredient[btnClass].pic, ingredient[btnClass].li);
+  } else {
+    changeBase(ingredient[btnClass].pic, ingredient[btnClass].clas, ingredient[btnClass].li);
+  }
+  updatePrice(ingredient[btnClass].li);
   $(this).toggleClass('active');
 });
