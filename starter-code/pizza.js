17aa9b6b7bd1c8c97d424eaa0e4504907640f4cd
Teresa Romero <tromerolozano@gmail.com>
2018-09-26 19:32:02 +0200

    Iteration 1!
    ..
    ...
    esc

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,23 @@
 // Write your Pizza Builder JavaScript in this file.
+
+
+
+// $( "button" ).click(function() {
+//   $( this ).toggleClass( "active" );
+//   });
+
+  $(document).ready(function(){
+    $(".btn-pepperonni").click(function(){
+        $(".pep").toggle();
+    });
+
+    $(".btn-mushrooms").click(function(){
+      $(".mushroom").toggle();
+  });
+
+    $(".btn-green-peppers").click(function(){
+      $(".green-pepper").toggle();
+  });
+
+
+});
