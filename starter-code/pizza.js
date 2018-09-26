744eac29c209ca15a4c3de65d8e3cd3d8b1ac531
Teresa Romero <tromerolozano@gmail.com>
2018-09-26 19:44:42 +0200

    Iteration 2

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -8,16 +8,32 @@
 
   $(document).ready(function(){
     $(".btn-pepperonni").click(function(){
-        $(".pep").toggle();
+      $(".pep").toggle();
+      $(this).toggleClass("active");
     });
 
     $(".btn-mushrooms").click(function(){
       $(".mushroom").toggle();
-  });
+      $(this).toggleClass("active");
+    });
 
     $(".btn-green-peppers").click(function(){
       $(".green-pepper").toggle();
-  });
+      $(this).toggleClass("active");
+    });
 
+  
+
+    $(".btn-sauce").toggleClass("active");
+    $(".sauce").toggle();
+    $(".btn-sauce").click(function(){
+      $(".sauce").toggle();
+      $(this).toggleClass("active");
+    });
+     
+    $(".btn-crust").toggleClass("active");
+    $(".btn-crust").click(function(){
+      $(this).toggleClass("active");
+    });
 
 });
