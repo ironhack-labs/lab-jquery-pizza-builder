0c33d1177d94a5963607cc8fda92fe0c44e3cefc
nicolas prieur <nicolas.prieurdelacomble@gmaile.com>
2018-10-09 21:36:56 +0200

    almost removed sauce, missing update prices

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,46 @@
 // Write your Pizza Builder JavaScript in this file.
+
+// Pepperonni
+    var btnPep = $(".btn-pepperonni") ;
+    var pep = $(".pep") ;
+    btnPep.click(function(){
+        btnPep.toggleClass("active");
+        pep.toggle()
+    });
+
+// Mushrooms
+var btnMush = $(".btn-mushrooms") ;
+var mush = $(".mushroom") ;
+btnMush.click(function(){
+    btnMush.toggleClass("active");
+    mush.toggle()
+});
+
+// Green peppers
+
+var btnPeppers = $(".btn-green-peppers") ;
+var greenPepper = $(".green-pepper") ;
+btnPeppers.click(function(){
+    btnPeppers.toggleClass("active");
+    greenPepper.toggle()
+});
+
+
+// remove sauce
+
+$(document).ready(function(){
+var crust = $(".crust") ;
+var gluten = $(".crust-gluten-free");
+sauce.removeClass(gluten);
+}
+
+
+
+
+
+
+
+
+
+
+
