afab8bd56e38f5e183b3469a1369c4d737574f15
justinezahiri <justinezahiri@gmail.com>
2018-10-15 22:50:36 +0200

    Iteration 4 completed

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -10,27 +10,34 @@ $(document).ready(function(){
     btnPep.click(function(){
         $(this).toggleClass("active");
         $(".pep").toggle();
+        $("li:contains('pepperonni')").toggle();
     });
 
     btnMush.click(function(){
         $(this).toggleClass("active");
         $(".mushroom").toggle();
+        $("li:contains('mushrooms')").toggle();
     });
 
     btnGreen.click(function(){
         $(this).toggleClass("active");
         $(".green-pepper").toggle();
+        $("li:contains('green')").toggle();
     });
 
     btnSauce.click(function(){
         $(this).toggleClass("active");
         $(".sauce").toggleClass("sauce-white");
+        $("li:contains('white')").toggle();
     });
 
     btnCrust.click(function(){
         $(this).toggleClass("active");
         $(".crust").toggleClass("crust-gluten-free");
+        $("li:contains('gluten')").toggle();
     });
+
+
 
 
 });
