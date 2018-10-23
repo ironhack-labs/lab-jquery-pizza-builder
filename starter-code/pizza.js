b9a824ef7e20060ba8f267dd5b156094282d5541
Ismael P. Paiva <ismael.paiva@fatec.sp.gov.br>
2018-10-23 17:41:29 -0300

    DONE

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,19 +1,37 @@
 // Write your Pizza Builder JavaScript in this file.
+$('.sauce-price').toggle();
+$('.crust-price').toggle();
+
+const totalPrice = function () {
+  const ingredientsPrice = $('.price>ul>li:visible');
+  let total = 10;
+  for (let i = 0; i < ingredientsPrice.length; i += 1) {
+    const cafe = ingredientsPrice[i].textContent;
+    total += parseInt(cafe[1]);
+    console.log(cafe);
+  }
+  $('.price>strong').empty().append(`$ ${total}`);
+};
+
+totalPrice();
 
 // append and remove
 $('.btn-pepperonni').on('click', function () {
   $('.pep').fadeToggle(2000);
   $('aside > ul :nth-child(1)').toggle('li');
+  totalPrice();
 });
 
 $('.btn-mushrooms').on('click', function () {
   $('.mushroom').fadeToggle(2000);
   $('aside > ul :nth-child(2)').toggle('li');
+  totalPrice();
 });
 
 $('.btn-green-peppers').on('click', function () {
   $('.green-pepper').fadeToggle(2000);
   $('aside > ul :nth-child(3)').toggle('li');
+  totalPrice();
 });
 
 // 2
@@ -30,26 +48,12 @@ $('.btn-crust').on('click', function () {
   $('.btn-crust').toggleClass('active');
   $('.crust').toggleClass('crust-gluten-free');
   $('aside > ul :nth-child(5)').toggle('li');
+  totalPrice();
 });
 
 $('.btn-sauce').on('click', function () {
   $('.btn-sauce').toggleClass('active');
   $('.sauce').toggle('sauce-white');
   $('aside > ul :nth-child(4)').toggle('li');
+  totalPrice();
 });
-
-$('.panel.price > ul :nth-Child(4)').hide().toggleClass('off');
-$('.panel.price > ul :nth-Child(5)').hide().toggleClass('off');
-$('.panel.price > strong').text('$13');
-function updatePrice() {
-  let totalPrice = 10;
-  const l = $('.panel.price > ul > li');
-  const n = $('.panel.price > ul > li').size();
-  for (let i = 0; i < n; i++) {
-    if (!$(l).eq(i).hasClass('off')) {
-      console.log(i, n, ($(l).eq(i).text().split('')[1]));
-      totalPrice += parseInt($(l).eq(i).text().split('')[1]);
-    }
-  }
-  $('.panel.price > strong').text('${$totalPrice}');
-}
