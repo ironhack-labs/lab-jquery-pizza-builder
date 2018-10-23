d660dba94aa0aad95b57fdd6d25fd6e3631b6678
Sarah Connor <sarahchamorro@gmail.com>
2018-10-23 17:43:23 -0300

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -2,18 +2,11 @@ $(window).ready(()=>{
     $(".pizza-ingredient:nth-child(5)").fadeToggle()
     $(".pizza-ingredient:nth-child(4)").fadeToggle()
 
-    let pepPrice = 1;
-    let mushPrice = 1;
-    let greenPrice = 1;
-    let whitePrice = 0;
-    let glutenPrice = 0;
     let sum;
-    let totalPrice;
 
 
-    sum = 10 + pepPrice + mushPrice + greenPrice + whitePrice + glutenPrice
-    totalPrice = "$" + sum
-    $(".price strong").text(totalPrice)
+    sum = 13
+    $(".price strong").text(sum)
 
 
 // Write your Pizza Builder JavaScript in this file.
@@ -23,92 +16,81 @@ $(".sauce").removeClass("sauce-white")
 $(".btn-sauce").removeClass("active")
 
 $(".btn-pepperonni").click(function(){
-    $(".price strong").text(totalPrice)
     $(".pep").fadeToggle()
     $(".pizza-ingredient:nth-child(1)").fadeToggle()
     if($(this).hasClass("active")){
         $(this).removeClass("active")
-        pepPrice = 0
-        sum = 10 + pepPrice + mushPrice + greenPrice + whitePrice + glutenPrice
-        totalPrice = "$" + sum
+        sum -= 1
+        $(".price strong").text("$"+sum)
+
     } else {
         $(this).addClass("active")
-        pepPrice = 1
-        sum = 10 + pepPrice + mushPrice + greenPrice + whitePrice + glutenPrice
-        totalPrice = "$" + sum
+        sum +=1
+        $(".price strong").text("$"+sum)
+
     }
     
 })
 
 $(".btn-mushrooms").click(function(){
-    sum = 10 + pepPrice + mushPrice + greenPrice + whitePrice + glutenPrice
-    totalPrice = "$" + sum
-    $(".price strong").text(totalPrice)
     $(".mushroom").fadeToggle()
     $(".pizza-ingredient:nth-child(2)").fadeToggle()
     if($(this).hasClass("active")){
         $(this).removeClass("active")
-        mushPrice = 1
-        totalPrice = "$" + sum
+        sum -= 1
+        $(".price strong").text("$"+sum)
     } else {
         $(this).addClass("active")
-        mushPrice = 0
-        totalPrice = "$" + sum
+        sum += 1
+        $(".price strong").text("$"+sum)
     }
     })
 
 $(".btn-green-peppers").click(function(){
-    sum = 10 + pepPrice + mushPrice + greenPrice + whitePrice + glutenPrice
-    $(".price strong").text(totalPrice)
     $(".green-pepper").fadeToggle()
     $(".pizza-ingredient:nth-child(3)").fadeToggle()
     if($(this).hasClass("active")){
         $(this).removeClass("active")
-        greenPrice = 1
-        totalPrice = "$" + sum
+        sum -= 1
+        $(".price strong").text("$"+sum)
     } else {
         $(this).addClass("active")
-        greenPrice = 0
-        totalPrice = "$" + sum
+        sum += 1
+        $(".price strong").text("$"+sum)        
     }
     
 
     })
 
 $(".btn-sauce").click(function(){
-    sum = 10 + pepPrice + mushPrice + greenPrice + whitePrice + glutenPrice
-    $(".price strong").text(totalPrice)
     $(".pizza-ingredient:nth-child(4)").fadeToggle()
     if($(".sauce").hasClass("sauce-white")){
         $(".btn-sauce").removeClass("active")
         $(".sauce").removeClass("sauce-white")
-        whitePrice = 3
-        totalPrice = "$" + sum
+        sum -= 3
+        $(".price strong").text("$"+sum)
     } else {
         $(".btn-sauce").removeClass("active")
         $(".sauce").addClass("sauce-white")
-        whitePrice = 0
-        totalPrice = "$" + sum
+        sum += 3
+        $(".price strong").text("$"+sum)
     }
     
 })
 
 
 $(".btn-crust").click(function(){
-    sum = 10 + pepPrice + mushPrice + greenPrice + whitePrice + glutenPrice
-    totalPrice = "$" + sum
-    $(".price strong").text(totalPrice)
     $(".pizza-ingredient:nth-child(5)").fadeToggle()
     if($(".thirty-three ~ section").hasClass("crust-gluten-free")){
         $(".btn-crust").removeClass("active")
         $(".crust:parent").removeClass("crust-gluten-free")
-        glutenPrice = 5
-        totalPrice = "$" + sum
+        sum -= 5
+        $(".price strong").text("$"+sum)
     } else {
         $(".crust:parent").addClass("crust-gluten-free")
         $(".btn-crust").addClass("active")
-        glutenPrice = 0
-        totalPrice = "$" + sum
+        sum += 5
+        $(".price strong").text("$"+sum)
     }
     
 })
