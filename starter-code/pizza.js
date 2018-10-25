99bb4af68e3b6d6dce2182310abe6fa5a0dacb1e
Adèle Revert <revertadele@hotmail.fr>
2018-10-25 10:03:08 +0200

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,26 @@
 // Write your Pizza Builder JavaScript in this file.
+
+// Iteration 1
+var pizzaDrawing = $("#pizza > .pep, #pizza > .mushroom, #pizza > .green-pepper");
+pizzaDrawing.hide();
+
+$(".btn-green-peppers").click(function () {
+  $("#pizza > .green-pepper").toggle();
+});
+
+$(".btn-mushrooms").click(function () {
+  $("#pizza > .mushroom").toggle();
+});
+  
+$(".btn-pepperonni").click(function () {
+  $("#pizza > .pep").toggle();
+});
+
+// Iteration 2
+$(".btn-sauce").click(function () {
+  $(".sauce").toggleClass("sauce-white");
+});
+
+$(".btn-crust").click(function () {
+  $(".crust").toggleClass("crust-gluten-free");
+});
