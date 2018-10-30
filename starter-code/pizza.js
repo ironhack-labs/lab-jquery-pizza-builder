bcf8f74cfb7028c9bfb2cca380a710fbf284a9be
Camille Chapleau <camille.chapleau@hotmail.fr>
2018-10-30 22:00:13 +0100

    first push

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,35 @@
-// Write your Pizza Builder JavaScript in this file.
+// Write your Pizza Builder JavaScript in this file
+$(".btn-pepperonni").click(function() {
+    $(".pep").toggle("true");
+    $(".btn-pepperonni").toggleClass("active");
+    } );
+$(".panel price:contains('$1 pepperonni')").remove();
+
+$(".btn-mushrooms").click(function() {
+    $(".mushroom").toggle("true");
+    $(".btn-mushrooms").toggleClass("active");
+} );
+
+$(".btn-green-peppers").click(function() {
+    $(".green-pepper").toggle("true");
+    $(".btn-green-peppers").toggleClass("active");
+} );
+
+// By default, regular crust
+$('.crust-gluten-free').removeClass("crust-gluten-free");
+$('.btn-crust').click(function() {
+    $('.btn-crust').toggleClass("active");
+    $('.crust').toggleClass("crust-gluten-free");
+});
+
+$('.sauce-white').removeClass("sauce-white");
+$('.btn-sauce').click(function() {
+    $('.btn-sauce').toggleClass("active");
+    $('.sauce').toggleClass("sauce-white");
+});
+
+
+//Use JavaScript to hide and show the items by default
+
+
+
