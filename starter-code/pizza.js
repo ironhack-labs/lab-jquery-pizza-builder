f56a170ad5f3648cf70546ac42ab2b8eb9d86693
Fer Perez Ayala <ferp@protonmail.ch>
2018-10-30 14:27:37 +0100

    Iteration 2 complete.

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,5 +1,16 @@
 // Write your Pizza Builder JavaScript in this file.
 
+$(function() {
+    
+    $('.sauce').toggleClass('sauce-white')
+    $('.btn-sauce').toggleClass("active");
+
+    $('.crust').toggleClass('crust-gluten-free')
+    $('.btn-crust').toggleClass("active");
+
+  });
+  
+
 $('.btn-pepperonni').on("click", function(){
     $('.pep').toggle();
     $(this).toggleClass("active");
@@ -15,3 +26,12 @@ $('.btn-green-peppers').on("click", function(){
     $(this).toggleClass("active");
 })
 
+$('.btn-sauce').on("click", function(){
+    $('.sauce').toggleClass('sauce-white')
+    $(this).toggleClass("active");
+})
+
+$('.btn-crust').on("click", function(){
+    $('.crust').toggleClass('crust-gluten-free')
+    $(this).toggleClass("active");
+})
