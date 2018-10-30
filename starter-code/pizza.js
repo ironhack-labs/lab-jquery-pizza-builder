d70d17c8c6484fd5ead462e497bf81b886054ba8
sitobusnan <sito@citytonight.es>
2018-10-30 16:43:56 +0100

    iteracion 1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,49 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(function(){
+
+  function toggleIngredient (button, ingredient){
+    button.toggleClass("active");
+    ingredient.toggle();
+  }
+  
+  //function removeingredient (ingredient){
+  //
+  //}
+  //
+  //function mushrooms (){
+  //
+  //}
+  //function greenPeppers () {
+  //
+  //}
+  //function pepperoni () {
+  //
+  //}
+  
+  var mush = $(".mushroom");
+  var green = $(".green-pepper");
+  var peppe = $(".pep");
+  var mushClick = $(".btn-mushrooms")
+  var greenClick = $(".btn-green-peppers")
+  var peppeClick = $(".btn-pepperonni")
+  
+  mushClick.click(function() {
+    toggleIngredient(mushClick, mush);
+    
+  });
+  greenClick.click(function() {
+    toggleIngredient(greenClick, green);
+  });
+  peppeClick.click(function(){
+    toggleIngredient(peppeClick, peppe);
+  });
+  
+  
+  
+
+
+
+
+})
+
