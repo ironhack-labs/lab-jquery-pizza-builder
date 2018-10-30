37c747ffc8c47f1201cc99492a3f6344e88cdb74
Jorge Devesa <jrgdevesa@gmail.com>
2018-10-30 16:33:08 +0100

    Iteration 2 done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,16 +1,29 @@
 // Write your Pizza Builder JavaScript in this file.
 // $(document).ready(function(){
 
+$(".btn-green-peppers").click(function() {
+  $(".green-pepper").toggle();
+});
+$(".btn-mushrooms").click(function() {
+  $(".mushroom").toggle();
+});
+$(".btn-pepperonni").click(function() {
+  $(".pep").toggle();
+});
 
-  $(".btn-green-peppers").click(function() {
-    $(".green-pepper").toggle();
-  });
-  $(".btn-mushrooms").click(function() {
-    $(".mushroom").toggle();
-  });
-  $(".btn-pepperonni").click(function() {
-    $(".pep").toggle();
-  });
+$(document).ready(function() {
+  $(".crust").removeClass("crust-gluten-free");
+  $(".sauce").removeClass("sauce-white");
+
+});
+
+$(".btn-sauce").click(function() {
+    $(".sauce").toggleClass("sauce-white");
+});
+
+$(".btn-crust").click(function() {
+    $(".crust").toggleClass("crust-gluten-free");
+});
 
 
 // });
