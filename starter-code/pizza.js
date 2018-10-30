a116566f72ba48ba2fe0d6764e1c31a61a346dca
PomPom248 <sofiroggero@gmail.com>
2018-10-30 14:37:04 +0100

    1st iteration

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,15 @@
 // Write your Pizza Builder JavaScript in this file.
+var pepperoni= $(".btn-pepperonni").click(function(){
+    $(".pep").toggle();
+})
+
+
+var mushrooms = $(".btn-mushrooms").click(function(){
+    $(".mushroom, .cap, .stem").toggle();
+})
+
+var peppers = $(".btn-green-peppers").click(function(){
+    $(".green-pepper").toggle();
+})
+
+
