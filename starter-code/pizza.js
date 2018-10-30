3069ef0891cdb6d1f9d53a5ecb4c4a6ea18f8c4c
David Diez <ddiezr@gmail.com>
2018-10-30 16:35:30 +0100

    Sedcond commit. Four iterations finished

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,32 +1,48 @@
 // Write your Pizza Builder JavaScript in this file.
-function isActive() {
-    if ($(this).hasClass("active")) {
-        $(this).removeClass("active");
+
+
+function fixPrices() {
+    $(`.price>ul>li:eq(4)`).hide();
+    $(`.price>ul>li:eq(3)`).hide();
+}
+
+fixPrices();
+
+function isActive(thisScope) {
+    if ($(thisScope).hasClass("active")) {
+        $(thisScope).removeClass("active");
     } else {
-        $(this).addClass("active");
+        $(thisScope).addClass("active");
     }
 }
 
+function showPrice(thisScope, ingrIndex) {
+    if ($(thisScope).hasClass("active")) {
+        $(`.price>ul>li:eq(${ingrIndex})`).show();
+    } else {
+        $(`.price>ul>li:eq(${ingrIndex})`).hide();
+    }
+}
+
+function totalPrice(){
+    parseInt($(`strong>span`).text()) += 1
+} 
+
 $(".btn-green-peppers").click(function () {
     $(".green-pepper").toggle();
-    isActive();
+    isActive(this);
+    showPrice(this, 2);
 })
 
 $(".btn-mushrooms").click(function () {
     $(".mushroom").toggle();
-    if ($(this).hasClass("active")) {
-        $(this).removeClass("active");
-    } else {
-        $(this).addClass("active");
-    }
+    isActive(this);
+    showPrice(this, 1);
 })
 $(".btn-pepperonni").click(function () {
     $(".pep").toggle();
-    if ($(this).hasClass("active")) {
-        $(this).removeClass("active");
-    } else {
-        $(this).addClass("active");
-    }
+    isActive(this);
+    showPrice(this, 0);
 })
 $(".btn-crust").click(function () {
     if ($(".crust").hasClass("crust-gluten-free")) {
@@ -34,6 +50,8 @@ $(".btn-crust").click(function () {
     } else {
         $(".crust").addClass("crust-gluten-free");
     }
+    isActive(this);
+    showPrice(this, 4);
 })
 
 $(".btn-sauce").click(function () {
@@ -42,4 +60,6 @@ $(".btn-sauce").click(function () {
     } else {
         $(".sauce").addClass("sauce-white");
     }
+    isActive(this);
+    showPrice(this, 3);
 })
