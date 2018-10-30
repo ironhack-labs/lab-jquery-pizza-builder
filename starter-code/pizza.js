c8fc0aa091127ff3a14787a938f042a39a4ef505
Jorge Devesa <jrgdevesa@gmail.com>
2018-10-30 15:55:05 +0100

    Iteration 1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,16 @@
 // Write your Pizza Builder JavaScript in this file.
+// $(document).ready(function(){
+
+
+  $(".btn-green-peppers").click(function() {
+    $(".green-pepper").toggle();
+  });
+  $(".btn-mushrooms").click(function() {
+    $(".mushroom").toggle();
+  });
+  $(".btn-pepperonni").click(function() {
+    $(".pep").toggle();
+  });
+
+
+// });
