75abd63ef1c063bc0ac6c36b4f3b8dce2d781f8c
aterron84 <aterron84@gmail.com>
2018-10-30 17:05:42 +0100

    Iteration1, Iteration2, Iteration3 completed

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,29 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(".btn.btn-pepperonni.active").click(function(){
+
+    $(".pep").toggle();
+ });
+ $(".btn.btn-mushrooms.active").click(function(){
+ 
+    $(".mushroom").toggle();
+ });
+ $(".btn.btn-green-peppers.active").click(function(){
+ 
+    $(".green-pepper").toggle();
+ });
+ 
+ $(".btn-sauce").click(function(){
+    $(".sauce").toggleClass("sauce-white");
+ });
+ 
+ $(".btn-crust").click(function(){
+    $(".crust").toggleClass("crust-gluten-free");
+ });
+
+ $("button").click(function(){
+    $(this).toggleClass("active");
+ });
+ 
+
+ 
