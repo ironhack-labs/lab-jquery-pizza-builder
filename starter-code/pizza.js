f76fae4297ede6e943e816cbaeb7aaa9b28159b1
Sandra Luengo <luchosandra@gmail.com>
2018-10-30 16:29:01 +0100

    Iteration 2

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -10,12 +10,24 @@ $('.btn-green-peppers').on('click',function(e){
     $('.green-pepper').toggle(".green-pepper");
 });
 
+$("#sauce").removeClass('sauce-white');
 
+$("#pan").removeClass('crust-gluten-free');
 
+$('.btn-sauce').on('click',function(e){
+    if($("#sauce").hasClass('sauce-white')){
+        $("#sauce").removeClass('sauce-white');
+    }else{
+        $("#sauce").addClass('sauce-white');
+    }
+});
 
-
-
-
-
+$('.btn-crust').on('click',function(e){
+    if($("#pan").hasClass('crust-gluten-free')){
+        $("#pan").removeClass('crust-gluten-free');
+    }else{
+        $("#pan").addClass('crust-gluten-free');
+    }
+});
 
 })
