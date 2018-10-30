02b7b3a354cd478c56c5866de22672326463c5ca
PomPom248 <sofiroggero@gmail.com>
2018-10-30 16:52:44 +0100

    2nd iteration

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,4 +1,6 @@
 // Write your Pizza Builder JavaScript in this file.
+
+
 var pepperoni= $(".btn-pepperonni").click(function(){
     $(".pep").toggle();
 })
@@ -11,5 +13,16 @@ var mushrooms = $(".btn-mushrooms").click(function(){
 var peppers = $(".btn-green-peppers").click(function(){
     $(".green-pepper").toggle();
 })
+$("section").removeClass("crust-gluten-free");
+$("section").removeClass("sauce-white");
 
 
+var whiteSauce= $(".btn-sauce").click(function(){
+    $("section:last").addClass("sauce-white");
+
+})
+
+var glutenCrust=$(".btn-crust").click(function(){
+    $("section").addClass("crust-gluten-free");
+})
+//console.log(glutenCrust);
