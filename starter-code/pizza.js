79454e7efbe35c314b9c2088e0a3e7f089d6c4d3
Anna Zhuravleva <helloboysandgirls.az@gmail.com>
2018-10-30 16:04:51 +0100

    Iteration 3: Change the buttons state

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -4,26 +4,34 @@ $(function(){
 
     $('.btn-pepperonni').click(function() {
         $('.pep').toggle();
+        $(this).toggleClass('active');
         });
 
         $('.btn-mushrooms').click(function() {
             $('.mushroom').toggle();
+            $(this).toggleClass('active');
             });
 
             $('.btn-green-peppers').click(function() {
                 $('.green-pepper').toggle();
+                $(this).toggleClass('active');
                 });
 
     $('.crust').removeClass('crust-gluten-free');
 
     $('.sauce').removeClass('sauce-white');
 
+    $('.btn-sauce').removeClass('active');
+    $('.btn-crust').removeClass('active');
+
     $('.btn-crust').click(function() {
         $('.crust').toggleClass('crust-gluten-free');
+        $(this).toggleClass('active');
     });
 
     $('.btn-sauce').click(function() {
         $('.sauce').toggleClass('sauce-white');
+        $(this).toggleClass('active');
     });
 
 
