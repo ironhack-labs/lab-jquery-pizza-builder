589ca140b9fbf3ed99fb564ef0eb6625383b4261
Anna Zhuravleva <helloboysandgirls.az@gmail.com>
2018-10-30 16:13:42 +0100

    Iteration 4: Ingredients and prices

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -5,17 +5,20 @@ $(function(){
     $('.btn-pepperonni').click(function() {
         $('.pep').toggle();
         $(this).toggleClass('active');
-        });
+        $('.price li:contains("pepperonni")').toggle();
+    });
 
-        $('.btn-mushrooms').click(function() {
-            $('.mushroom').toggle();
-            $(this).toggleClass('active');
-            });
+    $('.btn-mushrooms').click(function() {
+        $('.mushroom').toggle();
+        $(this).toggleClass('active');
+        $('.price li:contains("mushrooms")').toggle();
+    });
 
-            $('.btn-green-peppers').click(function() {
-                $('.green-pepper').toggle();
-                $(this).toggleClass('active');
-                });
+    $('.btn-green-peppers').click(function() {
+        $('.green-pepper').toggle();
+        $(this).toggleClass('active');
+        $('.price li:contains("peppers")').toggle();
+    });
 
     $('.crust').removeClass('crust-gluten-free');
 
@@ -24,15 +27,22 @@ $(function(){
     $('.btn-sauce').removeClass('active');
     $('.btn-crust').removeClass('active');
 
+    $('.price li:contains("sauce")').hide();
+    $('.price li:contains("crust")').hide();
+
     $('.btn-crust').click(function() {
         $('.crust').toggleClass('crust-gluten-free');
         $(this).toggleClass('active');
+        $('.price li:contains("crust")').toggle();
     });
 
     $('.btn-sauce').click(function() {
         $('.sauce').toggleClass('sauce-white');
         $(this).toggleClass('active');
+        $('.price li:contains("sauce")').toggle();
     });
+
+
 
 
 });
