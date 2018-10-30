d0d94daa0b3c31e0abe6205e31ce5c1ccb280602
Yared Santiago <yaredmyers@gmail.com>
2018-10-30 17:31:39 +0100

    iteracion 4

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -8,7 +8,8 @@ $(function() {
   $(".sauce").toggleClass("sauce-white");
 
 
-  // Iteration 1
+  $(".price > ul > li:eq(3)").toggle();
+  $(".price > ul > li:eq(4)").toggle();
 
 
   // var pizzaDrawing = $("#pizza > .pep, #pizza > .mushroom, #pizza > .green-pepper");
@@ -16,32 +17,38 @@ $(function() {
   $(".btn-pepperonni").click(function () {
    $("#pizza > .pep").toggle();
    $(".btn-pepperonni").toggleClass("active");
+   $(".price > ul > li:eq(0)").toggle();
+
   });
 
   $(".btn-mushrooms").click(function () {
     $("#pizza > .mushroom").toggle();
     $(".btn-mushrooms").toggleClass("active");
+    $(".price > ul > li:eq(1)").toggle();
 
    });
 
   $(".btn-green-peppers").click(function () {
     $("#pizza > .green-pepper").toggle();
     $(".btn-green-peppers").toggleClass("active");
+    $(".price > ul > li:eq(2)").toggle();
+
    });
 
   $(".btn-sauce").click(function () {
     $(".sauce").toggleClass("sauce-white");
     $(".btn-sauce").toggleClass("active");
-
+    $(".price > ul > li:eq(3)").toggle();
   });
 
   $(".btn-crust").click(function () {
     $(".crust").toggleClass("crust-gluten-free");
     $(".btn-crust").toggleClass("active");
-
-
+    $(".price > ul > li:eq(4)").toggle();
 });
 })
+
+
 
   
 
