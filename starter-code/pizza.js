7ad22edb6e66cafa9db3fae88cf45e0be1690d71
Sandra Luengo <luchosandra@gmail.com>
2018-10-30 17:12:26 +0100

    Iteration 4

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -4,24 +4,30 @@ $('.btn-pepperonni').on('click',function(e){
     $('.pep').toggle(".pep");
     if($(".btn-pepperonni").hasClass('active')){
         $(".btn-pepperonni").removeClass('active');
+        $(".price li:eq(0)").hide();
     }else{
         $(".btn-pepperonni").addClass('active');
+        $(".price li:eq(0)").show();
     }
 });
 $('.btn-mushrooms').on('click',function(e){
     $('.mushroom').toggle(".mushroom");
     if($(".btn-mushrooms").hasClass('active')){
         $(".btn-mushrooms").removeClass('active');
+        $(".price li:eq(1)").hide();
     }else{
         $(".btn-mushrooms").addClass('active');
+        $(".price li:eq(1)").show();
     }
 });
 $('.btn-green-peppers').on('click',function(e){
     $('.green-pepper').toggle(".green-pepper");
     if($(".btn-green-peppers").hasClass('active')){
         $(".btn-green-peppers").removeClass('active');
+        $(".price li:eq(2)").hide();
     }else{
         $(".btn-green-peppers").addClass('active');
+        $(".price li:eq(2)").show();
     }
 });
 
@@ -30,9 +36,11 @@ $('.btn-sauce').on('click',function(e){
     if($("#sauce").hasClass('sauce-white')){
         $("#sauce").removeClass('sauce-white');
         $(".btn-sauce").removeClass('active');
+        $(".price li:eq(3)").hide();
     }else{
         $("#sauce").addClass('sauce-white');
         $(".btn-sauce").addClass('active');
+        $(".price li:eq(3)").show();
     }
 });
 
@@ -40,9 +48,11 @@ $('.btn-crust').on('click',function(e){
     if($("#pan").hasClass('crust-gluten-free')){
         $("#pan").removeClass('crust-gluten-free');
         $(".btn-crust").removeClass('active');
+        $(".price li:eq(4)").hide();
     }else{
         $("#pan").addClass('crust-gluten-free');
         $(".btn-crust").addClass('active');
+        $(".price li:eq(4)").show();
     }
 });
 
@@ -53,5 +63,8 @@ $("#pan").removeClass('crust-gluten-free');
 $(".btn-sauce").removeClass('active');
 
 $(".btn-crust").removeClass('active');
+
+$(".price li:eq(3)").hide();
+$(".price li:eq(4)").hide();
 
 });
