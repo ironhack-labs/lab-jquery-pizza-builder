20bf18b95c20cdfa2fd5e122903c02926faa9ff2
Sonia Bravo <rinbra0@hotmail.com>
2018-10-30 17:23:52 +0100

    Iteration1-4 fixed

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,37 +1,61 @@
 // Write your Pizza Builder JavaScript in this file.
 
-$(".li-gluten-free-crust").hide();
-$(".li-white-sauce").hide();
+$(".btn-sauce").toggleClass("active");
+$(".btn-crust").toggleClass("active");
+$(".crust").toggleClass("crust-gluten-free");
+$(".sauce").toggleClass("sauce-white");
+$(".panel li:contains('gluten-free crust')").toggle();
+$(".panel li:contains('white sauce')").toggle();
+setTotalPrice();    
 
+function setTotalPrice(){
+  var totalPrice = 10;
+  $(".price li:visible").each(function(){
+    //console.log($(this).text().substring(1,2));
+    totalPrice+=parseInt($(this).text().substring(1,2));
+    
+    }
+  );
+  $(".price strong").text(`$${totalPrice}`);
+}
+
+  
 
 
 $(".btn").click(function(){
   $(this).toggleClass("active");
+  
+  
 });   
 
 $(".btn-pepperonni").click(function(){
   $(".pep").toggle();
-  $(".li-pepperonni").toggle();
+  $(".panel li:contains('pepperonni')").toggle();
+  setTotalPrice();
 });
 
 $(".btn-mushrooms").click(function(){
   $(".mushroom").toggle();
-  $(".li-mushrooms").toggle();
+  $(".panel li:contains('mushrooms')").toggle();
+  setTotalPrice();
 });
 
 $(".btn-green-peppers").click(function(){
   $(".green-pepper").toggle();
-  $(".li-green-peppers").toggle();
+  $(".panel li:contains('green peppers')").toggle();
+  setTotalPrice();
 });
 
 $(".btn-sauce").click(function(){
   $(".sauce").toggleClass("sauce-white");
-  $(".li-white-sauce").toggle();
+  $(".panel li:contains('white sauce')").toggle();
+  setTotalPrice();
 });
 
 $(".btn-crust").click(function(){
   $(".crust").toggleClass("crust-gluten-free");
-  $(".li-gluten-free-crust").toggle();
+  $(".panel li:contains('gluten-free crust')").toggle();
+  setTotalPrice();
 });
 
 
