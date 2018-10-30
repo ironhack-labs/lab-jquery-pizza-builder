d5641c4c70b1e4bc9c7f1a6f26d2880ca471def1
Yared Santiago <yaredmyers@gmail.com>
2018-10-30 17:12:48 +0100

    iteration3

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,7 +1,11 @@
 // Write your Pizza Builder JavaScript in this file.
 $(function() {
   // alert("DOM is Loaded");
- 
+  $(".btn-pepperonni").toggleClass("active");
+  $(".btn-mushrooms").toggleClass("active");
+  $(".btn-green-peppers").toggleClass("active");
+  $(".crust").toggleClass("crust-gluten-free");
+  $(".sauce").toggleClass("sauce-white");
 
 
   // Iteration 1
@@ -9,25 +13,33 @@ $(function() {
 
   // var pizzaDrawing = $("#pizza > .pep, #pizza > .mushroom, #pizza > .green-pepper");
   // pizzaDrawing.hide();
-
-
-  $(".btn-green-peppers").click(function () {
-   $("#pizza > .green-pepper").toggle();
-  });
-  $(".btn-mushrooms").click(function () {
-   $("#pizza > .mushroom").toggle();
-  });
-  
   $(".btn-pepperonni").click(function () {
    $("#pizza > .pep").toggle();
+   $(".btn-pepperonni").toggleClass("active");
   });
+
+  $(".btn-mushrooms").click(function () {
+    $("#pizza > .mushroom").toggle();
+    $(".btn-mushrooms").toggleClass("active");
+
+   });
+
+  $(".btn-green-peppers").click(function () {
+    $("#pizza > .green-pepper").toggle();
+    $(".btn-green-peppers").toggleClass("active");
+   });
 
   $(".btn-sauce").click(function () {
     $(".sauce").toggleClass("sauce-white");
+    $(".btn-sauce").toggleClass("active");
+
   });
 
   $(".btn-crust").click(function () {
     $(".crust").toggleClass("crust-gluten-free");
+    $(".btn-crust").toggleClass("active");
+
+
 });
 })
 
