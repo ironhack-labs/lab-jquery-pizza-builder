68f44644d3cf95a03f11abf2e8bdeb992bdf2b24
Jorge Devesa <jrgdevesa@gmail.com>
2018-10-30 16:48:40 +0100

    Iteration 3 done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -3,27 +3,39 @@
 
 $(".btn-green-peppers").click(function() {
   $(".green-pepper").toggle();
+  $(".btn-green-peppers").toggleClass("active");
 });
 $(".btn-mushrooms").click(function() {
   $(".mushroom").toggle();
+  $(".btn-mushrooms").toggleClass("active");
 });
 $(".btn-pepperonni").click(function() {
   $(".pep").toggle();
+
+$(".btn-pepperonni").toggleClass("active");
 });
 
 $(document).ready(function() {
   $(".crust").removeClass("crust-gluten-free");
   $(".sauce").removeClass("sauce-white");
+  $(".btn-sauce").removeClass("active");
+  $(".btn-crust").removeClass("active");
+
 
 });
 
 $(".btn-sauce").click(function() {
     $(".sauce").toggleClass("sauce-white");
+    $(".btn-sauce").toggleClass("active");
 });
 
 $(".btn-crust").click(function() {
     $(".crust").toggleClass("crust-gluten-free");
+    $(".btn-crust").toggleClass("active");
+
 });
 
+function changeButton(){
 
+}
 // });
