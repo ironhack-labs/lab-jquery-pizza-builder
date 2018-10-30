cf5650c6f6d2bce2e9fe69561db218b30515e563
PomPom248 <sofiroggero@gmail.com>
2018-10-30 17:46:33 +0100

    4th iteration

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -4,34 +4,45 @@
 var pepperoni= $(".btn-pepperonni").click(function(){
     $(".pep").toggle();
     $(".btn-pepperonni").toggleClass("active")
+    $(".price li:contains('pepperonni')").toggle();
 })
 
 
 var mushrooms = $(".btn-mushrooms").click(function(){
     $(".mushroom, .cap, .stem").toggle();
     $(".btn-mushrooms").toggleClass("active")
+    $(".price li:contains('mushrooms')").toggle();
 })
 
 
 var peppers = $(".btn-green-peppers").click(function(){
     $(".green-pepper").toggle();
     $(".btn-green-pepers").toggleClass("active")
+    $(".price li:contains('peppers')").toggle();
+
 })
 $("section").removeClass("crust-gluten-free");
 $(".btn, .btn-sauce").removeClass("active")
 $("section").removeClass("sauce-white");
 $(".btn, .btn-crust").removeClass("active")
+$(".price li:contains('gluten-free crust')").hide();
+$(".price li:contains('white sauce')").hide();
 
 
 
 var whiteSauce= $(".btn-sauce").click(function(){
     $("section:last").toggleClass("sauce-white");
     $(".btn-sauce").toggleClass("active")
+    $(".price li:contains('white sauce')").toggle();
+
 
 })
 
 var glutenCrust=$(".btn-crust").click(function(){
     $("section").toggleClass("crust-gluten-free");
     $(".btn-crust").toggleClass("active")
+    $(".price li:contains('gluten free crust')").toggle();
 })
+
+
 
