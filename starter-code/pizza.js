1ba8b34fbb47a6debe12ee647ce5c257c6a4c762
RuddyLotar <ruddylotar@gmail.com>
2018-10-30 15:04:26 +0100

    Iteration 1 and 2

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,56 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(function(){
+
+
+
+  $('.btn-pepperonni').click(function() {
+
+    var isActive = true;
+
+    if(isActive){
+      $('.pep').toggle();
+      isActive = false;
+    }else{
+      $('.pep').toggle();
+    }
+  
+  });
+
+  $('.btn-mushrooms').click(function() {
+    var isActive = true;
+
+    if(isActive){
+      $('.mushroom').toggle();
+      isActive = false;
+    }else{
+      $('.mushroom').toggle();
+    }
+  });
+
+  $('.btn-green-peppers').click(function() {
+    var isActive = true;
+
+    if(isActive){
+      $('.green-pepper').toggle();
+      isActive = false;
+    }else{
+      $('.green-pepper').toggle();
+    }
+  });
+  
+
+
+  $('.btn-crust').click(function() {
+   
+    $('.crust').toggleClass('crust-gluten-free');
+
+  });
+
+  $('.btn-sauce').click(function() {
+   
+    $('.sauce').toggleClass('sauce-white');
+
+  });
+
+})
