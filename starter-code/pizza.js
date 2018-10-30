22ede6817a18f70a8993882613a73f10d9e1c963
Fer Perez Ayala <ferp@protonmail.ch>
2018-10-30 15:03:16 +0100

    Iterations 3 & 4 complete.

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -5,8 +5,11 @@ $(function() {
     $('.sauce').toggleClass('sauce-white')
     $('.btn-sauce').toggleClass("active");
 
-    $('.crust').toggleClass('crust-gluten-free')
+    $('.crust').toggleClass('crust-gluten-free');
     $('.btn-crust').toggleClass("active");
+
+    $('.panel.price li').eq(3).toggle();
+    $('.panel.price li').eq(4).toggle();
 
   });
   
@@ -14,24 +17,29 @@ $(function() {
 $('.btn-pepperonni').on("click", function(){
     $('.pep').toggle();
     $(this).toggleClass("active");
+    $('.panel.price li').eq(0).toggle();
 })
 
 $('.btn-mushrooms').on("click", function(){
     $('.mushroom').toggle();
     $(this).toggleClass("active");
+    $('.panel.price li').eq(1).toggle()
 })
 
 $('.btn-green-peppers').on("click", function(){
     $('.green-pepper').toggle();
     $(this).toggleClass("active");
+    $('.panel.price li').eq(2).toggle()
 })
 
 $('.btn-sauce').on("click", function(){
     $('.sauce').toggleClass('sauce-white')
     $(this).toggleClass("active");
+    $('.panel.price li').eq(3).toggle()
 })
 
 $('.btn-crust').on("click", function(){
     $('.crust').toggleClass('crust-gluten-free')
     $(this).toggleClass("active");
+    $('.panel.price li').eq(4).toggle();
 })
