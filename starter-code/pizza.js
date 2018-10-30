91b4bde35bbfaf26e74931166c5cbd1b8dd26edf
Yared Santiago <yaredmyers@gmail.com>
2018-10-30 15:58:47 +0100

    iteracion 1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,27 @@
 // Write your Pizza Builder JavaScript in this file.
+$(function() {
+  // alert("DOM is Loaded");
+ 
+  // $("button").click(function(){
+  //   $("#pizza").toggle();
+
+  // Iteration 1
+
+
+  // var pizzaDrawing = $("#pizza > .pep, #pizza > .mushroom, #pizza > .green-pepper");
+  // pizzaDrawing.hide();
+
+  
+  $(".btn-green-peppers").click(function () {
+   $("#pizza > .green-pepper").toggle();
+  });
+  $(".btn-mushrooms").click(function () {
+   $("#pizza > .mushroom").toggle();
+  });
+  
+  $(".btn-pepperonni").click(function () {
+   $("#pizza > .pep").toggle();
+  });
+  })
+
+
