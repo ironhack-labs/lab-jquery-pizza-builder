adccfe3c3fd1806e35873d9265cbd43757db9a4c
Fer Perez Ayala <ferp@protonmail.ch>
2018-10-30 14:05:15 +0100

    Iteration 1 complete

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,17 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$('.btn-pepperonni').on("click", function(){
+    $('.pep').toggle();
+    $(this).toggleClass("active");
+})
+
+$('.btn-mushrooms').on("click", function(){
+    $('.mushroom').toggle();
+    $(this).toggleClass("active");
+})
+
+$('.btn-green-peppers').on("click", function(){
+    $('.green-pepper').toggle();
+    $(this).toggleClass("active");
+})
+
