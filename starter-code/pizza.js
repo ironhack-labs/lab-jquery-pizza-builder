a1f73138e4f47ee501631b66d05626793e7b2de4
Fer Perez Ayala <ferp@protonmail.ch>
2018-10-30 16:23:57 +0100

    Exercise Complete.

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,7 +1,7 @@
 // Write your Pizza Builder JavaScript in this file.
 
-$(function() {
-    
+$(function () {
+
     $('.sauce').toggleClass('sauce-white')
     $('.btn-sauce').toggleClass("active");
 
@@ -11,35 +11,54 @@ $(function() {
     $('.panel.price li').eq(3).toggle();
     $('.panel.price li').eq(4).toggle();
 
-  });
-  
+    calcPrice();
 
-$('.btn-pepperonni').on("click", function(){
+});
+
+
+$('.btn-pepperonni').on("click", function () {
     $('.pep').toggle();
     $(this).toggleClass("active");
     $('.panel.price li').eq(0).toggle();
+    calcPrice();
 })
 
-$('.btn-mushrooms').on("click", function(){
+$('.btn-mushrooms').on("click", function () {
     $('.mushroom').toggle();
     $(this).toggleClass("active");
-    $('.panel.price li').eq(1).toggle()
+    $('.panel.price li').eq(1).toggle();
+    calcPrice();
 })
 
-$('.btn-green-peppers').on("click", function(){
+$('.btn-green-peppers').on("click", function () {
     $('.green-pepper').toggle();
     $(this).toggleClass("active");
-    $('.panel.price li').eq(2).toggle()
+    $('.panel.price li').eq(2).toggle();
+    calcPrice();
 })
 
-$('.btn-sauce').on("click", function(){
+$('.btn-sauce').on("click", function () {
     $('.sauce').toggleClass('sauce-white')
     $(this).toggleClass("active");
-    $('.panel.price li').eq(3).toggle()
+    $('.panel.price li').eq(3).toggle();
+    calcPrice();
 })
 
-$('.btn-crust').on("click", function(){
+$('.btn-crust').on("click", function () {
     $('.crust').toggleClass('crust-gluten-free')
     $(this).toggleClass("active");
     $('.panel.price li').eq(4).toggle();
+    calcPrice();
 })
+
+
+function calcPrice() {
+    let total = 10;
+    for (i = 0; i < $('.panel.price li:visible').length; i++) {
+        total += parseInt($('.panel.price li:visible').eq(i).text().slice(1));
+    }
+    // console.log(total)
+    $('.panel.price strong').text(`$${total}`);
+
+};
+
