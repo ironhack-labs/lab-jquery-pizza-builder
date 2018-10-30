6e8776b5debff101359f634cbf4711b972d718f3
Sonia Bravo <rinbra0@hotmail.com>
2018-10-30 15:05:46 +0100

    Iteration 4

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,27 +1,37 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(".li-gluten-free-crust").hide();
+$(".li-white-sauce").hide();
+
+
+
 $(".btn").click(function(){
   $(this).toggleClass("active");
 });   
 
 $(".btn-pepperonni").click(function(){
   $(".pep").toggle();
-
-  
+  $(".li-pepperonni").toggle();
 });
 
 $(".btn-mushrooms").click(function(){
   $(".mushroom").toggle();
+  $(".li-mushrooms").toggle();
 });
 
 $(".btn-green-peppers").click(function(){
   $(".green-pepper").toggle();
+  $(".li-green-peppers").toggle();
 });
 
 $(".btn-sauce").click(function(){
   $(".sauce").toggleClass("sauce-white");
+  $(".li-white-sauce").toggle();
 });
 
 $(".btn-crust").click(function(){
   $(".crust").toggleClass("crust-gluten-free");
+  $(".li-gluten-free-crust").toggle();
 });
+
 
