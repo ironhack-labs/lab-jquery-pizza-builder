0a8ec614d8d08b132e50f592a91489f5f1a775f4
Sandra Luengo <luchosandra@gmail.com>
2018-10-30 15:57:57 +0100

    Iteration 1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,21 @@
-// Write your Pizza Builder JavaScript in this file.
+$(function(){
+
+$('.btn-pepperonni').on('click',function(e){
+    $('.pep').toggle(".pep");
+});
+$('.btn-mushrooms').on('click',function(e){
+    $('.mushroom').toggle(".mushroom");
+});
+$('.btn-green-peppers').on('click',function(e){
+    $('.green-pepper').toggle(".green-pepper");
+});
+
+
+
+
+
+
+
+
+
+})
