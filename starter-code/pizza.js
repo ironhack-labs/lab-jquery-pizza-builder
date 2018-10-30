0a11c6767d1fc603bfc83d23221630c341a4a566
Katharina Koenig <k.koenig1811@gmail.com>
2018-10-30 21:59:39 +0100

    Berlin-Katharina

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,34 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$('.btn-pepperonni').click(function() {
+    $('.pep').toggle("true");
+    $('.btn-pepperonni').toggleClass("active");
+});
+
+
+$('.btn-mushrooms').click(function() {
+    $('.mushroom').toggle("true");
+    $('.btn-mushrooms').toggleClass("active");
+});
+
+$('.btn-green-peppers').click(function() {
+    $('.green-pepper').toggle("true");
+    $('.btn-green-peppers').toggleClass("active");
+});
+
+// by default is regular crust
+$('.crust-gluten-free').removeClass("crust-gluten-free");
+
+$('.btn-crust').click(function() {
+    $('.btn-crust').toggleClass("active");
+    $('.crust').toggleClass("crust-gluten-free");
+});
+
+//by default NO white sauce
+$('.sauce-white').removeClass("sauce-white");
+
+$('.btn-sauce').click(function() {
+   $('.btn-sauce').toggleClass("active");
+   $('.sauce').toggleClass("sauce-white");
+});
+
