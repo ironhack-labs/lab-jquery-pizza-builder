0b26c732c66c65b19571094c488f6c7e46b0af5c
sitobusnan <sito@citytonight.es>
2018-10-30 17:45:51 +0100

    media iteration 4

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -6,27 +6,54 @@ $(function(){
     button.toggleClass("active");
     ingredient.toggle();
   }
+  function toggleSauces (button, sauce, putonoff){
+    button.toggleClass("active");
+    sauce.toggleClass(putonoff);
+    list.toggle(priceSauce)
+  }
   
-  //function removeingredient (ingredient){
-  //
-  //}
-  //
-  //function mushrooms (){
-  //
-  //}
-  //function greenPeppers () {
-  //
-  //}
-  //function pepperoni () {
-  //
-  //}
+  function listPrices (ingredient){
+  
+  }
+  
+  function mushrooms (){
+  
+  }
+  function greenPeppers () {
+  
+  }
+  function pepperoni () {
+  
+  }
   
   var mush = $(".mushroom");
   var green = $(".green-pepper");
   var peppe = $(".pep");
+  var sauce = $(".sauce")
+  var crust = $(".crust")
   var mushClick = $(".btn-mushrooms")
   var greenClick = $(".btn-green-peppers")
   var peppeClick = $(".btn-pepperonni")
+  var sauceClick = $(".btn-sauce")
+  var crustClick = $(".btn-crust")
+  var list = $(".price li")
+  console.log(list[3])
+
+  
+  sauce.removeClass("sauce-white");
+  crust.removeClass("crust-gluten-free");
+  sauceClick.removeClass("active");
+  crustClick.removeClass("active");
+  console.log(list[3])
+  list[3].remove();
+  list[4].remove();
+
+  var priceMush = '<li>$1 pepperonni</li>';
+  var priceGreen = '<li>$1 mushrooms</li>';
+  var pricePeppe = '<li>$1 green peppers</li>';
+  var priceSauce = '<li>$1 white sauce</li>';
+  var priceCrust = '<li>$1 gluten-free crust</li>';
+
   
   mushClick.click(function() {
     toggleIngredient(mushClick, mush);
@@ -38,6 +65,14 @@ $(function(){
   peppeClick.click(function(){
     toggleIngredient(peppeClick, peppe);
   });
+
+  sauceClick.click(function(){
+    toggleSauces(sauceClick,sauce,"sauce-white")
+  })
+
+  crustClick.click(function(){
+    toggleSauces(crustClick,crust,"crust-gluten-free")
+  })
   
   
   
