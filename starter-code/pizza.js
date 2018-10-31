94d4be069fff422d684a7d07d20988a0cabbbc71
Katharina Koenig <k.koenig1811@gmail.com>
2018-10-31 10:29:03 +0100

    Berlin-Katharina 3rd Push

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -6,8 +6,6 @@ $('.btn-pepperonni').click(function() {
 });
 
 
-
-
 $('.btn-mushrooms').click(function() {
     $('.mushroom').toggle("true");
     $('.btn-mushrooms').toggleClass("active");
@@ -34,6 +32,31 @@ $('.btn-sauce').click(function() {
    $('.sauce').toggleClass("sauce-white");
 });
 
-//by default REMOVE sauce-white and gluten-free-crust from price-list
+//by default REMOVE white sauce and gluten-free crust from price-list
 $('.price > ul > li:contains("white")').hide();
 $('.price > ul > li:contains("gluten")').hide();
+
+//click btn-pepperoni = remove pep from price list
+$('.btn-pepperonni').click(function() {
+    $('.price >ul > li:contains("pepperonni")').toggle();
+})
+
+//click btn-sauce = white-sauce on price list
+$('.btn-sauce').click(function() {
+    $('.price >ul > li:contains("white")').toggle();
+})
+
+//click btn-crust = gluten-free-crust on price list
+$('.btn-crust').click(function() {
+    $('.price >ul > li:contains("crust")').toggle();
+})
+
+//click btn-mushroom = remove mushrooms from price list
+$('.btn-mushrooms').click(function() {
+    $('.price >ul > li:contains("mushroom")').toggle();
+})
+
+$('.btn-green-peppers').click(function() {
+    $('.price >ul > li:contains("green")').toggle();
+})
+
