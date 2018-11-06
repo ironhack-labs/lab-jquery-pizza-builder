2b01cffe8d51efa038e5e0551a5855bb1dc30826
conviertemx <ro@convierte.mx>
2018-11-06 13:48:39 -0600

    iteracion 4 done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -4,6 +4,8 @@
 
 var pizza = $('#pizza')
 
+var priceList = $('#priceList')
+//console.log(priceList)
 
 //Toppings
 
@@ -23,44 +25,78 @@ var btnCrust = $('.btn-crust')
 
 //listeners click
 
+// Pepperoni
 btnPepp.on('click',function(e){
     var opacity = pepperoni.css('opacity')
     if(opacity === '1'){
         pepperoni.css('opacity','0')
+        var liPep = $("li:contains('$1 pepperonni')")
+        liPep.remove()
+        //priceList.removeChild(liPep)
     } else{
         pepperoni.css('opacity','1')
+        priceList.append("<li>$1 pepperonni</li>")
     }
     btnPepp.toggleClass('active')
+
 })
 
+// Mushroom
 btnMush.on('click',function(e){
     var opacity = mushroom.css('opacity')
     if(opacity === '1'){
         mushroom.css('opacity','0')
+        var liMush = $("li:contains('$1 mushrooms')")
+        liMush.remove()
     } else{
         mushroom.css('opacity','1')
+        priceList.append("<li>$1 mushrooms</li>")
+        
     }
     btnMush.toggleClass('active')
 })
 
+// Green Pepper
 btnGreen.on('click',function(e){
     var opacity = greenPepper.css('opacity')
     if(opacity === '1'){
         greenPepper.css('opacity','0')
+        var liGreen = $("li:contains('$1 green peppers')")
+        liGreen.remove()
     } else{
         greenPepper.css('opacity','1')
+        priceList.append("<li>$1 green peppers</li>")
     }
     btnGreen.toggleClass('active')
 })
-
+//White Sauce
 btnSauce.on('click',function(e){
     sauce.toggleClass('sauce-white')
     btnSauce.toggleClass('active')
+    if(sauce.hasClass('sauce-white'))
+    {
+    priceList.append("<li>$3 white sauce</li>") 
+    }else{
+    var liGreen = $("li:contains('$3 white sauce')")
+        liGreen.remove()  
+    }
+
+    //var liGreen = $("li:contains('$1 green peppers')")
+    //liGreen.remove()
 })
+
+//Gluten
 
 btnCrust.on('click',function(e){
     crust.toggleClass('crust-gluten-free')
     btnCrust.toggleClass('active')
+    if(crust.hasClass('crust-gluten-free'))
+    {
+    priceList.append("<li>$5 gluten-free crust</li>") 
+    }else{
+    var liGreen = $("li:contains('$5 gluten-free crust')")
+        liGreen.remove()  
+    }
 })
 
 
