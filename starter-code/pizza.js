fdd523f6d3def7e760098ecbe57711ba91b63da8
conviertemx <ro@convierte.mx>
2018-11-06 12:32:57 -0600

    iteracion 1 done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,62 @@
 // Write your Pizza Builder JavaScript in this file.
+
+// Para cachar la pizza
+
+var pizza = $('#pizza')
+
+
+//Toppings
+
+var pepperoni = $('.pep')
+var mushroom = $('.mushroom')
+var greenPepper = $('.green-pepper')
+var sauce = $('.sauce')
+var Crust = $('.crust')
+
+
+//Botones
+var btnPepp = $('.btn-pepperonni')
+var btnMush = $('.btn-mushrooms')
+var btnGreen = $('.btn-green-peppers')
+var btnSauce = $('.btn-sauce')
+var btnCrust = $('.btn-crust')
+
+//listeners click
+
+btnPepp.on('click',function(e){
+    var opacity = pepperoni.css('opacity')
+    if(opacity === '1'){
+        pepperoni.css('opacity','0')
+    } else{
+        pepperoni.css('opacity','1')
+    }
+})
+btnMush.on('click',function(e){
+    var opacity = mushroom.css('opacity')
+    if(opacity === '1'){
+        mushroom.css('opacity','0')
+    } else{
+        mushroom.css('opacity','1')
+    }
+})
+
+btnGreen.on('click',function(e){
+    var opacity = greenPepper.css('opacity')
+    if(opacity === '1'){
+        greenPepper.css('opacity','0')
+    } else{
+        greenPepper.css('opacity','1')
+    }
+})
+
+/*btnSauce.on('click',function(e){
+    sauce.css('opacity','0')
+})
+
+btnCrust.on('click',function(e){
+    sauce.css('opacity','0')
+})
+*/
+
+
+
