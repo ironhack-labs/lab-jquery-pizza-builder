46274da3ee87a9ef4d9e6b9c583d38e65c16691f
Gonzalo Sobrino <gonzalosobrino6@gmail.com>
2018-11-12 21:23:02 +0100

    almost done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,33 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$( document ).ready(function() {
+    $(".btn-pepperonni").click(function() {
+        $(".pep").toggle();
+        $(this).toggleClass("active");
+    });
+
+    $(".btn-mushrooms").click(function() {
+        $(".mushroom").toggle();
+        $(this).toggleClass("active");
+    });
+
+    $(".btn-green-peppers").click(function() {
+        $(".green-pepper").toggle();
+        $(this).toggleClass("active");
+    });
+
+    $(".crust").removeClass("crust-gluten-free");
+
+    $(".btn-crust").click(function() {
+        $(".crust").toggleClass("crust-gluten-free");
+        $(this).toggleClass("active");
+    });
+
+    $(".btn-sauce").click(function() {
+        $(".sauce").toggleClass("sauce-white");
+        $(this).toggleClass("active");
+    });
+
+
+
+});
