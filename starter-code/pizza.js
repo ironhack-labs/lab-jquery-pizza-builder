8ea52a6b1d2853704ecfbde2c6eeb35c65e0a76c
CarlosDiazGirol <carlosdiazgirol@hotmail.com>
2018-11-12 21:34:32 +0100

    rica pizza

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,29 @@
 // Write your Pizza Builder JavaScript in this file.
+
+
+
+$(document).ready(function() {
+
+  function handleBtn(btnClass, ingredient) {
+    $(btnClass).click(function(){
+      $(ingredient).toggle();
+      $(this).toggleClass("active");
+    });
+  };
+
+  function base(btnClass, ingredient, bases) {
+    $(btnClass).click(function(){
+      $(ingredient).toggleClass(bases);
+      $(this).toggleClass("active");
+    });
+  };
+  handleBtn('.btn-pepperonni', '.pep');
+  handleBtn('.btn-green-peppers', '.green-pepper');
+  handleBtn('.btn-mushrooms', '.mushroom');
+  base('.btn-sauce', '.sauce', 'sauce-white');
+  base('.btn-crust', '.crust', 'crust-gluten-free');
+});
+
+
+
+
