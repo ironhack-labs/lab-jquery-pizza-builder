ec7c07903edf044440a0638326a59022236edb3b
Lucas <lucas@MacBook-Air-de-Lucas.local>
2018-11-14 11:30:48 +0100

    Doesnt works the Total Price

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,53 @@
 // Write your Pizza Builder JavaScript in this file.
+
+// Write your Pizza Builder JavaScript in this file.
+
+
+
+$(document).ready(function() {
+
+resultPrice = 21;
+
+  function handleBtn(btnClass, ingredient, value) {
+   $(btnClass).click(function(){
+     $(ingredient).toggle();
+
+     $(this).toggleClass("active");
+     if ($(this).hasClass("active")) {
+       resultPrice = resultPrice + value;
+       modificar(".price strong");
+      }else{
+        return resultPrice = resultPrice - value;
+        }
+   });
+ };
+
+ function base(btnClass, ingredient, base, value) {
+   $(btnClass).click(function(){
+     $(ingredient).toggleClass(base);
+     $(this).toggleClass("active");
+
+      if ($(this).hasClass("active")) {
+      resultPrice = value + resultPrice;
+      modificar(".price strong");
+      }else{
+       return resultPrice = value - resultPrice;
+      }
+    
+   });
+ };
+
+  function modificar(btnClass){
+    $(btnClass).click(function(){
+    $(".price strong").text("$" + resultPrice);
+  });
+};
+
+
+
+ handleBtn('.btn-pepperonni', '.pep', 1);
+ handleBtn('.btn-green-peppers', '.green-pepper', 2);
+ handleBtn('.btn-mushrooms', '.mushroom', 3);
+ base('.btn-sauce', '.sauce', 'sauce-white', 4);
+ base('.btn-crust', '.crust', 'crust-gluten-free', 5);
+});
