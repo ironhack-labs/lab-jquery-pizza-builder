1f4447528af84af866c21c3c10d173b459cc9040
Daniel Goycochea <danielgoycocheardz@gmail.com>
2018-11-15 17:24:56 -0600

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,79 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(document).ready(function() {
+  
+    // $(".green-pepper").hide();
+    // $(".mushroom").hide();
+    // $(".pep").hide();
+  
+    $(".btn-green-peppers").click(function(){
+      $(".green-pepper").toggle()
+      $(".btn-green-peppers").toggleClass("active");
+      $("aside > ul > li:eq(2)").toggle();
+      precioTotal(); 
+    })
+  
+    $(".btn-mushrooms").click(function(){
+      $(".mushroom").toggle()
+      $(".btn-mushrooms").toggleClass("active");
+      $("aside > ul > li:eq(1)").toggle();
+      precioTotal()
+    })
+  
+    $(".btn-pepperonni").click(function(){
+      $(".pep").toggle()
+      $(".btn-pepperonni").toggleClass("active");
+      $("aside > ul > li:eq(0)").toggle();
+      precioTotal()
+    })
+  
+ 
+    $(".btn-sauce").click(function(){
+      $(".btn-sauce").toggleClass("active")
+      $(".sauce").toggle()
+      $("aside > ul > li:eq(3)").toggle();
+      precioTotal()
+
+    })
+  
+    $(".btn-crust").click(function(){
+      $(".btn-crust").toggleClass("active")
+      $(".cheese").toggle()
+      $("aside > ul > li:eq(4)").toggle();
+      precioTotal()
+    })
+
+    function precioTotal(){
+      var precio= 10;
+    
+      if ($('.btn-pepperonni').hasClass('active')) {
+        precio += 1;
+      }
+      if ($('.btn-mushrooms').hasClass('active')) {
+        precio += 1;
+      }
+      if ($('.btn-green-peppers').hasClass('active')) {
+        precio += 1;
+      }
+      if ($('.btn-sauce').hasClass('active')) {
+        precio += 3;
+      }
+      if ($('.btn-crust').hasClass('active')) {
+        precio += 5;
+      }
+    
+      $('aside>strong').text("$"+precio);
+    }
+    
+     $(".btn-green-peppers").toggleClass("active");
+     $(".green-pepper").toggle()
+     $(".btn-mushrooms").removeClass("active");
+     $(".mushroom").toggle()
+     $(".btn-pepperonni").removeClass("active");
+     $(".pep").toggle()
+  
+     $('aside>strong').text("$"+18);
+       
+    $("aside > ul > li:lt(3)").hide();
+  
+  }); 
