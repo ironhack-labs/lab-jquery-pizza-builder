c7ceffd6059aa4ca39831eace3828fc0490d4e01
Moises Garcia <moisesgarcia83@gmail.com>
2018-11-19 14:22:38 +0100

    refactoring

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,5 +1,7 @@
 // Write your Pizza Builder JavaScript in this file.
 $(function () { 
+
+/*  
 $( ".btn-pepperonni" ).click(function() {
   $(".pep").toggle();
   $(this).toggleClass("active");
@@ -37,12 +39,55 @@ $( ".btn-sauce" ).click(function() {
   $(".sauce").toggleClass("sauce-white");
   $("li:contains('$3 white sauce')").toggle();
 });
+*/ // codigo inicial
 
-var totalBill = 10;
+
+$(".btn-sauce").toggleClass("active");
+$(".sauce").toggleClass("sauce-white");
+$("li:contains('$3 white sauce')").hide();
+
+$(".btn-crust").toggleClass("active");
+$(".crust").toggleClass("crust-gluten-free");
+$("li:contains('$5 gluten-free crust')").hide();
+
+// para empezar desactivamos el botón y ocultamos de la lista gluten free y sauce white
+
+function ingredientActive (btn, classIngredient, licontains){
+  $(btn).click(function() {
+    $(classIngredient).toggle();
+    $(btn).toggleClass("active");
+    $(licontains).toggle();
+  });
+} 
+
+// con esta funcion desactivamos el boton, eliminamos el ingrediente y lo quitamos de la lista
+
+
+function ingredientNoActive (btn, classIngredient, classActive, licontains){
+  $(btn).click(function() { 
+    $(btn).toggleClass("active");
+    $(classIngredient).toggleClass(classActive);
+    $(licontains).toggle();
+  });  
+}
+
+// con esta funcion activamos el boton, eliminamos el ingrediente y lo quitamos de la lista
+
+
+ingredientActive(".btn-pepperonni", ".pep", "li:contains('$1 pepperonni')");
+ingredientActive(".btn-mushrooms", ".mushroom", "li:contains('$1 mushrooms')");
+ingredientActive(".btn-green-peppers", ".green-pepper", "li:contains('$1 green peppers')");
+ingredientNoActive(".btn-crust", ".crust", "crust-gluten-free", "li:contains('$5 gluten-free crust')");
+ingredientNoActive(".btn-sauce", ".sauce", "sauce-white", "li:contains('$3 white sauce')");
+
+// llamamos a todas las funciones para cuando hagamos click en cada boton
+
 $("strong").text("$13");
 
+// establecemos el importe inicial de la pizza por defecto $13
 
 function updatePrice(){
+  var totalBill = 10;
   $(".btn").click(function() {
     $( ".panel.price li:visible" ).each(function() {
       var priceUnd = parseInt($(this).text().substr(1,1));
@@ -54,5 +99,6 @@ function updatePrice(){
   }
   updatePrice();
 })
-  
+
+// en esta función actualizamos el precio de la cuenta según se vayan seleccionando o no los ingredientes
 
