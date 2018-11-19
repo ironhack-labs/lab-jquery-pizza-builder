d443ebf65ff8ef73860a9ae893d78e2e1e31ec1d
@cdiaz <>
2018-11-19 15:46:53 +0100

    a falta de la suma

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,28 +1,41 @@
 // Write your Pizza Builder JavaScript in this file.
 
-
-
 $(document).ready(function() {
+  var prices = document.querySelectorAll('.price ul li');
+  var priceOne = 1;
+  var priceThree = 3;
+  var priceFive = 5;
+  prices[0].value = priceOne;
+  prices[1].value = priceOne;
+  prices[2].value = priceOne;
+  prices[3].value = priceThree;
+  prices[4].value = priceFive;
+  suma = prices[0].value + prices[1].value + prices[2].value + prices[3].value + prices[4].value;
 
-  function handleBtn(btnClass, ingredient) {
+  function handleBtn(btnClass, ingredient, value) {
     $(btnClass).click(function(){
       $(ingredient).toggle();
       $(this).toggleClass("active");
+      $('.price ul li:nth-child(' + value + ')').toggle();
     });
   };
 
-  function base(btnClass, ingredient, bases) {
+  function base(btnClass, ingredient, base, value) {
     $(btnClass).click(function(){
-      $(ingredient).toggleClass(bases);
+      $(ingredient).toggleClass(base);
       $(this).toggleClass("active");
+      $('.price ul li:nth-child(' + value + ')').toggle();
     });
   };
-  handleBtn('.btn-pepperonni', '.pep');
-  handleBtn('.btn-green-peppers', '.green-pepper');
-  handleBtn('.btn-mushrooms', '.mushroom');
-  base('.btn-sauce', '.sauce', 'sauce-white');
-  base('.btn-crust', '.crust', 'crust-gluten-free');
+
+  handleBtn('.btn-pepperonni', '.pep', 1);
+  handleBtn('.btn-green-peppers', '.green-pepper', 2);
+  handleBtn('.btn-mushrooms', '.mushroom', 3);
+  base('.btn-sauce', '.sauce', 'sauce-white', 4);
+  base('.btn-crust', '.crust', 'crust-gluten-free', 5);
 });
+  
+
 
 
 
