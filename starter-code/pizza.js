781b9d5e65b4a525de760dde58f094df5f7c66bb
Mariana <marianamrs1995@hotmail.com>
2018-12-04 19:38:03 -0200

    feito

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,4 +1,12 @@
 // Write your Pizza Builder JavaScript in this file.
+let price = 21;
+ function containElement(e, ePrice){
+    if(e.hasClass('active')){
+        $('.price strong').text("$"+(price+=ePrice));
+    }else{
+        $('.price strong').text("$"+(price-=ePrice));
+    }
+}
 $('document').ready(() => {
     $('.sauce').removeClass('sauce-white');
     $('.crust').removeClass('crust-gluten-free');
@@ -8,6 +16,7 @@ $('document').ready(() => {
         $this.toggleClass('active');
         $('.pep').toggle('btn-pepperoni');
         $('.price li:contains(pepperonni)').toggle(600);
+        containElement($this,1);
     });
 
     //add mushrooms
@@ -16,6 +25,7 @@ $('document').ready(() => {
         $this.toggleClass('active');
         $('.mushroom').toggle();
         $('.price li:contains(mushroom)').toggle(600);
+        containElement($this,1);
     });
 
     //add green-pepper
@@ -24,6 +34,7 @@ $('document').ready(() => {
         $this.toggleClass('active');
         $('.green-pepper').toggle();
         $('.price li:contains(green)').toggle(600);
+        containElement($this,1);
     });
 
     //add white-sauce
@@ -32,6 +43,7 @@ $('document').ready(() => {
         $this.toggleClass('active');
         $('.sauce').toggleClass('sauce-white');
         $('.price li:contains(sauce)').toggle(600);
+        containElement($this,3);
     });
 
     //add gluten free crust
@@ -40,5 +52,6 @@ $('document').ready(() => {
         $this.toggleClass('active');
         $('.crust').toggleClass('crust-gluten-free');
         $('.price li:contains(gluten)').toggle(600);
+        containElement($this,5);
     });
 })
