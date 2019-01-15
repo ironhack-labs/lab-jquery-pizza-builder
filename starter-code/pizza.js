2fb16896bdc1da6798052fd195a3bb62b1da8aa7
Rojiblanco182 <robertosalinas1988@gmail.com>
2019-01-15 19:02:23 +0100

    first version

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,42 @@
 // Write your Pizza Builder JavaScript in this file.
+var liArr = [];
+liArr = $("aside.panel.price ul li");
+
+$(".btn-pepperonni").on("click", function () {
+  $(".pep").toggle();
+  $(this).toggleClass("active");
+})
+
+$(".btn-mushrooms").on("click", function () {
+  $(".mushroom").toggle();
+  $(this).toggleClass("active");
+})
+
+$(".btn-green-peppers").on("click", function () {
+  $(".green-pepper").toggle();
+  $(this).toggleClass("active");
+})
+
+$(".btn-sauce").removeClass("active");
+$(".btn-crust").removeClass("active");
+
+$(".sauce").removeClass("sauce-white");
+$(".crust").removeClass("crust-gluten-free");
+
+$(".btn-sauce").on("click", function () {
+  $(".sauce").toggleClass("sauce-white");
+  $(this).toggleClass("active");
+})
+
+$(".btn-crust").on("click", function () {
+  $(".crust").toggleClass("crust-gluten-free");
+  $(this).toggleClass("active");
+})
+
+$(".panel.price").children("li")
+
+
+
+function updatePrice() {
+
+}
