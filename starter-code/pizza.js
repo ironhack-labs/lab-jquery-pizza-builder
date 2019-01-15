70661d075a2629d4449e7ed47af0dd9a6d2a6b02
Raphael Huber <raphael.ehuber@gmail.com>
2019-01-15 17:12:13 -0200

    finished

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,37 +1,80 @@
 // add and remove green-pepper
+let basePrice = 13;
+let dolar = "$";
 
 $(document).ready (function(){
   $(".btn-green-peppers").click(function(event) {
     $(".green-pepper").toggle();
-    $(".btn-green-peppers").toggleClass("active");
+    $(this).toggleClass("active");
+    $(".pep-price").toggle(500);
+    if ($(this).hasClass("active")){
+      basePrice += 1;
+      $("strong").html(dolar + basePrice);  
+    } else {
+        basePrice -= 1;
+      $("strong").html(dolar + basePrice);
+    }
   });
 });
 
 $(document).ready (function(){
   $(".btn-mushrooms").click(function(event) {
     $(".mushroom").toggle();
-    $(".btn-mushrooms").toggleClass("active");
+    $(this).toggleClass("active");
+    $(".mushroom-price").toggle(500);
+    if ($(this).hasClass("active")){
+      basePrice += 1;
+      $("strong").html(dolar + basePrice);  
+    } else {
+        basePrice -= 1;
+      $("strong").html(dolar + basePrice);
+    }
   })
 })
 
 $(document).ready (function(){
   $(".btn-pepperonni").click(function(event) {
     $(".pep").toggle();
-    $(".btn-pepperonni").toggleClass("active");
+    $(this).toggleClass("active");
+    $(".pepperonni-price").toggle(500);
+    if ($(this).hasClass("active")){
+      basePrice += 1;
+      $("strong").html(dolar + basePrice);  
+    } else {
+        basePrice -= 1;
+      $("strong").html(dolar + basePrice);
+    }
   })
 })
 
 $(document).ready (function(){
   $(".btn-sauce").click(function(event) {
     $(".sauce").toggleClass("sauce-white");
-    $(".btn-sauce").toggleClass("active");
+    $(this).toggleClass("active");
+    $(".sauce-price").toggle(500);
+    if ($(this).hasClass("active")){
+      basePrice += 3;
+      $("strong").html(dolar + basePrice);  
+    } else {
+        basePrice -= 3;
+      $("strong").html(dolar + basePrice);
+    }
   })
 })
 
 $(document).ready (function(){
   $(".btn-crust").click(function(event) {
     $(".crust").toggleClass("crust-gluten-free");
-    $(".btn-crust").toggleClass("active");
-    // toggle display to none / block
-  })
+    $(this).toggleClass("active");
+    $(".crust-price").toggle(500);
+    if ($(this).hasClass("active")){
+      basePrice += 5;
+      $("strong").html(dolar + basePrice);  
+    } else {
+        basePrice -= 5;
+      $("strong").html(dolar + basePrice);
+    }
+  });
 })
+
+
