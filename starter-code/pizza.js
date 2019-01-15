7ade2b7ee4d5b77ef59d68df6ae98db6c5677da8
Raphael Huber <raphael.ehuber@gmail.com>
2019-01-15 15:08:04 -0200

    first draft, done until iteration 4

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,37 @@
-// Write your Pizza Builder JavaScript in this file.
+// add and remove green-pepper
+
+$(document).ready (function(){
+  $(".btn-green-peppers").click(function(event) {
+    $(".green-pepper").toggle();
+    $(".btn-green-peppers").toggleClass("active");
+  });
+});
+
+$(document).ready (function(){
+  $(".btn-mushrooms").click(function(event) {
+    $(".mushroom").toggle();
+    $(".btn-mushrooms").toggleClass("active");
+  })
+})
+
+$(document).ready (function(){
+  $(".btn-pepperonni").click(function(event) {
+    $(".pep").toggle();
+    $(".btn-pepperonni").toggleClass("active");
+  })
+})
+
+$(document).ready (function(){
+  $(".btn-sauce").click(function(event) {
+    $(".sauce").toggleClass("sauce-white");
+    $(".btn-sauce").toggleClass("active");
+  })
+})
+
+$(document).ready (function(){
+  $(".btn-crust").click(function(event) {
+    $(".crust").toggleClass("crust-gluten-free");
+    $(".btn-crust").toggleClass("active");
+    // toggle display to none / block
+  })
+})
