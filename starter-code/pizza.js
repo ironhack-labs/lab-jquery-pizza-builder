85d24d1ab0c82851c4169883fa23bca0ad9901df
cameron-chromiak <cam.chromiak@hotmail.com>
2019-01-29 10:56:11 -0500

    -all buttons work without errors. woo!

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,6 +1,28 @@
 // Write your Pizza Builder JavaScript in this file.
-$(document).ready(function() {
-    $("#pep-btn").click(function(){
-        $('.pep').toggleClass('pepperoni')
-    });
+$( document ).ready(function() {
+    $('.sauce').toggleClass('sauce-white')
+    $('.crust').toggleClass('crust-gluten-free')
 });
+
+$("#pep-btn").click(function(){
+
+    $('.pep').toggleClass('pepperoni')
+});
+
+$('#mushroom-btn').click(function(){
+  $('.mushroom').toggleClass('no-mushroom')
+});
+
+$('#gp-btn').click(function(){
+  $('.green-pepper').toggleClass('no-pepper')
+})
+
+//Sauce
+
+$('#sauce-btn').click(function(){
+  $('.sauce').toggleClass('sauce-white')
+})
+
+$('#crust-btn').click(function(){
+    $('.crust').toggleClass('crust-gluten-free')
+})
