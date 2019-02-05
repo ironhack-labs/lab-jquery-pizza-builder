10361321d1e68353228da0b24c73cd2d861ad855
Jelte Homminga <jeltehomminga@gmail.com>
2019-02-05 21:49:32 +0100

    button controls show/hide ingredients

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,25 @@
 // Write your Pizza Builder JavaScript in this file.
+
+$(document).ready(function () {
+
+
+    $('.btn-pepperonni').click(function () {
+        $('.pep').toggleClass("inactive active")
+    })
+    $('.btn-mushrooms').click(function () {
+        $('.mushroom').toggleClass("inactive active")
+    })
+    $('.btn-green-peppers').click(function () {
+        $('.green-pepper').toggleClass("inactive active")
+    })
+
+    $('.btn-sauce').click(function () {
+        $('.sauce').toggleClass("sauce-white")
+    })
+
+    $('.btn-crust').click(function () {
+        $('.crust').toggleClass('crust-gluten-free')
+    })
+
+
+});
