f67a25751bc3b8ecc470d7239963dfd0196083c0
Rafael Torres Escobar <torresc.rafael@gmail.com>
2019-02-05 22:18:42 -0600

    Iteration 4

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,30 @@
 // Write your Pizza Builder JavaScript in this file.
+$(".btn-green-peppers").click(function(){
+    $("section.green-pepper").toggleClass("hidden")
+    $(".btn-green-peppers").toggleClass("active")
+    $(".ing-pepper").toggleClass("hidden")
+})
+
+$(".btn-pepperonni").click(function(){
+    $("section.pep").toggleClass("hidden")
+    $(".btn-pepperonni").toggleClass("active")
+    $(".ing-pep").toggleClass("hidden")
+})
+
+$(".btn-mushrooms").click(function(){
+    $("section.mushroom").toggleClass("hidden")
+    $(".btn-mushrooms").toggleClass("active")
+    $(".ing-mushroom").toggleClass("hidden")
+})
+
+$(".btn-crust").click(function(){
+    $("section.crust").toggleClass("crust-gluten-free")
+    $(".btn-crust").toggleClass("active")
+    $(".ing-crust").toggleClass("hidden")
+})
+
+$(".btn-sauce").click(function(){
+    $("section.sauce").toggleClass("sauce-white")
+    $(".btn-sauce").toggleClass("active")
+    $(".ing-sauce").toggleClass("hidden")
+})
