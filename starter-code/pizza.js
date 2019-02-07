72dcf87fa5a04f0305cc26f025b6036a6a737b44
Tomas Freire Knight <tomasfreire@gmail.com>
2019-02-07 00:26:47 -0600

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,57 +1,66 @@
-// $(".pep").toggle()
-// $(".green-pepper").toggle();
-// $(".mushroom").toggle();
 $(".crust").removeClass("crust-gluten-free");
 $(".sauce").removeClass("sauce-white");
-// $(".btn-pepperonni").removeClass("active");
-// $(".btn-green-peppers").removeClass("active");
-// $(".btn-mushrooms").removeClass("active");
 $(".btn-sauce").removeClass("active");
 $(".btn-crust").removeClass("active");
 $(".price li:nth-child(4)").toggle();
 $(".price li:nth-child(5)").toggle();
 
+let totalValue = 13;
+
+const pepperonniPrice = 1;
+const greenPeppersPrice = 1;
+const mushroomsPrice = 1;
+const whiteSaucePrice = 3;
+const glutenFreeCrustPrice = 5;
+
+$("strong").html("$" + totalValue);
 
 
-function finalPrice() {
-  let realValue = 10;
+function startingPrice() {
   if ($(".btn-pepperonni").hasClass("active")) {realValue += 1} else {realValue}
   if ($(".btn-green-peppers").hasClass("active")) {realValue += 1} else {realValue}
   if ($(".btn-mushrooms").hasClass("active")) {realValue += 1} else {realValue}
   if ($(".btn-sauce").hasClass("active")) {realValue += 3} else {realValue}
   if ($(".btn-crust").hasClass("active")) {realValue += 5} else {realValue}
-  $("strong").html("$" + realValue);
 }
-
-finalPrice();
 
 
 $(".btn-pepperonni").on("click", function() {
   $(".pep").toggle();
   $(".btn-pepperonni").toggleClass("active");
   $(".price li:first-child").toggle();
-})
-
-$(".btn-green-peppers").on("click", function() {
-  $(".green-pepper").toggle();
-  $(".btn-green-peppers").toggleClass("active");
-  $(".price li:nth-child(3)").toggle();
+  if ($(".btn-pepperonni").hasClass("active")) {totalValue += pepperonniPrice} else {totalValue -= pepperonniPrice};
+  $("strong").html("$" + totalValue)
 })
 
 $(".btn-mushrooms").on("click", function() {
   $(".mushroom").toggle();
   $(".btn-mushrooms").toggleClass("active");
   $(".price li:nth-child(2)").toggle();
+  if ($(".btn-mushrooms").hasClass("active")) {totalValue += mushroomsPrice} else {totalValue -= mushroomsPrice};
+  $("strong").html("$" + totalValue)
 })
 
-$(".btn-crust").on("click", function() {
-  $(".crust").toggleClass("crust-gluten-free");
-  $(".btn-crust").toggleClass("active");
-  $(".price li:nth-child(5)").toggle();
+$(".btn-green-peppers").on("click", function() {
+  $(".green-pepper").toggle();
+  $(".btn-green-peppers").toggleClass("active");
+  $(".price li:nth-child(3)").toggle();
+  if ($(".btn-green-peppers").hasClass("active")) {totalValue += greenPeppersPrice} else {totalValue -= greenPeppersPrice};
+  $("strong").html("$" + totalValue)
 })
 
 $(".btn-sauce").on("click", function() {
   $(".sauce").toggleClass("sauce-white");
   $(".btn-sauce").toggleClass("active");
   $(".price li:nth-child(4)").toggle();
+  if ($(".btn-sauce").hasClass("active")) {totalValue += whiteSaucePrice} else {totalValue -= whiteSaucePrice};
+  $("strong").html("$" + totalValue)
+})
+
+$(".btn-crust").on("click", function() {
+  $(".crust").toggleClass("crust-gluten-free");
+  $(".btn-crust").toggleClass("active");
+  $(".price li:nth-child(5)").toggle();
+  if ($(".btn-crust").hasClass("active")) {totalValue += glutenFreeCrustPrice} else {totalValue -= glutenFreeCrustPrice};
+  $("strong").html("$" + totalValue)
 })
