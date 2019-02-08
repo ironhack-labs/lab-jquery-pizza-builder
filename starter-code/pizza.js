5e0dcc8080b980076b1a33f731b43f43cc8ede70
rpernicone <roberto.pernicone@gmail.com>
2019-02-07 18:58:11 -0600

    :alien: pizza builder

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,16 @@
 // Write your Pizza Builder JavaScript in this file.
+$(".btn-pepperonni").click(function(){
+    $("* .pep").toggle();
+});
+$(".btn-mushrooms").click(function(){
+    $("* .mushroom").toggle();
+});
+$(".btn-green-peppers").click(function(){
+    $("* .green-pepper").toggle();
+});
+$(".btn-sauce").click(function(){
+    $("* .sauce").toggle();
+});
+$(".btn-crust").click(function(){
+    $(".crust").toggleClass("crust-gluten-free");
+});
