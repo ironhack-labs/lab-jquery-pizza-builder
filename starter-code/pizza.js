52cbfc4a4da4dc5a5ee5c2fe13dc200f3fb4dc92
GomezOrtiz <david.gomez.mail@gmail.com>
2019-02-12 23:18:38 +0100

    Fixes. Done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,7 +1,9 @@
 // Write your Pizza Builder JavaScript in this file.
 $(document).ready(function () {
     $(".crust").removeClass("crust-gluten-free")
+    $("li:contains('white sauce')").css("display","none")
     $(".sauce").removeClass("sauce-white")
+    $("li:contains('gluten-free crust')").css("display","none")
     var totalPrice = 0
 
     $(".btn-pepperonni").on("click", function () {
@@ -9,26 +11,24 @@ $(document).ready(function () {
         if ($(".pep").css("display") === "none") {
             $(".pep").css("display", "block")
             $(".btn-pepperonni").addClass("active")
+            $("li:contains('pepperonni')").css("display","list-item")
         } else {
             $(".btn-pepperonni").removeClass("active")
             $(".pep").css("display", "none")         
-            
-            $("li:contains('pepperonni')").remove()	
-  
+            $("li:contains('pepperonni')").css("display","none")
         }
         getTotal()
     })
-
-
-
 
     $(".btn-mushrooms").on("click", function () {
         if ($(".mushroom").css("display") === "none") {
             $(".mushroom").css("display", "block")
             $(".btn-mushrooms").addClass("active")
+            $("li:contains('mushrooms')").css("display","list-item")
         } else {
             $(".btn-mushrooms").removeClass("active")
             $(".mushroom").css("display", "none")
+            $("li:contains('mushrooms')").css("display","none")	
         }
         getTotal()
     })
@@ -37,61 +37,60 @@ $(document).ready(function () {
         if ($(".green-pepper").css("display") === "none") {
             $(".green-pepper").css("display", "block")
             $(".btn-green-peppers").addClass("active")
+            $("li:contains('green peppers')").css("display","list-item")
         } else {
             $(".btn-green-peppers").removeClass("active")
             $(".green-pepper").css("display", "none")
+            $("li:contains('green peppers')").css("display","none")	
         }
         getTotal()
     })
 
     $(".btn-sauce").on("click", function () {
-        $(".sauce").addClass("sauce-white")
+        if ($(".sauce").hasClass("sauce-white")) {
+            $(".sauce").removeClass("sauce-white")
+            $(".btn-sauce").removeClass("active")
+            $("li:contains('white sauce')").css("display","none")
+        } else {
+            $(".sauce").addClass("sauce-white")
+            $(".btn-sauce").addClass("active")
+            $("li:contains('white sauce')").css("display","list-item")
+        }
         getTotal()
     })
 
     $(".btn-crust").on("click", function () {
-        $(".crust").addClass("crust-gluten-free")
+        // $(".crust").addClass("crust-gluten-free")
+        if ($(".crust").hasClass("crust-gluten-free")) {
+            $(".crust").removeClass("crust-gluten-free")
+            $(".btn-crust").removeClass("active")
+            $("li:contains('gluten-free crust')").css("display","none")
+        } else {
+            $(".crust").addClass("crust-gluten-free")
+            $(".btn-crust").addClass("active")
+            $("li:contains('gluten-free crust')").css("display","list-item")
+        }
         getTotal()
-
     })
 
-function getTotal (){
-    totalPrice = 10
-    if ($(".btn-pepperonni").hasClass("active")){
-        totalPrice += 1
-}
-if ($(".btn-mushrooms").hasClass("active")){
-         totalPrice += 1
-}
-if ($(".btn-green-peppers").hasClass("active")){
-    totalPrice += 1
-}
-if ($(".btn-sauce").hasClass("active")){
-        totalPrice += 3
-    }
-    if ($(".btn-crust").hasClass("active")){
-        totalPrice += 5
-    }
-    $("strong").html(totalPrice)
-}
-
-    function numberToLetter(num) {
-        var stringify = num.toString()
-        var units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-        var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-        var arr = [];
-        var splited = stringify.split("")
-
-
-
-        var firstWord = tens[parseInt(splited[0])]
-        var secondWord = units[parseInt(splited[1])]
-
-        if (num > 10 && num < 20) {
-            return units[num]
-        } else {
-            return firstWord + " " + secondWord
+    function getTotal() {
+        totalPrice = 10
+        if ($(".btn-pepperonni").hasClass("active")) {
+            totalPrice += 1
         }
+        if ($(".btn-mushrooms").hasClass("active")) {
+            totalPrice += 1
+        }
+        if ($(".btn-green-peppers").hasClass("active")) {
+            totalPrice += 1
+        }
+        if ($(".btn-sauce").hasClass("active")) {
+            totalPrice += 3
+        }
+        if ($(".btn-crust").hasClass("active")) {
+            totalPrice += 5
+        }
+        $("strong").html(totalPrice + "$")
     }
 
 })
