352ec9d8e204dcf8ffff75a5556ecccd78d05c42
Fedde <feddetilma@hotmail.com>
2019-02-12 19:59:06 +0100

    first version till crust

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,22 @@
 // Write your Pizza Builder JavaScript in this file.
+$("document").ready(function() {
+
+  // Add or hide ingredients
+  $(".btn-green-peppers").click(function() {
+    $(".green-pepper").toggleClass("hidden");
+  });
+  $(".btn-mushrooms").click(function() {
+    $(".mushroom").toggleClass("hidden");
+  });
+  $(".btn-pepperonni").click(function() {
+    $(".pep").toggleClass("hidden");
+  });
+
+  // change standard sauce and crust
+  $(".crust").removeClass("crust-gluten-free");
+  $(".btn-crust").click(function() {
+    $(".crust").toggleClass("crust-gluten-free");
+  })
+
+});
+
