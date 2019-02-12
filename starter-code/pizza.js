dd328421df84938aca85b8e141c0e55a3c8b85f2
Patricia Delgado <patricia.delgado.morcillo@gmail.com>
2019-02-12 22:11:04 +0100

    first iteration add and remove items when click on buttons

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,17 @@
 // Write your Pizza Builder JavaScript in this file.
+
+//Agregar y quitar 
+// la pimienta de
+// los champiñones 
+//los pimientos verdes
+$( ".btn-pepperonni" ).click(function() {
+  $(".pep").slideToggle();
+});
+$( ".btn-mushrooms" ).click(function() {
+  $(".mushroom").slideToggle();
+});
+$( ".btn-green-peppers" ).click(function() {
+  $(".green-pepper").slideToggle();
+});
+$(document).ready(function() {
+});
