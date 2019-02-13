1ee1bc1aae77c73f83c4e6bfea43a2c4ae6245e6
Guille LA <guillermolucena90@gmail.com>
2019-02-13 13:38:10 +0100

    first iteration done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,34 @@
 // Write your Pizza Builder JavaScript in this file.
+// variables to use
+var buttonCol = $("button")
+var greenPepper = $(".green-pepper")
+var mushroom = $(".mushroom")
+var caps = $(".cap")
+var stems = $(".stem")
+var pepperoni = $(".pep")
+
+
+//function declaration
+function addRemovePepperoni() {
+  $(pepperoni).toggleClass("visible")
+}
+
+function addRemoveGreenPepper() {
+  $(greenPepper).toggleClass("green-pepper")
+}
+
+function addRemoveMushroom() {
+  $(caps).toggleClass("cap")
+  $(stems).toggleClass("stem")
+}
+
+function resetPizza() {
+  $(".selector").removeClass(className)
+}
+
+// JS Functioning after load of site
+$(document).ready(function() {
+
+
+
+});
