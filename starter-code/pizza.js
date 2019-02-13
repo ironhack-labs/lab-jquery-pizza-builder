9a0f069f5bd0b876e9cb20eb17ee26a0eba0b051
Antonio Méndez <antonio.mendez.martin89@gmail.com>
2019-02-13 15:59:09 +0100

    iteration 1 and 2 finished

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,30 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+  $(".btn.btn-pepperonni.active").click(function(){
+    $(".pep").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".btn.btn-mushrooms.active").click(function(){
+    $(".mushroom").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".btn.btn-green-peppers.active").click(function(){
+    $(".green-pepper").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".btn.btn-sauce.active").click(function(){
+    $(".sauce").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".btn.btn-crust.active").click(function(){
+    $(".cheese").toggle();
+  });
+});
