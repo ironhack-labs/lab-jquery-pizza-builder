e9a6e7a699741affe81549aa070dc0f835b37d67
Fedde <feddetilma@hotmail.com>
2019-02-13 16:56:14 +0100

    version 2: js with variable input and less redundant + added pineapple

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,66 +1,51 @@
-// Write your Pizza Builder JavaScript in this file.
+// Write your Pizza Builder JavaScript in this file (after loading content)
 $("document").ready(function () {
-  // Add or hide ingredients. Standard = peppers, mushroom, pepperonni
-  // GREEN-PEPPER
-  // $(".green-pepper").addClass("hidden");
-  // $(".btn-green-peppers").removeClass("active");
-  $(".btn-green-peppers").click(function () {
-    $(".green-pepper").toggleClass("hidden");
-    $(".btn-green-peppers").toggleClass("active");
-    $(".panel.price ul li:contains(green peppers)").toggleClass("hidden");
-  });
-  // MUSHROOM
-  // $(".mushroom").addClass("hidden");
-  // $(".btn-mushrooms").removeClass("active");
-  $(".btn-mushrooms").click(function () {
-    $(".mushroom").toggleClass("hidden");
-    $(".btn-mushrooms").toggleClass("active");
-    $(".panel.price ul li:contains(mushroom)").toggleClass("hidden");
-  });
-  // PEPPERONNI
-  // $(".pep").addClass("hidden");
-  // $(".btn-pepperonni").removeClass("active");
-  $(".btn-pepperonni").click(function () {
-    $(".pep").toggleClass("hidden");
-    $(".btn-pepperonni").toggleClass("active");
-    $(".panel.price ul li:contains(pepperonni)").toggleClass("hidden");
-  });
-  
-  // change standard crust + toggle
-  $(".crust").removeClass("crust-gluten-free");
-  $(".btn-crust").removeClass("active");
-  $(".panel.price ul li:contains(crust)").addClass("hidden");
-  $(".btn-crust").click(function () {
-    $(".crust").toggleClass("crust-gluten-free");
-    $(".btn-crust").toggleClass("active");
-    $(".panel.price ul li:contains(crust)").toggleClass("hidden");
-  });
-  // change standard sauce + toggle
-  $(".sauce").removeClass("sauce-white");
-  $(".btn-sauce").removeClass("active");
-  $(".panel.price ul li:contains(sauce)").addClass("hidden");
-  $(".btn-sauce").click(function () {
-    $(".sauce").toggleClass("sauce-white");
-    $(".btn-sauce").toggleClass("active");
-    $(".panel.price ul li:contains(sauce)").toggleClass("hidden");
+
+  // initial status changes (no white sauce or gluten free crust)
+  changeStatus($(".btn-crust"));
+  changeStatus($(".btn-sauce"));
+  calcPrice();
+
+  // clicking a button
+  $(".panel.controls .btn").click(function () {
+    changeStatus(this);
   });
 
-  // if one of the add/remove buttons is clicked, recalculate the price
-  $(".btn").click(function() {
+  //---------------//
+  // CHANGE STATUS //
+  //---------------//
+  
+  // input = the button clicked element
+  function changeStatus(input) {
+    var product = $(input).attr("id"); // e.g: mushroom
+    var productClass = "." + product // e.g: .mushroom
+    // toggle button active / inactive (this)
+    $(input).toggleClass("active");
+    // toggle pizza visibility (.product). Crust and sauce are custom
+    switch (productClass) {
+      case ".sauce": $(productClass).toggleClass("sauce-white"); break;
+      case ".crust": $(productClass).toggleClass("crust-gluten-free"); break;
+      default: $(productClass).toggleClass("hidden");
+    };
+    // toggle price (contains product)
+    $(".panel.price ul li:contains(" + product + ")").toggleClass("hidden");
+    // update price
     calcPrice();
-  });
+  }
+  
+  //---------------//
+  // CALC PRICE    //
+  //---------------//
 
   // calculate the price of all non-hidden price lines
   function calcPrice() {
     var total = 10; // basic cheese pizza
     var list = $(".panel.price ul li").not(".hidden").toArray();
     list.map(item => {
-      total += parseInt(item.innerHTML.substr(1,3)); // supports 0-999 $ items
+      total += parseInt(item.innerHTML.substr(1, 3)); // supports 0-999 $ items
     });
     $(".panel.price strong").html("$" + total);
   };
 
-  // initialize price once on load
-  calcPrice();
 });
 
