86abb25537254f87aa5fec07f631e87012089461
Nouri T <zorisse@gmail.com>
2019-02-13 21:49:22 +0100

    finished

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,5 +1,15 @@
 // Write your Pizza Builder JavaScript in this file.
 // itération 1 selectionner les éléments et les faire apparaitre ou disparaitre
+{
+  /* <li class="pricePep">$1 pepperonni</li>
+        <li class="priceMush">$1 mushrooms</li>
+        <li class="priceGreen">$1 green peppers</li>
+        <li class="priceWhite">$3 white sauce</li>
+        <li class="priceGlut">$5 gluten-free crust</li> */
+}
+
+var priceList = $("#priceList");
+var price = 13;
 
 var greenpepper = $(".green-pepper");
 var btngreenpeppers = $(".btn-green-peppers ");
@@ -21,28 +31,76 @@ var btnmushrooms = $(".btn-mushrooms");
 // $(".btn-crust").click(() => $(".crust").toggleClass("crust-gluten-free"));
 
 // itération 3: remove or add active if the element is therer or not
+$("strong").html(price);
 
 btngreenpeppers.click(function() {
   greenpepper.toggle();
   $(this).toggleClass("active");
+  if ($(this).hasClass("active")) {
+    $(".priceGreen").show();
+    price += 1;
+  } else if (!$(this).hasClass("active")) {
+    $(".priceGreen").hide();
+    price -= 1;
+  }
+  $("strong").html(price);
 });
 
 btnpepperonni.click(function() {
   pepperonni.toggle();
   $(this).toggleClass("active");
+  if ($(this).hasClass("active")) {
+    $(".pricePep").show();
+    price += 1;
+  } else if (!$(this).hasClass("active")) {
+    $(".pricePep").hide();
+    price -= 1;
+  }
+  $("strong").html(price);
 });
 btnmushrooms.click(function() {
   mushroom.toggle();
   $(this).toggleClass("active");
+  if ($(this).hasClass("active")) {
+    $(".priceMush").show();
+    price += 1;
+  } else if (!$(this).hasClass("active")) {
+    $(".priceMush").hide();
+    price -= 1;
+  }
+  $("strong").html(price);
 });
+
+// on cache les li
+$(".priceWhite").hide();
 
 $(".btn-sauce").click(function() {
   $(".sauce").toggleClass("sauce-white");
-  $("this").toggleClass("active");
+  $(this).toggleClass("active");
+  if ($(this).hasClass("active")) {
+    $(".priceWhite").show();
+    price += 3;
+  } else if (!$(this).hasClass("active")) {
+    $(".priceWhite").hide();
+    price -= 3;
+  }
+  $("strong").html(price);
 });
+
+// on cache les li
+$(".priceGlut").hide();
+
 $(".btn-crust").click(function() {
   $(".crust").toggleClass("crust-gluten-free");
   $(this).toggleClass("active");
+  if ($(this).hasClass("active")) {
+    $(".priceGlut").show();
+    price += 5;
+  } else if (!$(this).hasClass("active")) {
+    $(".priceGlut").hide();
+    price -= 5;
+  }
+  $("strong").html(price);
 });
 
-// 4
+// 4 PRIX
