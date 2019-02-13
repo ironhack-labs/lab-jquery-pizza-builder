0725f5d9712b4df315a642723535d268dad533a4
Guille LA <guillermolucena90@gmail.com>
2019-02-13 14:11:09 +0100

    2nd and 3rd iteration done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,34 +1,81 @@
 // Write your Pizza Builder JavaScript in this file.
 // variables to use
+
+//buttons
 var buttonCol = $("button")
+var butPep = $(".btn-pepperonni")
+var butGP = $(".btn-green-peppers")
+var butMush = $(".btn-mushrooms")
+var butSauce = $(".btn-sauce")
+var butGluten = $(".btn-crust")
+//ingredients
 var greenPepper = $(".green-pepper")
 var mushroom = $(".mushroom")
 var caps = $(".cap")
 var stems = $(".stem")
 var pepperoni = $(".pep")
+var whiteSauce = $(".sauce-white")
+var glutenFree = $(".crust-gluten-free")
+
 
 
 //function declaration
 function addRemovePepperoni() {
-  $(pepperoni).toggleClass("visible")
+  butPep.toggleClass("active");
+  pepperoni.toggleClass("visible");
 }
 
 function addRemoveGreenPepper() {
-  $(greenPepper).toggleClass("green-pepper")
+  butGP.toggleClass("active");
+  greenPepper.toggleClass("green-pepper");
 }
 
 function addRemoveMushroom() {
-  $(caps).toggleClass("cap")
-  $(stems).toggleClass("stem")
+  butMush.toggleClass("active");
+  caps.toggleClass("cap");
+  stems.toggleClass("stem");
+}
+
+function addRemoveGlutenFree() {
+  butGluten.toggleClass("active");
+  glutenFree.toggleClass("crust-gluten-free");
+}
+
+function addRemoveWhiteSauce() {
+  butSauce.toggleClass("active");
+  whiteSauce.toggleClass("sauce-white");
 }
 
 function resetPizza() {
-  $(".selector").removeClass(className)
+  addRemovePepperoni();
+  addRemoveGreenPepper();
+  addRemoveMushroom();
+  addRemoveGlutenFree();
+  addRemoveWhiteSauce();
 }
 
 // JS Functioning after load of site
 $(document).ready(function() {
+  resetPizza();
 
+  butPep.click(function () {
+    addRemovePepperoni();
+  });
 
+  butGP.click(function () {
+    addRemoveGreenPepper();
+  });
+
+  butMush.click(function () {
+    addRemoveMushroom();
+  });
+
+  butSauce.click(function () {
+    addRemoveWhiteSauce();
+  });
+
+  butGluten.click(function () {
+    addRemoveGlutenFree();
+  });
 
 });
