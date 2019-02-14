f93496afe2a67693660933305200a585b10baedf
NuriaTired <nuriatired@gmail.com>
2019-02-14 17:09:48 +0100

    Primera Iteracion

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,22 @@
 // Write your Pizza Builder JavaScript in this file.
+// Quitar Champiñones
+$(document).ready(function (){
+    $('.btn-mushrooms').on("click", function () {
+    $('.cap').toggle();
+    $('.stem').toggle();
+    });
+});
+
+// Quitar Champiñones
+$(document).ready(function () {
+    $('.btn-pepperonni').on("click", function () {
+        $('.pep').toggle();
+    });
+});
+
+// Quitar pimientos
+$(document).ready(function () {
+    $('.btn-green-peppers').on("click", function () {
+        $('.green-pepper').toggle();
+    });
+});
