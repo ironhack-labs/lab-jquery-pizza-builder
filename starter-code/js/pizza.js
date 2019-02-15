e0b7db10ce3d7f2c1f6fb363cf559ac1ac25dba0
Eva Francés <evafrances@MacBook-Pro-de-Eva.local>
2019-02-15 14:27:48 +0100

    terminado

--- starter-code/js/pizza.js
+++ starter-code/js/pizza.js
@@ -1,14 +1,24 @@
 // Write your Pizza Builder JavaScript in this file.
 $(document).ready(function() {
+
+
+    var sumPrice = document.getElementsByClassName('ingredients');
+    var totalPrice = document.querySelector('.totalPrice');
+    var calculatePrice = 0;
+    
     //elegimos ingredientes, ingrediente-botón activo ||no ingrediente-no activo
+    $(".btn").removeClass("active")
+
     $(".btn-pepperonni").click(function(event){
         if ($(".pep").hasClass("pep-index")) {
             $(".pep").removeClass("pep-index") && $(".price li:nth-child(1)").removeClass("visible");
             $(".btn-pepperonni").removeClass("active")
+            totalPrice.innerHTML = calculatePrice -= 1;
            
         } else {
             $(".pep").addClass("pep-index") && $(".price li:nth-child(1)").addClass("visible");
             $(".btn-pepperonni").addClass("active")
+            totalPrice.innerHTML = calculatePrice += 1;
         }
         
     })
@@ -17,9 +27,12 @@ $(document).ready(function() {
         if ($(".mushroom").hasClass("mushroom-index")) {
             $(".mushroom").removeClass("mushroom-index") && $(".price li:nth-child(2)").removeClass("visible");
             $(".btn-mushrooms").removeClass("active")
+            totalPrice.innerHTML = calculatePrice -= 1;
+            
         } else {
             $(".mushroom").addClass("mushroom-index") && $(".price li:nth-child(2)").addClass("visible");
             $(".btn-mushrooms").addClass("active")
+            totalPrice.innerHTML = calculatePrice += 1;
         }
     })
 
@@ -27,9 +40,12 @@ $(document).ready(function() {
         if ($(".green-pepper").hasClass("green-pepper-index")) {
             $(".green-pepper").removeClass("green-pepper-index") && $(".price li:nth-child(3)").removeClass("visible");
             $(".btn-green-peppers").removeClass("active")
+            totalPrice.innerHTML = calculatePrice -= 1;
+            
         } else {
             $(".green-pepper").addClass("green-pepper-index") && $(".price li:nth-child(3)").addClass("visible");
             $(".btn-green-peppers").addClass("active")
+            totalPrice.innerHTML = calculatePrice += 1;
         }
     }) 
 
@@ -37,9 +53,12 @@ $(document).ready(function() {
         if ($(".crust").hasClass("crust-gluten-free")) {
             $(".crust").removeClass("crust-gluten-free") && $(".price li:nth-child(4)").removeClass("visible");
             $(".btn-crust").removeClass("active")
+            totalPrice.innerHTML = calculatePrice -= 5;
+            
         } else {
             $(".crust").addClass("crust-gluten-free") && $(".price li:nth-child(4)").addClass("visible");
             $(".btn-crust").addClass("active")
+            totalPrice.innerHTML = calculatePrice += 5;
         }
     })
 
@@ -47,22 +66,14 @@ $(document).ready(function() {
         if ($(".sauce").hasClass("sauce-white")) {
             $(".sauce").removeClass("sauce-white") && $(".price li:nth-child(5)").removeClass("visible");
             $(".btn-sauce").removeClass("active")
+            totalPrice.innerHTML = calculatePrice -= 3;
+            
         } else {
             $(".sauce").addClass("sauce-white") && $(".price li:nth-child(5)").addClass("visible");
             $(".btn-sauce").addClass("active")
+            totalPrice.innerHTML = calculatePrice += 3;
         }
     })
 
-
-    // Total price
-    var sumPrice = document.getElementsByClassName('ingredients');
-    var whatPrice = [];
-    var calculatePrice = 0;
-    for(i=0; i<sumPrice.length; i++){
-        whatPrice = sumPrice[i].innerHTML;
-        whatPrice = whatPrice.substr(1, 1);
-        calculatePrice += whatPrice;
-        console.log(calculatePrice);
-    }
 });
 
