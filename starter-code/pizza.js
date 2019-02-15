55d86a80705883660e7cf4806da7827027f77bf8
danidimarti <dani.dimarti@gmail.com>
2019-02-15 22:58:30 +0100

    final

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -8,18 +8,18 @@ $("document").ready(function () {
 
     //setting default price on load
     $(".total").html("13");
-    
-    
+
+
 
 
     // "if the ingredient is turned on, its button should have active."
     //removing active class so it will appear again on click. 
 
-    $(".btn-green-peppers").removeClass("active");
+    // $(".btn-green-peppers").removeClass("active");
 
-    $(".btn-mushrooms").removeClass("active");
+    // $(".btn-mushrooms").removeClass("active");
 
-    $(".btn-pepperonni").removeClass("active");
+    // $(".btn-pepperonni").removeClass("active");
 
     $(".btn-sauce").removeClass("active");
 
@@ -37,6 +37,17 @@ $("document").ready(function () {
         $(".green-pepper").toggleClass("hidden");
         $(".btn-green-peppers").toggleClass("active");
         $("#li-3").toggleClass("hidden");
+        var className = $(this).attr('class')
+
+        var total = $(".total").html();
+
+        if (className === "btn btn-green-peppers active") {
+            $(".total").html(parseFloat(total) + 1);
+
+        } else {
+            $(".total").html(parseFloat(total) - 1);
+        }
+
     });
 
 
@@ -44,19 +55,52 @@ $("document").ready(function () {
         $(".mushroom").toggleClass("hidden");
         $(".btn-mushrooms").toggleClass("active");
         $("#li-2").toggleClass("hidden");
+        var className = $(this).attr('class')
+
+        var total = $(".total").html();
+
+        if (className === "btn btn-mushrooms active") {
+            $(".total").html(parseFloat(total) + 1);
+
+        } else {
+            $(".total").html(parseFloat(total) - 1);
+        }
+
     });
 
     $(".btn-pepperonni").click(function () {
         $(".pep").toggleClass("hidden");
         $(".btn-pepperonni").toggleClass("active");
         $("#li-1").toggleClass("hidden");
+
+        var className = $(this).attr('class')
+
+        var total = $(".total").html();
+
+        if (className === "btn btn-pepperonni active") {
+            $(".total").html(parseFloat(total) + 1);
+
+        } else {
+            $(".total").html(parseFloat(total) - 1);
+        }
     });
 
     $(".btn-sauce").click(function () {
         $(".sauce").toggleClass("sauce-white");
         $(".btn-sauce").toggleClass("active");
         $("#li-4").toggleClass("hidden");
+        //var className stores all class values of sauce 
+        var className = $(this).attr('class')
 
+        //var total contains the initial total price before click
+        var total = $(".total").html();
+
+        if (className === "btn btn-sauce active") {
+            $(".total").html(parseFloat(total) + 3);
+
+        } else {
+            $(".total").html(parseFloat(total) - 3);
+        }
 
     });
 
@@ -64,15 +108,20 @@ $("document").ready(function () {
         $(".crust").toggleClass("crust-gluten-free");
         $(".btn-crust").toggleClass("active");
         $("#li-5").toggleClass("hidden");
-        $(function(){
-            var total = $(".total").parseFloat("14");
-            return total;
-        })
-            
 
-        
-        
-    
+        //var className contains the complete class value 
+        var className = $(this).attr('class')
+
+        //var total contains the initial total price before click
+        var total = $(".total").html();
+
+        //In case the button has active in his class (which means pressed), i need to add +5 to the price 
+        if (className === "btn btn-crust active") {
+            $(".total").html(parseFloat(total) + 5);
+            //In case the button crust does not have active in the class (which means unpressed) i need to extract -5 from the price.
+        } else {
+            $(".total").html(parseFloat(total) - 5);
+        }
 
 
     });
@@ -80,15 +129,3 @@ $("document").ready(function () {
 });
 
 
-
-// var currentValue = $("strong").slice(1);
-// var productValue = $("#li-5").slice(1, 3);
-// var subtotal = currentValue + productValue;
-// $(strong).html("$" + subtotal)
-
-// //make pizza look empty on load - cancelled
-// $(".green-pepper").addClass("hidden");
-
-// $(".mushroom").addClass("hidden");
-
-// $(".pep").addClass("hidden");
