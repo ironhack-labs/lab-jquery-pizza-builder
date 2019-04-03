18d54910e14157755b55740fe326c0a014cbc713
pipepico <fpr29.10@gmail.com>
2019-04-02 23:32:27 -0600

    iteration 4 not finished

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -3,38 +3,75 @@
 
 // Buttons
 let btnPepperonni   = document.getElementsByClassName("btn-pepperonni")
-let btnMushrooms  = document.getElementsByClassName("btn-mushrooms")
-let btnPeppers     = document.getElementsByClassName("btn-peppers")
-let btnSauce      = document.getElementsByClassName("btn-sauce")
-let btnCrust      = document.getElementsByClassName("btn-crust")
+let btnMushrooms    = document.getElementsByClassName("btn-mushrooms")
+let btnPeppers      = document.getElementsByClassName("btn-green-peppers")
+let btnSauce        = document.getElementsByClassName("btn-sauce")
+let btnCrust        = document.getElementsByClassName("btn-crust")
 
 // Ingredients
-let ingPeperoni = document.getElementsByClassName("pep")
-let ingMushroom = document.getElementsByClassName("mushroom")
-let ingPeppers = document.getElementsByClassName("green-pepper")
-let ingSauce = document.getElementsByClassName("sauce")
-let ingCrust = document.getElementsByClassName("crust")
+let ingPeperoni     = document.getElementsByClassName("pep")
+let ingMushroom     = document.getElementsByClassName("mushroom")
+let ingPeppers      = document.getElementsByClassName("green-pepper")
+let ingSauce        = document.getElementsByClassName("sauce ")
+let ingCrust        = document.getElementsByClassName("crust")
 
+// Prices
+let prices = document.getElementsByTagName('aside')
 
-
-const selected = (arr) =>{
-  return arr
+let pricesAll = (arr) => {
+  let price = [...arr]
+  
 }
 
-const toggle = (arr) =>{
-  console.log(arr);
-  for (let index = 0; index < arr.length; index++) {
-    const element = arr[index];
-    element.style.display = 'none'
+pricesAll(prices)
+
+const removingClass = (arr,removeClass) => {
+  if (arr[0].classList.contains(removeClass)) {
+    arr[0].classList.remove(removeClass)
+  }else{
+    arr[0].classList.add(removeClass)
   }
 }
-// Adding Event
+
+removingClass(ingCrust,'crust-gluten-free')
+removingClass(ingSauce,'sauce-white')
+removingClass(btnPepperonni,'active')
+removingClass(btnMushrooms,'active')
+removingClass(btnPeppers,'active')
+removingClass(btnSauce,'active')
+removingClass(btnCrust,'active')
+
+const toggleds = arr =>{
+  let array = [...arr]
+  array.forEach( (e) => {
+    
+    if(e.style.opacity == 1 || e.style.opacity == ''){
+      e.style.opacity = 0
+    }else{
+      e.style.opacity = 1
+    }
+  })
+}
+
 
 // Events click
-btnPepperonni.onclick = ()=>{
-  toggle(ingPeperoni)
+btnPepperonni[0].onclick = () =>{
+  toggleds(ingPeperoni)
+  removingClass(btnPepperonni,'active')
 }
-toggle(ingMushroom)
-toggle(ingPeppers)
-toggle(ingSauce)
-// toggle(ingCrust)
+btnMushrooms[0].onclick = () =>{
+  toggleds(ingMushroom)
+  removingClass(btnMushrooms,'active')
+}
+btnPeppers[0].onclick = () => {
+  toggleds(ingPeppers)
+  removingClass(btnPeppers,'active')
+}
+btnSauce[0].onclick = () => {
+  removingClass(ingSauce,'sauce-white')
+  removingClass(btnSauce,'active')
+}
+btnCrust[0].onclick = () => {
+  removingClass(ingCrust,'crust-gluten-free')
+  removingClass(btnCrust,'active')
+}
