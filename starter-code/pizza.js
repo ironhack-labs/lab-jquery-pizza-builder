0a0cafde47ddae7ce9c92ccfa7cbcd96d59660ff
geguiluz <memo.eguiluz@gmail.com>
2019-05-16 21:57:30 -0500

    Ya apagan y prenden todos los ingredientes rebanados

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,25 @@
 // Write your Pizza Builder JavaScript in this file.
+new Vue({
+  el: '#app',
+  data: {
+    mushroomsVisible: true,
+    greenPeppersVisible: true,
+    pepperonniVisible: true,
+    whiteSauceVisible: true,
+  },
+  methods: {
+    toggleMushrooms: function(){
+      this.mushroomsVisible = !this.mushroomsVisible;
+    },
+    toggleGreenPeppers: function(){
+      this.greenPeppersVisible = !this.greenPeppersVisible;
+    },
+    togglePepperonni: function(){
+      this.pepperonniVisible = !this.pepperonniVisible;
+    },
+    toggleWhiteSauce: function(){
+      this.whiteSauceVisible = !this.whiteSauceVisible;
+    }
+
+  }
+});
