1792495fbc2e955cfbd90044f173f16828eb170e
geguiluz <memo.eguiluz@gmail.com>
2019-05-17 02:41:54 -0500

    Todo el lab funcionando al 100%

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -2,24 +2,61 @@
 new Vue({
   el: '#app',
   data: {
-    mushroomsVisible: true,
-    greenPeppersVisible: true,
-    pepperonniVisible: true,
-    whiteSauceVisible: true,
+    pizza: {
+      ingredients:{
+        hasMushrooms: true,
+        hasGreenPeppers: true,
+        hasPepperonni: true,
+        hasWhiteSauce: true,
+        hasGlutenFreeCrust: true
+      },
+      totalPrice: 21
+    },
+    priceList:{
+        basePizza: 10,
+        mushroomsPrice: 1,
+        greenPeppersPrice: 1,
+        pepperonniPrice: 1,
+        whiteSaucePrice: 3,
+        gluttenFreeCrustPrice: 5
+    }
   },
   methods: {
     toggleMushrooms: function(){
-      this.mushroomsVisible = !this.mushroomsVisible;
+      this.pizza.ingredients.hasMushrooms = !this.pizza.ingredients.hasMushrooms;
+      // console.log('hasMushrooms',this.pizza.ingredients.hasMushrooms);
+      this.calculateTotal();
     },
     toggleGreenPeppers: function(){
-      this.greenPeppersVisible = !this.greenPeppersVisible;
+      this.pizza.ingredients.hasGreenPeppers = !this.pizza.ingredients.hasGreenPeppers;
+      // console.log('hasGreenPeppers',this.pizza.ingredients.hasGreenPeppers);
+      this.calculateTotal();
     },
     togglePepperonni: function(){
-      this.pepperonniVisible = !this.pepperonniVisible;
+      this.pizza.ingredients.hasPepperonni = !this.pizza.ingredients.hasPepperonni;
+      // console.log('hasPepperonni',this.pizza.ingredients.hasPepperonni);
+      this.calculateTotal();
     },
     toggleWhiteSauce: function(){
-      this.whiteSauceVisible = !this.whiteSauceVisible;
+      this.pizza.ingredients.hasWhiteSauce = !this.pizza.ingredients.hasWhiteSauce;
+      // console.log('hasWhiteSauce',this.pizza.ingredients.hasWhiteSauce);
+      this.calculateTotal();
+    },
+    toggleGluttenFree: function(){
+      this.pizza.ingredients.hasGlutenFreeCrust = !this.pizza.ingredients.hasGlutenFreeCrust;
+      // console.log('hasGlutenFreeCrust',this.pizza.ingredients.hasGlutenFreeCrust);
+      this.calculateTotal();
+    },
+    calculateTotal: function(){
+      var subtotal = 0;
+      this.pizza.ingredients.hasMushrooms ? subtotal += this.priceList.mushroomsPrice : 0;
+      this.pizza.ingredients.hasPepperonni ? subtotal += this.priceList.pepperonniPrice : 0;
+      this.pizza.ingredients.hasGreenPeppers ? subtotal += this.priceList.greenPeppersPrice : 0;
+      this.pizza.ingredients.hasWhiteSauce ? subtotal += this.priceList.whiteSaucePrice : 0;
+      this.pizza.ingredients.hasGlutenFreeCrust ? subtotal += this.priceList.gluttenFreeCrustPrice : 0;
+      // console.log("subtotal", subtotal);
+      this.pizza.totalPrice = subtotal + this.priceList.basePizza;
+      console.log('Total Price:',this.pizza.totalPrice);
     }
-
   }
 });
