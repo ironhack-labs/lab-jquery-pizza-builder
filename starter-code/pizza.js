7d95a91a363c670e7501c7998e57749a4003a81c
LuisOrihuela <33643227+LuisOrihuela@users.noreply.github.com>
2019-05-17 20:22:38 -0500

    Update pizza.js

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,62 @@
 // Write your Pizza Builder JavaScript in this file.
+var pizza = new Vue({
+  el: "#app",
+  data:{    
+    queseavisiblePepperoni: false,
+    queseavisibleMushroom: false,
+    queseavisiblePepper: false,
+    whiteSauce: false,
+    glutenCrust: false,
+    currentTotal: 10    
+  },
+  methods:{
+    escondePepperoni: function(){
+      this.queseavisiblePepperoni = !this.queseavisiblePepperoni;
+      if(this.queseavisiblePepperoni){
+        this.currentTotal++;
+      }else{
+        this.currentTotal--;
+      }
+      
+    },
+
+    escondePepper: function(){
+      this.queseavisiblePepper = !this.queseavisiblePepper;
+      if(this.queseavisiblePepper){
+        this.currentTotal++;
+      }else{
+        this.currentTotal--;
+      }
+    },
+
+    escondeMushroom: function(){
+      this.queseavisibleMushroom = !this.queseavisibleMushroom;
+      if(this.queseavisibleMushroom){
+        this.currentTotal++;
+      }else{
+        this.currentTotal--;
+      }
+    },
+
+    cambiaSauce: function(){
+      this.whiteSauce = !this.whiteSauce;
+      if(this.whiteSauce){
+        this.currentTotal+=3;
+      }else{
+        this.currentTotal-=3;
+      }
+    },
+
+    cambiaCrust: function(){
+      this.glutenCrust = !this.glutenCrust;
+      if(this.glutenCrust){
+        this.currentTotal+=5;
+      }else{
+        this.currentTotal-=5;
+      }
+    }
+
+    
+  }
+
+});
