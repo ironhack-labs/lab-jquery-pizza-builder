4e59c64ca0b24b5c34798c4b75dc9f86290f0798
Daniel Orio <danielislas3@live.com>
2019-06-11 13:48:05 -0500

    Botones de salsa y gluten terminados

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -4,11 +4,17 @@ const pizza= document.querySelector('#pizza')
 const peperoni= document.querySelectorAll('.pep')
 const champ= document.querySelectorAll('.mushroom')
 const chile= document.querySelectorAll('.green-pepper')
+const gluten= document.querySelector('.crust-gluten-free')
+const salsa= document.querySelector('.sauce-white')
+
 
 //botones
 const btnPeperoni=document.querySelector('.btn-pepperonni')
 const btnChamp=document.querySelector('.btn-mushrooms')
 const btnChile=document.querySelector('.btn-green-peppers')
+//botones de salsa y glueten
+const btnGluten=document.querySelector('.btn-crust')
+const btnSalsa=document.querySelector('.btn-sauce')
 
 //funcion para desaparecer peperonis
 btnPeperoni.onclick = e =>{
@@ -41,6 +47,24 @@ btnChile.onclick = e =>{
     }
   })
 }
+//funcion para desaparecer la Salsa
+btnSalsa.onclick = e =>{
+    if( salsa.classList.item(1) == "sauce-white" ){
+    salsa.classList.remove("sauce-white");
+    } else {       
+      salsa.classList.add("sauce-white"); 
+    }
+}
+//funcion para desaparecer Gluten
+btnGluten.onclick = e =>{
+  if( gluten.classList.item(1) == "crust-gluten-free" ){
+    gluten.classList.remove("crust-gluten-free");
+  } else {       
+    gluten.classList.add("crust-gluten-free"); 
+  }
+}
+
+
 
 
 
