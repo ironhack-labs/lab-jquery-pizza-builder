86655d558ac8ebdd5f508159e018ce4e83f9a320
Daniel Orio <danielislas3@live.com>
2019-06-11 13:26:15 -0500

    Botones de ingredientes terminados

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -3,28 +3,47 @@
 const pizza= document.querySelector('#pizza')
 const peperoni= document.querySelectorAll('.pep')
 const champ= document.querySelectorAll('.mushroom')
-const chile= document.querySelectorAll('.green-papper')
+const chile= document.querySelectorAll('.green-pepper')
 
 //botones
 const btnPeperoni=document.querySelector('.btn-pepperonni')
 const btnChamp=document.querySelector('.btn-mushrooms')
-const btnChile=document.querySelector('.green-peppers')
+const btnChile=document.querySelector('.btn-green-peppers')
 
+//funcion para desaparecer peperonis
 btnPeperoni.onclick = e =>{
-  
     peperoni.forEach(e=>{
       if( e.style.visibility == "visible" ){
         e.style.visibility = "hidden"  
-      } else {
-       
+      } else {       
           e.style.visibility = "visible"  
-          
       }
     })
-      // peperoni.currentTarget.style.visibility = "hidden";
-  // btnPeperoni.value=true
   }
- 
+
+//funcion para desaparecer champiñones
+btnChamp.onclick = e =>{
+  champ.forEach(e=>{
+    if( e.style.visibility == "visible" ){
+      e.style.visibility = "hidden"  
+    } else {       
+        e.style.visibility = "visible"  
+    }
+  })
+}
+//funcion para desaparecer chiles
+btnChile.onclick = e =>{
+  chile.forEach(e=>{
+    if( e.style.visibility == "visible" ){
+      e.style.visibility = "hidden"  
+    } else {       
+        e.style.visibility = "visible"  
+    }
+  })
+}
+
+
+
 
 
 
