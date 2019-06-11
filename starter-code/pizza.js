ba4b279fe35f00fdacc420db73356d6825c56b79
Daniel Orio <danielislas3@live.com>
2019-06-11 13:13:46 -0500

    Boton de peperoni funciona :D

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,30 @@
 // Write your Pizza Builder JavaScript in this file.
+//sujetamos llo que vamos a ocupar
+const pizza= document.querySelector('#pizza')
+const peperoni= document.querySelectorAll('.pep')
+const champ= document.querySelectorAll('.mushroom')
+const chile= document.querySelectorAll('.green-papper')
+
+//botones
+const btnPeperoni=document.querySelector('.btn-pepperonni')
+const btnChamp=document.querySelector('.btn-mushrooms')
+const btnChile=document.querySelector('.green-peppers')
+
+btnPeperoni.onclick = e =>{
+  
+    peperoni.forEach(e=>{
+      if( e.style.visibility == "visible" ){
+        e.style.visibility = "hidden"  
+      } else {
+       
+          e.style.visibility = "visible"  
+          
+      }
+    })
+      // peperoni.currentTarget.style.visibility = "hidden";
+  // btnPeperoni.value=true
+  }
+ 
+
+
+
