8cf16c582b9d802face685a0e0f6ac7758b93edc
horaleon1 <horaleon1@gmail.com>
2019-07-01 21:28:34 -0500

    refactoring, trash and format done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,61 +1,43 @@
-// Write your Pizza Builder JavaScript in this file.
-
-
-
-
-
-
-// Write your Pizza Builder JavaScript in this file.
 var app1 = new Vue({
-  el: '#app1',
+  el: "#app1",
   data: {
-    pepperoni:false,
-    mushrooms:false,
-    greenPeppers:false,
-    whiteSauce:false,
-    glutenFree:false,
-    price:10,
+    pepperoni: false,
+    mushrooms: false,
+    greenPeppers: false,
+    whiteSauce: false,
+    glutenFree: false,
+    price: 10
   },
   methods: {
-    showPepperonni:function(){
-      this.pepperoni = !this.pepperoni
-      if (this.pepperoni){
-        this.price++;
-      }else{
-        this.price--;
-      }
+    showPepperonni: function() {
+      this.pepperoni = !this.pepperoni;
+
+      if (this.pepperoni) this.price++;
+      else this.price--;
     },
-    showMushrooms:function(){
-      this.mushrooms = !this.mushrooms
-      if (this.mushrooms){
-        this.price++;
-      }else{
-        this.price--;
-      }
+    showMushrooms: function() {
+      this.mushrooms = !this.mushrooms;
+
+      if (this.mushrooms) this.price++;
+      else this.price--;
     },
-    showGreenPeppers:function(){
-      this.greenPeppers = !this.greenPeppers
-      if (this.greenPeppers){
-        this.price++;
-      }else{
-        this.price--;
-      }
+    showGreenPeppers: function() {
+      this.greenPeppers = !this.greenPeppers;
+
+      if (this.greenPeppers) this.price++;
+      else this.price--;
     },
-    showWhiteSauce:function(){
-      this.whiteSauce = !this.whiteSauce
-      if (this.whiteSauce){
-        this.price++;
-      }else{
-        this.price--;
-      }
+    showWhiteSauce: function() {
+      this.whiteSauce = !this.whiteSauce;
+
+      if (this.whiteSauce) this.price++;
+      else this.price--;
     },
-    showGlutenFree:function(){
-      this.glutenFree = !this.glutenFree
-      if (this.glutenFree){
-        this.price++;
-      }else{
-        this.price--;
-      }
-    },
+    showGlutenFree: function() {
+      this.glutenFree = !this.glutenFree;
+
+      if (this.glutenFree) this.price++;
+      else this.price--;
+    }
   }
-})
+});
