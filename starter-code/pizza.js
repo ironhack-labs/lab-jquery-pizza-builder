787a5b2d5c47f581320353ded0837c9864d848b4
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2019-07-03 19:10:05 +0200

    added my comments

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,20 +1,13 @@
 $(document).ready(function(){
   
-  // MY ELEMENTS
+  // SET UP MY DEFAULT PIZZA
+  // default sauce and crust
+  // hidden prices of ingredients we don't want
 
-  var buttonPepperonnis = $(".btn-pepperonni");
-  var buttonMushrooms = $(".btn-mushrooms");
-  var buttonGreenpeppers = $(".btn-green-peppers");
   var buttonSauce = $(".btn-sauce");
   var buttonCrust = $(".btn-crust");
-
-  var pepperonnis = $(".pep");
-  var mushrooms = $(".mushroom");
-  var greenpeppers = $(".green-pepper");
   var sauce = $(".sauce");
   var crust = $(".crust");
-
-  // SET UP MY DEFAULT PIZZA
 
   function defaultPizza() {
     sauce.removeClass("sauce-white");
@@ -27,7 +20,15 @@ $(document).ready(function(){
 
   defaultPizza();
 
-  // DISPLAY INGREDIENTS DEPENDING ON THE PRESSED BUTTONS
+  // DISPLAY INGREDIENTS (MUSHROOMS/PEPERONNIS/GREENPEPPERS) DEPENDING ON THE PRESSED BUTTONS
+  // when we press a given button it adds/removes the ingredients we target
+
+  var buttonPepperonnis = $(".btn-pepperonni");
+  var buttonMushrooms = $(".btn-mushrooms");
+  var buttonGreenpeppers = $(".btn-green-peppers");
+  var pepperonnis = $(".pep");
+  var mushrooms = $(".mushroom");
+  var greenpeppers = $(".green-pepper");
 
   function toggleIngredients(button,ingredients) {
     button.click(function(){
@@ -40,7 +41,24 @@ $(document).ready(function(){
   toggleIngredients(buttonMushrooms,mushrooms);
   toggleIngredients(buttonGreenpeppers,greenpeppers);
 
+  // SWITCH BETWEEN TWO TYPES FOR ONE GIVEN TOPPING (SAUCE/CRUST)
+  // for one given topping, adds/removes a class that changes the type of topping
+
+  buttonSauce.click(function(){
+    sauce.toggleClass("sauce-white");
+    buttonSauce.toggleClass("active");
+    $(".panel ul li:contains('white')").toggle();
+  })
+
+  buttonCrust.click(function(){
+    crust.toggleClass("crust-gluten-free");
+    buttonCrust.toggleClass("active");
+    $(".panel ul li:contains('gluten')").toggle();
+  })
+
   // DISPLAY PRICES DEPENDING ON THE PRESSED BUTTONS
+  // when we press a given button (mushrooms Button for ex.), using a keyword (ex:"mushroom") that is inside the <li> we target (thanks to the selector)
+  // it shows/hides this price of this element
 
   function togglePrices(button,keyword,selector) {
     button.click(function(){
@@ -52,21 +70,8 @@ $(document).ready(function(){
   togglePrices(buttonMushrooms, "mushrooms",".panel ul li");
   togglePrices(buttonGreenpeppers, "green",".panel ul li");
 
-  // SWITCH BETWEEN TWO TYPES OF INGREDIENTS FOR ONE GIVEN TOPPING
-
-  $(".btn-sauce").click(function(){
-    $(".sauce").toggleClass("sauce-white");
-    $(".btn-sauce").toggleClass("active");
-    $(".panel ul li:contains('white')").toggle();
-  })
-
-  $(".btn-crust").click(function(){
-    $(".crust").toggleClass("crust-gluten-free");
-    $(".btn-crust").toggleClass("active");
-    $(".panel ul li:contains('gluten')").toggle();
-  })
-
   // CHECK FOR TOTAL PRICE
+  // everytime a ingredient button is pressed, and depending if this item is shown or not on the page, we add its price to the total price
 
   $(".btn").click(function(){
     let myTotalPriceContainer = $("strong");
