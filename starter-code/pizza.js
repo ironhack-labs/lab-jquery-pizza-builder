d34c1fa8ff3b7017d90e7cef247eb173212d36df
Leandro Martins <leandromartins85@gmail.com>
2019-07-24 22:51:35 -0300

    Iteration 1, 2 e 3 done.

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,71 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+  // add and remove peps
+  $(".btn-pepperonni").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(".pep").each(function(index, element) {
+        $(element).fadeOut("fast");
+      });
+    } else {
+      $(this).addClass("active");
+      $(".pep").each(function(index, element) {
+        $(element).fadeIn("fast");
+      });
+    }
+  });
+  // add and remove mushroom
+  $(".btn-mushrooms").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(".mushroom").each(function(index, element) {
+        $(element).fadeOut("fast");
+      });
+    } else {
+      $(this).addClass("active");
+      $(".mushroom").each(function(index, element) {
+        $(element).fadeIn("fast");
+      });
+    }
+  });
+  // add and remove btn-green-peppers
+  $(".btn-green-peppers").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(".green-pepper").each(function(index, element) {
+        $(element).fadeOut("fast");
+      });
+    } else {
+      $(this).addClass("active");
+      $(".green-pepper").each(function(index, element) {
+        $(element).fadeIn("fast");
+      });
+    }
+  });
+
+  $(".crust").removeClass("crust-gluten-free");
+  $(".btn-crust").removeClass("active");
+
+  $(".btn-crust").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(".crust").removeClass("crust-gluten-free");
+    } else {
+      $(this).addClass("active");
+      $(".crust").addClass("crust-gluten-free");
+    }
+  });
+
+  $(".sauce").removeClass("sauce-white");
+  $(".btn-sauce").removeClass("active");
+
+  $(".btn-sauce").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(".sauce").removeClass("sauce-white");
+    } else {
+      $(this).addClass("active");
+      $(".sauce").addClass("sauce-white");
+    }
+  });
+});
