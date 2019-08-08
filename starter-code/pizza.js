8fcfac3b5c213edcb7e7619fa2431f23c8ea7fe3
nancy27vazquez <nevadg@outlook.com>
2019-08-07 22:35:04 -0500

    Iteration 1: Add and remove toppings

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,32 @@
 // Write your Pizza Builder JavaScript in this file.
+
+// Adding and removing pepperonni
+$(document).ready(function() {
+  $(".btn-pepperonni").click(function() {
+    $(".pep").toggleClass("off");
+  });
+});
+
+// Adding and removing mushroom
+$(document).ready(function() {
+  $(".btn-mushrooms").click(function() {
+    $(".mushroom").toggleClass("off");
+  });
+});
+
+// Adding and removing green pepper
+$(document).ready(function() {
+  $(".btn-green-peppers").click(function() {
+    $(".green-pepper").toggleClass("off");
+  });
+});
+
+/* Reference:
+
+$( document ).ready(function() {
+  $('.heart').click(function(){
+    $(this).toggleClass('on');
+  });
+});
+
+*/
