66928f891e64255d030c5a9f78598a441476ba30
guimartinsalmeida <50251761+guimartinsalmeida@users.noreply.github.com>
2019-08-21 08:08:37 -0500

    Update pizza.js

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,73 +1,81 @@
 //usar toggle
 // Write your Pizza Builder JavaScript in this file.
-let total = 13
-function calculatePrecio() {
-  $('.price strong').text('$' + total)
-}
-//pizza peperoni
-$('.btn-pepperonni').click(function(event) {
-  $('* .pep').toggle()
-  $('.btn-pepperonni').toggleClass('active')
-  $('.price ul li:nth-child(1)').toggle()
-  //toggle
-  if ($('.btn-pepperonni').hasClass('active')) {
-    total += 1
-  } else {
-    total -= 1
-  }
-  calculatePrecio()
-})
+//usar toggle
+// Write your Pizza Builder JavaScript in this file.
+var totalPrecio = 13
+$('.price strong').text('$' + totalPrecio)
 
-$('.btn-mushrooms').click(function(event) {
-  $('* .mushroom').toggle()
-  $('.btn-mushrooms').toggleClass('active')
-  $('.price ul li:nth-child(2)').toggle()
-  if ($('.btn-mushrooms').hasClass('active')) {
-    total += 1
-  } else {
-    total -= 1
-  }
-  calculatePrecio()
-})
-
-$('.btn-green-peppers').click(function(event) {
-  $('* .green-pepper').toggle()
-  $('.btn-green-peppers').toggleClass('active')
-  $('.price ul li:nth-child(3)').toggle()
-  if ($('.btn-green-peppers').hasClass('active')) {
-    total += 1
-  } else {
-    total -= 1
-  }
-  calculatePrecio()
-})
-
-$('.sauce').toggleClass('sauce-white')
-$('.btn-sauce').toggleClass('active')
-$('.price ul li:nth-child(4)').toggle()
-$('.btn-sauce').click(function(event) {
-  $('.sauce').toggleClass('sauce-white')
-  $('.btn-sauce').toggleClass('active')
-  $('.price ul li:nth-child(4)').toggle()
-  if ($('.btn-sauce').hasClass('active')) {
-    total += 3
-  } else {
-    total -= 3
-  }
-  calculatePrecio()
-})
-//gluten free
-$('.crust').toggleClass('crust-gluten-free')
-$('.btn-crust').toggleClass('active')
-$('.price ul li:nth-child(5)').toggle()
-$('.btn-crust').click(function(event) {
+$(function() {
   $('.crust').toggleClass('crust-gluten-free')
   $('.btn-crust').toggleClass('active')
-  $('.price ul li:nth-child(5)').toggle()
-  if ($('.btn-crust').hasClass('active')) {
-    total += 5
-  } else {
-    total -= 5
-  }
-  calculatePrecio()
+  $('.sauce').toggleClass('sauce-white')
+  $('.btn-sauce').toggleClass('active')
+  $($(".price li:contains('$3 white sauce')")).hide()
+  $($(".price li:contains('$5 gluten-free crust')")).hide()
+
+  $('.btn-pepperonni').click(function() {
+    $('.pep').toggle()
+    $(this).toggleClass('active')
+
+    $($(".price li:contains('$1 pepperonni')")).toggle()
+    if ($(this).hasClass('active')) {
+      totalPrecio += 1
+    } else {
+      totalPrecio -= 1
+    }
+    upPrecio()
+  })
+  $('.btn-mushrooms').click(function() {
+    $('.mushroom').toggle()
+    $(this).toggleClass('active')
+
+    $($(".price li:contains('$1 mushrooms')")).toggle()
+    if ($(this).hasClass('active')) {
+      totalPrecio += 1
+    } else {
+      totalPrecio -= 1
+    }
+    upPrecio()
+  })
+  $('.btn-green-peppers').click(function() {
+    $('.green-pepper').toggle()
+    $(this).toggleClass('active')
+
+    $($(".price li:contains('$1 green peppers')")).toggle()
+    if ($(this).hasClass('active')) {
+      totalPrecio += 1
+    } else {
+      totalPrecio -= 1
+    }
+    upPrecio()
+  })
+  $('.btn-sauce').click(function() {
+    $('.sauce').toggleClass('sauce-white')
+    $(this).toggleClass('active')
+
+    $($(".price li:contains('$3 white sauce')")).toggle()
+    if ($(this).hasClass('active')) {
+      totalPrecio += 3
+    } else {
+      totalPrecio -= 3
+    }
+    upPrecio()
+  })
+  $('.btn-crust').click(function() {
+    $('.crust').toggleClass('crust-gluten-free')
+    $(this).toggleClass('active')
+
+    $($(".price li:contains('$5 gluten-free crust')")).toggle()
+    if ($(this).hasClass('active')) {
+      totalPrecio += 5
+    } else {
+      totalPrecio -= 5
+    }
+    upPrecio()
+  })
 })
+
+function upPrecio() {
+  $('.price strong').text('')
+  $('.price strong').text('$' + totalPrecio)
+}
